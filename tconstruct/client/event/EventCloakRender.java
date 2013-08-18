d786d59ae0b24b5d563289314a8fcb9bf5d721a0
Arthur Uzulin <fuj1nman@hotmail.com>
2013-08-18 15:24:25 -0700

    Made the capes even more efficient
    The thread is only started when it needs to download the cape

--- tconstruct/client/event/EventCloakRender.java
+++ tconstruct/client/event/EventCloakRender.java
@@ -24,7 +24,21 @@ public class EventCloakRender {
 	
 	@ForgeSubscribe
 	public void onPreRenderSpecials(RenderPlayerEvent.Specials.Pre event){
-		new Thread(new CloakThread(event)).start();
+		if(event.entityPlayer instanceof AbstractClientPlayer){
+			AbstractClientPlayer abstractClientPlayer = (AbstractClientPlayer)event.entityPlayer;
+			
+			if (abstractClientPlayer.func_110310_o().field_110560_d == null) {
+				String cloakURL = cloaks.get(event.entityPlayer.username);
+				
+				if(cloakURL == null){
+					return;
+				}
+				
+				abstractClientPlayer.func_110310_o().field_110559_g = false;
+				new Thread(new CloakThread(abstractClientPlayer, cloakURL)).start();
+				event.renderCape = true;
+			}
+		}
 	}
 	
 	public void buildCloakURLDatabase(){
@@ -57,38 +71,24 @@ public class EventCloakRender {
 	
 	private class CloakThread implements Runnable{
 
-		RenderPlayerEvent.Specials.Pre event;
+		AbstractClientPlayer abstractClientPlayer;
+		String cloakURL;
 		
-		public CloakThread(RenderPlayerEvent.Specials.Pre event){
-			this.event = event;
+		public CloakThread(AbstractClientPlayer player, String cloak){
+			abstractClientPlayer = player;
+			cloakURL = cloak;
 		}
-		
+
 		@Override
 		public void run() {
-			if(event.entityPlayer instanceof AbstractClientPlayer){
-				AbstractClientPlayer abstractClientPlayer = (AbstractClientPlayer)event.entityPlayer;
-				
-				if (abstractClientPlayer.func_110310_o().field_110560_d == null) {
-					String cloakURL = cloaks.get(event.entityPlayer.username);
-					
-					if(cloakURL == null){
-						return;
-					}
-					
-					BufferedImage bo = new BufferedImage(64, 32, BufferedImage.TYPE_INT_RGB);
-					abstractClientPlayer.func_110310_o().field_110559_g = false;
-					try {
-						bo.getGraphics().drawImage(new ImageIcon(new URL(cloakURL)).getImage(), 0, 0, null);
-						abstractClientPlayer.func_110310_o().field_110560_d = bo;
-					} catch (MalformedURLException e) {
-						e.printStackTrace();
-					}
-
-					event.renderCape = true;
-				}
+			try {
+				BufferedImage bo = new BufferedImage(64, 32, BufferedImage.TYPE_INT_RGB);
+				bo.getGraphics().drawImage(new ImageIcon(new URL(cloakURL)).getImage(), 0, 0, null);
+				abstractClientPlayer.func_110310_o().field_110560_d = bo;
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
 			}
 		}
-		
 	}
 	
 }
