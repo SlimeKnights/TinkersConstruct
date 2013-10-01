2bc58d41b206eb67aa6bf1e930d9e599ff407700
progwml6 <progwml6@gmail.com>
2013-09-30 23:54:04 -0400

    clean up formatting, tweak build.xml more

--- src/tconstruct/plugins/fmp/ForgeMultiPart.java
+++ src/tconstruct/plugins/fmp/ForgeMultiPart.java
@@ -1,19 +1,18 @@
 package tconstruct.plugins.fmp;
 
-
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import cpw.mods.fml.common.*;
+import tconstruct.common.TContent;
+import tconstruct.plugins.fmp.register.RegisterWithFMP;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import tconstruct.common.TContent;
-import tconstruct.plugins.fmp.register.RegisterWithFMP;
-
 
 @Mod(modid = "TConstruct|ForgeMuliPart", name = "TConstruct Compat: FMP", version = "0.1", dependencies = "after:ForgeMultipart;after:TConstruct")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
-public class ForgeMultiPart {
+public class ForgeMultiPart
+{
     @EventHandler
     public static void load (FMLInitializationEvent ev)
     {
@@ -30,13 +29,13 @@ public class ForgeMultiPart {
             RegisterWithFMP.registerBlock(TContent.clearGlass);
             RegisterWithFMP.registerBlock(TContent.searedBlock);
             //register blocks w/ metadata here
-            for (int x =0;x<=15;x++)
+            for (int x = 0; x <= 15; x++)
             {
                 RegisterWithFMP.registerBlock(TContent.stainedGlassClear, x);
-                RegisterWithFMP.registerBlock(TContent.multiBrick,x);
-                RegisterWithFMP.registerBlock(TContent.metalBlock ,x);
+                RegisterWithFMP.registerBlock(TContent.multiBrick, x);
+                RegisterWithFMP.registerBlock(TContent.metalBlock, x);
                 RegisterWithFMP.registerBlock(TContent.multiBrickFancy, x);
-                
+
             }
 
         }
@@ -45,6 +44,5 @@ public class ForgeMultiPart {
             e.printStackTrace();
         }
     }
-   
 
 }
--- src/tconstruct/plugins/fmp/register/RegisterWithFMP.java
+++ src/tconstruct/plugins/fmp/register/RegisterWithFMP.java
@@ -4,22 +4,24 @@ import net.minecraft.block.Block;
 import codechicken.microblock.BlockMicroMaterial;
 import codechicken.microblock.MicroMaterialRegistry;
 
-public class RegisterWithFMP {
-    public static void registerBlock (Block block,int meta)
+public class RegisterWithFMP
+{
+    public static void registerBlock (Block block, int meta)
     {
         // meta = number 0->15
-        String identifier = new String( block.getUnlocalizedName());
+        String identifier = new String(block.getUnlocalizedName());
         MicroMaterialRegistry.registerMaterial(new BlockMicroMaterial(block, meta), identifier + meta);
-       // BlockMicroMaterial.createAndRegister(block);
+        // BlockMicroMaterial.createAndRegister(block);
 
         //MicroMaterialRegistry.registerMaterial((IMicroMaterial) block.blockMaterial, "tinkers:" + identifier + meta);
     }
-    public static void registerBlock (Block block )
+
+    public static void registerBlock (Block block)
     {
         //s = string identifier for meta ex: brick...
         //MicroMaterialRegistry.registerMaterial(material, s + meta);
-        String identifier = new String( block.getUnlocalizedName());
-    
+        String identifier = new String(block.getUnlocalizedName());
+
         //MicroMaterialRegistry.registerMaterial(new BlockMicroMaterial(block, 0), identifier );
 
         //MicroMaterialRegistry.registerMaterial((IMicroMaterial) block.blockMaterial,"tinkers:" + identifier);
