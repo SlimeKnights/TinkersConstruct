dc0319101e0973c21092a6f6a72214b7ee20309b
samrg472 <samrg472@gmail.com>
2013-10-04 17:53:11 -0400

    Restore compatibility with MFR

--- src/tconstruct/plugins/minefactoryreloaded/MineFactoryReloaded.java
+++ src/tconstruct/plugins/minefactoryreloaded/MineFactoryReloaded.java
@@ -1,14 +1,13 @@
 package tconstruct.plugins.minefactoryreloaded;
 
-import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import net.minecraft.item.ItemStack;
 import powercrystals.minefactoryreloaded.api.FarmingRegistry;
 import tconstruct.common.TContent;
-import tconstruct.entity.BlueSlime;
-import tconstruct.plugins.minefactoryreloaded.grindables.GrindableStandard;
 import tconstruct.plugins.minefactoryreloaded.harvestables.HarvestableOreBerry;
 
 @Mod(modid = "TConstruct|CompatMineFactoryReloaded", name = "TConstruct Compat: MFR", version = "0.1", dependencies = "after:MineFactoryReloaded;after:TConstruct")
@@ -29,13 +28,6 @@ public class MineFactoryReloaded
 
             FarmingRegistry.registerHarvestable(new HarvestableOreBerry(TContent.oreBerry.blockID, TContent.oreBerries.itemID, 0));
             FarmingRegistry.registerHarvestable(new HarvestableOreBerry(TContent.oreBerrySecond.blockID, TContent.oreBerries.itemID, 4));
-
-            // A note: in 1.6, this method of registering grindables will no longer exist, once MFR moves to a more universal way of gathering drops.
-            FarmingRegistry.registerGrindable(new GrindableStandard(BlueSlime.class, new ItemStack(TContent.strangeFood)));
-            //FarmingRegistry.registerGrindable(new GrindableStandard(Crystal.class, new ItemStack(Item.gunpowder)));
-            //FarmingRegistry.registerGrindable(new GrindableStandard(MetalSlime.class, new ItemStack(TContent.strangeFood)));
-            //FarmingRegistry.registerGrindable(new GrindableStandard(NitroCreeper.class, new ItemStack(Item.gunpowder)));
-            //FarmingRegistry.registerGrindable(new GrindableStandard(Skyla.class, new ItemStack(Item.gunpowder)));
 
             /*
              * Perhaps TC ores should be registered as drops from the MFR Laser Drill here, but I don't know which things would be suitable for that.
