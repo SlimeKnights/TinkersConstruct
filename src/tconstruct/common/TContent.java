c50a1b3d29c27c30bc7e09334a634809e88e3ad0
Progwml6 <progwml6@gmail.com>
2013-10-06 23:53:35 -0400

    update thaumium support by upating the location of the necessary config in Thaumcraft 4

--- src/tconstruct/common/TContent.java
+++ src/tconstruct/common/TContent.java
@@ -2025,7 +2025,7 @@ public class TContent implements IFuelHandler
             TConstructClientRegistry.registerManualModifier("electricmod", ironpick.copy(), (ItemStack) chargedReBattery, (ItemStack) electronicCircuit);
 
         /* Thaumcraft */
-        Object obj = getStaticItem("itemResource", "thaumcraft.common.Config");
+        Object obj = getStaticItem("itemResource", "thaumcraft.common.config.ConfigItems");
         if (obj != null)
         {
             System.out.println("[TConstruct] Thaumcraft detected. Adding thaumium tools.");
