766b4cf6c3e202ff0bd949c9374a97a32f885c3c
mDiyo <merdiwendiyo@gmail.com>
2013-10-20 18:55:22 -0700

    Pure Peace

--- src/tconstruct/plugins/misc/PeaceOfMind.java
+++ src/tconstruct/plugins/misc/PeaceOfMind.java
@@ -13,22 +13,51 @@ public class PeaceOfMind
 {
     public PeaceOfMind()
     {
+        /** Explicit permission: http://forum.industrial-craft.net/index.php?page=Thread&postID=121457#post121457 */
+
+        try
+        {
+            Class clazz = Class.forName("gregtechmod.GT_Mod");
+            
+            try
+            {
+                Field ice = clazz.getDeclaredField("mDoNotInit");
+                ice.setBoolean(clazz, true);
+            }
+            catch (Exception e)
+            {
+                System.err.println("Could not set init to false");
+                e.printStackTrace();
+            }
+            
+            try
+            {
+                Class ores = Class.forName("gregtechmod.common.GT_OreDictHandler");
+                Field ice = ores.getDeclaredField("instance");
+                MinecraftForge.EVENT_BUS.unregister(ice);
+            }
+            catch (Exception e)
+            {
+                System.err.println("Cannot unregister GregTech Ore handler");
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e)
+        {
+            
+        }
+        
         for (ModContainer o : Loader.instance().getModList())
         {
             if (o.getModId().toLowerCase().contains("gregtech"))
             {
                 try
                 {
-                    /** Explicit permission: http://forum.industrial-craft.net/index.php?page=Thread&postID=121457#post121457 */
                     ModContainer mod = o;
                     Field ann = FMLModContainer.class.getDeclaredField("eventBus");
                     ann.setAccessible(true);
                     com.google.common.eventbus.EventBus googlebus = (com.google.common.eventbus.EventBus) ann.get(mod);
                     googlebus.unregister(mod);
-                    MinecraftForge.EVENT_BUS.unregister(mod);
-                    
-                    Class clazz = Class.forName("gregtechmod.common.GT_OreDictHandler");
-                    MinecraftForge.EVENT_BUS.unregister(clazz);
                 }
                 catch (Exception e)
                 {
@@ -37,6 +66,5 @@ public class PeaceOfMind
                 }
             }
         }
-
     }
 }
