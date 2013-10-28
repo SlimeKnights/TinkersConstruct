f28c169d43913c972649ca964bc3e5e00cf60813
Zach <progwml6@gmail.com>
2013-10-28 15:47:46 -0400

    get rid of extra space in NBT messages to mystcraft

--- src/tconstruct/util/config/MystImcHandler.java
+++ src/tconstruct/util/config/MystImcHandler.java
@@ -18,7 +18,7 @@ public static String[] FluidBlackList = new String[]{"moltenInvar", "moltenElect
 		NBTMsg.setCompoundTag("fluidsymbol",new NBTTagCompound());
 		NBTMsg.getCompoundTag("fluidsymbol").setFloat("rarity", 0.0F);
 		NBTMsg.getCompoundTag("fluidsymbol").setFloat("grammarweight", 0.0F);
-		NBTMsg.getCompoundTag("fluidsymbol").setFloat("instabilityPerBlock ", 10000F);// renders creative symbol useless
+		NBTMsg.getCompoundTag("fluidsymbol").setFloat("instabilityPerBlock", 10000F);// renders creative symbol useless
 		NBTMsg.getCompoundTag("fluidsymbol").setString("fluidname", FluidName);
 		FMLInterModComms.sendMessage("Mystcraft", "fluidsymbol", NBTMsg);
 	}
