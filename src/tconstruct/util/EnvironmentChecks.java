a28021cdda07814b34d1806800098b0d9d469757
Sunstrike <sunstrike@azurenode.net>
2013-11-17 11:35:59 +0000

    Add warning-shot to DragonAPI in EnvChecks.

--- src/tconstruct/util/EnvironmentChecks.java
+++ src/tconstruct/util/EnvironmentChecks.java
@@ -26,8 +26,7 @@ public class EnvironmentChecks
 
         if (Loader.isModLoaded("gregtech_addon"))
         {
-            TConstruct.logger
-                    .severe("[Environment Checks] Gelatinous iceberg dead ahead! Entering Greggy waters! Abandon hope all ye who enter here! (No, seriously, we don't support GT. Don't report any issues. Thanks.)");
+            TConstruct.logger.severe("[Environment Checks] Gelatinous iceberg dead ahead! Entering Greggy waters! Abandon hope all ye who enter here! (No, seriously, we don't support GT. Don't report any issues. Thanks.)");
             TConstruct.logger.severe("[Environment Checks] A NOTE TO THE USER: Gregtech has been known to crash the game with Metallurgy and for certain specific 'banished' players.");
             TConstruct.logger.severe("[Environment Checks]                     For this reason, the TCon dev team urge you to seek your tedium fix from someone else, as Greg disobeys");
             TConstruct.logger.severe("[Environment Checks]                     the basic tenets of good modders conduct. We log this message rather than sabotage. We'd hope he'd do the same.");
@@ -35,10 +34,16 @@ public class EnvironmentChecks
             modIds.add("gregtech_addon");
         }
 
-        if (FMLCommonHandler.instance().getSide() == Side.CLIENT && FMLClientHandler.instance().hasOptifine())
+        if (FMLCommonHandler.instance().getSide() == Side.CLIENT && FMLClientHandler.instance().hasOptifine() || Loader.isModLoaded("optifine"))
         {
             TConstruct.logger.severe("[Environment Checks] Optifine detected. This is a Bad Thing(tm) and can crash Minecraft due to an Optifine bug during TCon armor renders!");
             modIds.add("optifine");
+        }
+
+        if (Loader.isModLoaded("DragonAPI"))
+        {
+            TConstruct.logger.severe("[Environment Checks] DragonAPI detected. Currently this just logs a warning, however if Reika continues his plan to implement DRM-like");
+            TConstruct.logger.severe("[Environment Checks] code in his mod then it may become marked as fully unsupported. This is just a warning at this stage.");
         }
 
         try
