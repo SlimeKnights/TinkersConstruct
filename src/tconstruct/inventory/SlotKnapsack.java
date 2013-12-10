041381258c0a0b1bc38d6c884c5045254982bb8c
unleashurgeek <kyle@unleashurgeek.com>
2013-12-09 20:44:25 -0800

    Fixed unused, Knapsack, and Canister slots

---
+++ src/tconstruct/inventory/SlotKnapsack.java
@@ -0,0 +1,34 @@
+package tconstruct.inventory;
+
+import tconstruct.items.armor.Knapsack;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+
+public class SlotKnapsack extends Slot
+{
+    public SlotKnapsack(IInventory par2IInventory, int par3, int par4, int par5)
+    {
+        super(par2IInventory, par3, par4, par5);
+        this.parent = container;
+    }
+
+    /**
+     * Returns the maximum stack size for a given slot (usually the same as getInventoryStackLimit(), but 1 in the case
+     * of armor slots)
+     */
+    public int getSlotStackLimit ()
+    {
+        return 1;
+    }
+
+    /**
+     * Check if the stack is a valid item for this slot. Always true beside for the armor slots.
+     */
+    public boolean isItemValid (ItemStack par1ItemStack)
+    {
+        Item item = (par1ItemStack == null ? null : par1ItemStack.getItem());
+        return item != null && (item instanceof Knapsack);
+    }
+}
