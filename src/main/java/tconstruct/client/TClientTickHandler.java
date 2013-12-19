d579d0047faa1189fa33d76e547d4e459061fcdc
mDiyo <merdiwendiyo@gmail.com>
2013-12-19 08:14:59 -0800

    Revert "Add incompatibles to EnvironmentChecks"
    This reverts commit 1b1803c48bc8f0449306d1a47dc232a3a5bd0176.

--- src/main/java/tconstruct/client/TClientTickHandler.java
+++ src/main/java/tconstruct/client/TClientTickHandler.java
@@ -4,7 +4,10 @@ import java.util.EnumSet;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.ResourceLocation;
+import tconstruct.TConstruct;
+import tconstruct.common.TContent;
 import tconstruct.common.TRepo;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
@@ -12,6 +15,7 @@ import cpw.mods.fml.common.TickType;
 public class TClientTickHandler implements ITickHandler
 {
     Minecraft mc = Minecraft.getMinecraft();
+    //TControls controlInstance = ((TProxyClient)TConstruct.proxy).controlInstance;
 
     @Override
     public void tickEnd (EnumSet<TickType> type, Object... tickData)
@@ -19,6 +23,8 @@ public class TClientTickHandler implements ITickHandler
         TRepo.oreBerry.setGraphicsLevel(Block.leaves.graphicsLevel);
         TRepo.oreBerrySecond.setGraphicsLevel(Block.leaves.graphicsLevel);
         TRepo.slimeLeaves.setGraphicsLevel(Block.leaves.graphicsLevel);
+        /*if (mc.thePlayer != null && !mc.thePlayer.isAirBorne)
+            controlInstance.landOnGround();*/
     }
 
     @Override
--- src/main/java/tconstruct/util/EnvironmentChecks.java
+++ src/main/java/tconstruct/util/EnvironmentChecks.java
@@ -1,20 +1,15 @@
 package tconstruct.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import mantle.crash.CallableSuppConfig;
-import mantle.crash.CallableUnsuppConfig;
-import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ForgeSubscribe;
-import tconstruct.TConstruct;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.ICrashCallable;
 import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tconstruct.TConstruct;
+import mantle.crash.CallableSuppConfig;
+import mantle.crash.CallableUnsuppConfig;
 
 public class EnvironmentChecks
 {
@@ -27,19 +22,14 @@ public class EnvironmentChecks
      * Checks for conflicting stuff in environment; adds callable to any crash logs if so.
      * Note: This code adds additional data to crashlogs. It does not trigger any crashes.
      */
-    private static List<String> incompatibilities = new ArrayList<String>();
-    private static EnvironmentChecks instance = new EnvironmentChecks();
-
     public static void verifyEnvironmentSanity ()
     {
         List<String> modIds = new ArrayList<String>();
 
         if (Loader.isModLoaded("gregtech_addon"))
         {
-            TConstruct.logger.severe("Tinkers' Construct and GregTech are incompatible for the following reasons:");
-            TConstruct.logger.severe(modCompatDetails("GregTech", true));
+            TConstruct.logger.severe("[Environment Checks] Gelatinous iceberg dead ahead! Entering Greggy waters! Abandon hope all ye who enter here! (No, seriously, we don't support GT. Don't report any issues. Thanks.)");
             modIds.add("gregtech_addon");
-            incompatibilities.add("GregTech");
         }
 
         // Bukkit/Magic Launcher/Optifine are caught by Mantle, so we no longer check for those.
@@ -54,38 +44,6 @@ public class EnvironmentChecks
             ICrashCallable callable = new CallableUnsuppConfig("TConstruct", modIds);
             FMLCommonHandler.instance().registerCrashCallable(callable);
         }
-
-        if (incompatibilities.size() > 0 && FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
-        {
-            MinecraftForge.EVENT_BUS.register(instance);
-        }
     }
 
-    public static String modCompatDetails (String type, boolean consoleFormat)
-    {
-        String n = consoleFormat ? System.getProperty("line.separator") : "\n";
-        if (type.equals("GregTech"))
-        {
-            return  "- GregTech is an \"IndustrialCraft 2 addon\". People do not expect a total conversion meta-mod that alters every other mod when they install it the first time." + n
-                    + "- The ore dictionary's purpose is interchangable items and mod compatibility. It is not a tool to bend people to your will." + n 
-                    + "- GregTech actively breaks mods with \"invalid ore dictionary entries\"." + n
-                    + "- The mod has a blacklist that can corrupt worlds of people on it." + n
-                    + "- The author blames any problems introduced by his mod on others. See \"NotMyFaultException\" for intentional crashes.)";
-        }
-        return "";
-    }
-
-    @SideOnly(Side.CLIENT)
-    @ForgeSubscribe
-    public void openMainMenu (GuiOpenEvent event)
-    {
-        if (event.gui instanceof GuiMainMenu)
-        {
-            if (incompatibilities.size() > 0)
-            {
-                event.gui = new EnvironmentGui(event.gui, incompatibilities);
-            }
-            MinecraftForge.EVENT_BUS.unregister(instance);
-        }
-    }
 }
