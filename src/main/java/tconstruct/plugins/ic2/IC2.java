2fbeb7f516a82d73245cf3103095cf8bc34ff5ef
Sunstrike <sun@sunstrike.io>
2013-12-19 13:02:19 +0000

    Update IC2 plugin.
    Adjust liquid value for dirt-to-diamonds, remove all other UU recipes.

--- src/main/java/tconstruct/plugins/ic2/IC2.java
+++ src/main/java/tconstruct/plugins/ic2/IC2.java
@@ -38,27 +38,27 @@ public class IC2 implements ICompatPlugin {
         if (fluidUUM == null) return;
 
         // Useful stuff
-        ItemStack ingotCast = new ItemStack(TRepo.metalPattern, 1, 0);
-        ItemStack gemCast = new ItemStack(TRepo.metalPattern, 1, 26);
-        FluidStack fluidStack = new FluidStack(fluidUUM, 1);
+        //ItemStack ingotCast = new ItemStack(TRepo.metalPattern, 1, 0);
+        //ItemStack gemCast = new ItemStack(TRepo.metalPattern, 1, 26);
+        //FluidStack fluidStack = new FluidStack(fluidUUM, 1);
         FluidStack fluidStackBlock = new FluidStack(fluidUUM, 9);
-        LiquidCasting tableCasting = TConstructRegistry.getTableCasting();
+        //LiquidCasting tableCasting = TConstructRegistry.getTableCasting();
         LiquidCasting basinCasting = TConstructRegistry.getBasinCasting();
 
         // Table casting
-        tableCasting.addCastingRecipe(new ItemStack(Item.ingotIron), fluidStack, ingotCast, 20);
-        tableCasting.addCastingRecipe(new ItemStack(Item.emerald), fluidStack, gemCast, 20);
+        //tableCasting.addCastingRecipe(new ItemStack(Item.ingotIron), fluidStack, ingotCast, 20);
+        //tableCasting.addCastingRecipe(new ItemStack(Item.emerald), fluidStack, gemCast, 20);
 
         // Block casting
-        basinCasting.addCastingRecipe(new ItemStack(Block.blockIron), fluidStackBlock, null, true, 50); // Iron convenience
+        //basinCasting.addCastingRecipe(new ItemStack(Block.blockIron), fluidStackBlock, null, true, 50); // Iron convenience
         // Dirt <-> Diamonds
-        basinCasting.addCastingRecipe(new ItemStack(Block.dirt), new FluidStack(fluidUUM, 500), new ItemStack(Block.blockDiamond), true, 50);
+        //basinCasting.addCastingRecipe(new ItemStack(Block.dirt), new FluidStack(fluidUUM, 500), new ItemStack(Block.blockDiamond), true, 50);
         basinCasting.addCastingRecipe(new ItemStack(Block.blockDiamond), fluidStackBlock, new ItemStack(Block.dirt), true, 50);
         // RS cycle
-        basinCasting.addCastingRecipe(new ItemStack(Block.blockRedstone), fluidStack, new ItemStack(Item.redstone), true, 50);
-        basinCasting.addCastingRecipe(new ItemStack(Block.hardenedClay, 14, 1), fluidStack, new ItemStack(Block.blockRedstone), true, 50);
-        basinCasting.addCastingRecipe(new ItemStack(Item.dyePowder, 1, 1), fluidStack, new ItemStack(Block.hardenedClay, 14, 1), true, 50);
-        basinCasting.addCastingRecipe(new ItemStack(Block.blockRedstone), fluidStack, new ItemStack(Item.dyePowder, 1, 1), true, 50);
+        //basinCasting.addCastingRecipe(new ItemStack(Block.blockRedstone), fluidStack, new ItemStack(Item.redstone), true, 50);
+        //basinCasting.addCastingRecipe(new ItemStack(Block.hardenedClay, 14, 1), fluidStack, new ItemStack(Block.blockRedstone), true, 50);
+        //basinCasting.addCastingRecipe(new ItemStack(Item.dyePowder, 1, 1), fluidStack, new ItemStack(Block.hardenedClay, 14, 1), true, 50);
+        //basinCasting.addCastingRecipe(new ItemStack(Block.blockRedstone), fluidStack, new ItemStack(Item.dyePowder, 1, 1), true, 50);
 
         // Alloy Mixing
         // 1mB Anything + 1mB UU = 2mB UU
