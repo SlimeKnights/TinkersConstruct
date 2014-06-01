54dda43dcf0efff9a1a8681c5192d980d84ea2c6
mDiyo <merdiwendiyo@gmail.com>
2014-06-01 19:01:43 +0000

    Fix memory leak by removing packet out

--- src/main/java/tconstruct/util/network/packet/PacketPipeline.java
+++ src/main/java/tconstruct/util/network/packet/PacketPipeline.java
@@ -123,8 +123,6 @@ public class PacketPipeline extends MessageToMessageCodec<FMLProxyPacket, Abstra
 
         default:
         }
-
-        out.add(pkt);
     }
 
     // Method to call from FMLInitializationEvent
