9ed65aace05e6f94b48b94e7fb64a6ed237986fb
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-08-20 22:38:24 +0200

    Fix obsolete/weird todo comment ;P

--- src/main/java/tconstruct/tools/TinkerTools.java
+++ src/main/java/tconstruct/tools/TinkerTools.java
@@ -358,7 +358,7 @@ public class TinkerTools
         registerStencils();
 
 
-        //TODO: Redesign stencil table to be a sensible block
+        // this array is only used to register the remaining pattern-part-interactions
         TinkerTools.patternOutputs = new Item[] { TinkerTools.toolRod, TinkerTools.pickaxeHead, TinkerTools.shovelHead, TinkerTools.hatchetHead, TinkerTools.swordBlade, TinkerTools.wideGuard,
                 TinkerTools.handGuard, TinkerTools.crossbar, TinkerTools.binding, TinkerTools.frypanHead, TinkerTools.signHead, TinkerTools.knifeBlade, TinkerTools.chiselHead, TinkerTools.toughRod,
                 TinkerTools.toughBinding, TinkerTools.largePlate, TinkerTools.broadAxeHead, TinkerTools.scytheBlade, TinkerTools.excavatorHead, TinkerTools.largeSwordBlade, TinkerTools.hammerHead,
