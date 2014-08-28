9e263e26debe353bfd57a6420b7141c94f12f684
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-08-28 11:25:27 +0200

    Unmerged 1.6.4 change: Move healthbar when experience bar isn't drawn #919

--- src/main/java/tconstruct/armor/ArmorProxyClient.java
+++ src/main/java/tconstruct/armor/ArmorProxyClient.java
@@ -243,6 +243,12 @@ public class ArmorProxyClient extends ArmorProxyCommon
                 int left = scaledWidth / 2 - 91;
                 int top = scaledHeight - GuiIngameForge.left_height;
 
+                if (!GuiIngameForge.renderExperiance)
+                {
+                    top += 7;
+                    yBasePos += 7;
+                }
+
                 int regen = -1;
                 if (mc.thePlayer.isPotionActive(Potion.regeneration))
                 {
