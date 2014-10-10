8fe42be9f2f627cb3ada89c2db02e9bc20374948
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-10-10 18:00:15 +0200

    Fix entity id conflicts

--- src/main/java/tconstruct/weaponry/WeaponryCommonProxy.java
+++ src/main/java/tconstruct/weaponry/WeaponryCommonProxy.java
@@ -6,10 +6,10 @@ import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class WeaponryCommonProxy {
     public void init() {
-        EntityRegistry.registerModEntity(ShurikenEntity.class, "Shuriken", 10, TConstruct.instance, 32, 5, true);
-        EntityRegistry.registerModEntity(ThrowingKnifeEntity.class, "ThrowingKnife", 11, TConstruct.instance, 32, 5, true);
-        EntityRegistry.registerModEntity(JavelinEntity.class, "Javelin", 12, TConstruct.instance, 32, 5, true);
-        EntityRegistry.registerModEntity(ArrowEntity.class, "Arrow", 13, TConstruct.instance, 32, 5, true);
-        EntityRegistry.registerModEntity(BoltEntity.class, "Bolt", 14, TConstruct.instance, 32, 5, true);
+        EntityRegistry.registerModEntity(ShurikenEntity.class, "Shuriken", 20, TConstruct.instance, 32, 5, true);
+        EntityRegistry.registerModEntity(ThrowingKnifeEntity.class, "ThrowingKnife", 21, TConstruct.instance, 32, 5, true);
+        EntityRegistry.registerModEntity(JavelinEntity.class, "Javelin", 22, TConstruct.instance, 32, 5, true);
+        EntityRegistry.registerModEntity(ArrowEntity.class, "Arrow", 23, TConstruct.instance, 32, 5, true);
+        EntityRegistry.registerModEntity(BoltEntity.class, "Bolt", 24, TConstruct.instance, 32, 5, true);
     }
 }
