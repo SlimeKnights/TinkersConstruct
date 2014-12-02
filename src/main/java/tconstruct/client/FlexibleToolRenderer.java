c4d9ebd5059fa28c1d6f38d97c870385d41f6baf
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-12-02 21:03:01 +0100

    Add a possibly missing rendering state reset

--- src/main/java/tconstruct/client/FlexibleToolRenderer.java
+++ src/main/java/tconstruct/client/FlexibleToolRenderer.java
@@ -251,6 +251,7 @@ public class FlexibleToolRenderer implements IItemRenderer {
 
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
     }
