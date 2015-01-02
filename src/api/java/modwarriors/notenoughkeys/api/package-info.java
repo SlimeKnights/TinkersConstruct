d183001a4fd6ac365ed683802dcb8edc636e87ad
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-01-02 18:54:04 +0100

    RIP terrible old source. You wont be missed. Still better than having a non working repo for ages.

--- src/api/java/modwarriors/notenoughkeys/api/package-info.java
+++
@@ -1,5 +0,0 @@
-
-@API(owner = "Not Enough Keys", provides = "API_NEK",
-		apiVersion = "1.0.0") package modwarriors.notenoughkeys.api;
-
-import cpw.mods.fml.common.API;
--- src/main/java/tconstruct/library/ItemBlocklike.java
+++
@@ -1,5 +0,0 @@
-package tconstruct.library;
-
-public interface ItemBlocklike
-{
-}
