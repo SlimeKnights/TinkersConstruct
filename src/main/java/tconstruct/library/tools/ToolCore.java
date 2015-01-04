db03603640a3d7f8bbabe12724032761a8fd3383
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-01-05 00:26:09 +0100

    First approach for tool building management is quite messy. Let's try a different one.

--- src/main/java/tconstruct/library/tools/ToolCore.java
+++ src/main/java/tconstruct/library/tools/ToolCore.java
@@ -1,6 +1,11 @@
 package tconstruct.library.tools;
 
 
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import tconstruct.library.ITinkerable;
 import tconstruct.library.utils.TagUtil;
 import tconstruct.library.utils.Tags;
@@ -9,6 +14,19 @@ import tconstruct.library.utils.Tags;
  * The base for each Tinker tool.
  */
 public abstract class ToolCore implements ITinkerable {
+
+  public final PartMaterialWrapper[] requiredComponents;
+
+  public ToolCore(PartMaterialWrapper[] requiredComponents) {
+    this.requiredComponents = requiredComponents;
+  }
+
+  public boolean validComponent(int slot, ItemStack stack) {
+    if(slot > requiredComponents.length || slot < 0)
+      return false;
+
+    return requiredComponents[slot].isValid(stack);
+  }
 
   @Override
   public String getTagName() {
--- src/main/java/tconstruct/library/utils/ToolBuilder.java
+++ src/main/java/tconstruct/library/utils/ToolBuilder.java
@@ -2,6 +2,7 @@ package tconstruct.library.utils;
 
 import net.minecraft.nbt.NBTTagCompound;
 
+import tconstruct.library.tools.materials.ToolMaterialStats;
 import tconstruct.library.tools.partbehaviors.HeadPartBehavior;
 import tconstruct.library.tools.partbehaviors.PartBehavior;
 
@@ -16,4 +17,34 @@ public final class ToolBuilder {
     return null;
   }
 
+  /**
+   * Takes an arbitrary amount of ToolMaterialStats and creates a tag with the mean of their stats.
+   * @return The resulting TagCompound
+   */
+  public static NBTTagCompound calculateHeadParts(ToolMaterialStats... stats) {
+    int durability = 0;
+    float attack = 0f;
+    float speed = 0f;
+
+    // sum up stats
+    for(ToolMaterialStats stat : stats)
+    {
+      durability += stat.durability;
+      attack += stat.attack;
+      speed += stat.miningspeed;
+    }
+
+    // take mean
+    durability /= stats.length;
+    attack /= (float)stats.length;
+    speed /= (float)stats.length;
+
+    // create output tag
+    NBTTagCompound tag = new NBTTagCompound();
+    tag.setInteger(Tags.DURABILITY, durability);
+    tag.setFloat(Tags.ATTACK, attack);
+    tag.setFloat(Tags.MININGSPEED, speed);
+
+    return tag;
+  }
 }
