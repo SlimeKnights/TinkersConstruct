0972d78077d4290f578c54b99b24e98d6e2cf011
Glassmaker <JeanGlassmaker@mail.com>
2015-01-06 04:02:42 +0300

    Adds UndergroundBiomes Construct support

---
+++ src/main/java/tconstruct/plugins/ubc/TinkerUBC.java
@@ -0,0 +1,37 @@
+package tconstruct.plugins.ubc;
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry.ObjectHolder;
+import exterminatorJeff.undergroundBiomes.api.UBAPIHook;
+import exterminatorJeff.undergroundBiomes.api.UBOreTexturizer.BlocksAreAlreadySet;
+import mantle.pulsar.pulse.Handler;
+import mantle.pulsar.pulse.Pulse;
+import tconstruct.TConstruct;
+import tconstruct.world.TinkerWorld;
+
+@ObjectHolder(TConstruct.modID)
+@Pulse(id = "Tinkers' Underground Biomes Compatiblity", description = "Tinkers' Construct compatibilty for Underground Biomes Construct", modsRequired = "UndergroundBiomes", pulsesRequired = "Tinkers' World", forced = true)
+public class TinkerUBC
+{
+    @Handler
+    public void preInit (FMLPreInitializationEvent event)
+    {
+        registerBlock(3, "Copper");
+        registerBlock(4, "Tin");
+        registerBlock(5, "Aluminum");
+    }
+
+    private void registerBlock (int meta, String blockName)
+    {
+        String overlayTexture = blockName.toLowerCase();
+        try
+        {
+            UBAPIHook.ubAPIHook.ubOreTexturizer.requestUBOreSetup(TinkerWorld.oreSlag, meta, "tinker:ore_" + overlayTexture + "_overlay", "MetalOre." + blockName);
+        }
+        catch (BlocksAreAlreadySet exception)
+        {
+            TConstruct.logger.error(blockName + " is already registered in UBC");
+        }
+
+    }
+}
