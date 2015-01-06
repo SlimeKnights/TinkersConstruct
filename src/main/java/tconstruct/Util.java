e520b22b655b6098c98a86f29bd56287042275ad
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-01-06 16:16:13 +0100

    Utility prefix functions from 1.7

--- src/main/java/tconstruct/Util.java
+++ src/main/java/tconstruct/Util.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Util {
+  public static final String RESOURCE = "tinker";
+
   public static Logger getLogger(String type) {
     String log = TConstruct.modID;
 
@@ -16,4 +18,19 @@ public class Util {
   public static String sanitizeLocalizationString(String string) {
     return string.toLowerCase().replaceAll(" ", "");
   }
+
+  /**
+   * Returns the given Resource prefixed with tinkers resource location.
+   * Use this function instead of hardcoding resource locations.
+   */
+  public static String resource(String res)
+  {
+    return String.format("%s:%s", RESOURCE, res);
+  }
+
+  /**
+   * Prefixes the given unlocalized name with tinkers prefix.
+   * Use this when passing unlocalized names for a uniform namespace.
+   */
+  public static String prefix(String name) { return String.format("tconstruct.%s", name);}
 }
