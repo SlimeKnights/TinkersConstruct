53999baa28568f28fbb1c60302fc829984dd8f56
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-02-12 10:12:25 +0100

    Final TE integration: Tesseract, Redstone Cell, Illuminator and plates are fillable with casting table/basin

--- src/main/java/tconstruct/plugins/te4/TinkerTE4.java
+++ src/main/java/tconstruct/plugins/te4/TinkerTE4.java
@@ -1,12 +1,17 @@
 package tconstruct.plugins.te4;
 
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.*;
 import cpw.mods.fml.common.registry.GameRegistry.ObjectHolder;
 import mantle.pulsar.pulse.*;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.*;
 import tconstruct.TConstruct;
+import tconstruct.library.TConstructRegistry;
 import tconstruct.tools.TinkerTools;
 import tconstruct.world.TinkerWorld;
 
@@ -35,5 +40,38 @@ public class TinkerTE4
         // Dust Recipes
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(TinkerTools.materials, 1, 41), "dustArdite", "dustCobalt"));
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(TinkerTools.materials, 4, 42), "dustAluminum", "dustAluminum", "dustAluminum", "dustCopper"));
+    }
+
+    @Handler
+    public void postInit(FMLPostInitializationEvent event) {
+        Fluid redstoneFluid = FluidRegistry.getFluid("redstone");
+        Fluid glowstoneFluid = FluidRegistry.getFluid("glowstone");
+        Fluid enderFluid = FluidRegistry.getFluid("ender");
+
+        // selected fluid transposer recipies
+        // tesseract
+        ItemStack tesseractEmpty = GameRegistry.findItemStack("ThermalExpansion", "frameTesseractEmpty" ,1);
+        ItemStack tesseractFull = GameRegistry.findItemStack("ThermalExpansion", "frameTesseractFull" ,1);
+        TConstructRegistry.getBasinCasting().addCastingRecipe(tesseractFull, new FluidStack(enderFluid, 1000), tesseractEmpty, true, 300);
+
+        // redstone energy cell
+        ItemStack cellFrameEmpty = GameRegistry.findItemStack("ThermalExpansion", "frameCellReinforcedEmpty", 1);
+        ItemStack cellFrameFull = GameRegistry.findItemStack("ThermalExpansion", "frameCellReinforcedFull", 1);
+        TConstructRegistry.getBasinCasting().addCastingRecipe(cellFrameFull, new FluidStack(redstoneFluid, 4000), cellFrameEmpty, true, 100);
+
+        // glowstone illuminator
+        ItemStack illuminatorEmpty = GameRegistry.findItemStack("ThermalExpansion", "frameIlluminator" ,1);
+        ItemStack illuminator = GameRegistry.findItemStack("ThermalExpansion", "illuminator" ,1);
+        TConstructRegistry.getBasinCasting().addCastingRecipe(illuminator, new FluidStack(glowstoneFluid, 1000), illuminatorEmpty, true, 300);
+
+        // plates
+        ItemStack plate = GameRegistry.findItemStack("ThermalExpansion", "plateFrame" ,1);
+        ItemStack plateRedstone = GameRegistry.findItemStack("ThermalExpansion", "plateSignal" ,1);
+        ItemStack plateGlowstone = GameRegistry.findItemStack("ThermalExpansion", "plateImpulse" ,1);
+        ItemStack plateEnder = GameRegistry.findItemStack("ThermalExpansion", "plateTranslocate" ,1);
+
+        TConstructRegistry.getTableCasting().addCastingRecipe(plateRedstone, new FluidStack(redstoneFluid, 1000), plate, true, 100);
+        TConstructRegistry.getTableCasting().addCastingRecipe(plateGlowstone, new FluidStack(glowstoneFluid, 1000), plate, true, 100);
+        TConstructRegistry.getTableCasting().addCastingRecipe(plateEnder, new FluidStack(enderFluid, 1000), plate, true, 100);
     }
 }
