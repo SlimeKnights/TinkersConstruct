daf38a16cb55961550e608cff487d389b3afe6af
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-03-17 18:18:13 +0100

    Abstract our generated textures out and actually color by base texture!

---
+++ src/main/java/tconstruct/test/AbstractColoredTexture.java
@@ -0,0 +1,170 @@
+package tconstruct.test;
+
+import com.google.common.collect.Lists;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Arrays;
+
+import tconstruct.library.utils.Log;
+
+public abstract class AbstractColoredTexture extends TextureAtlasSprite {
+  private final TextureAtlasSprite baseTexture;
+
+  protected AbstractColoredTexture(TextureAtlasSprite baseTexture, String spriteName) {
+    super(spriteName);
+
+    this.baseTexture = baseTexture;
+  }
+
+  @Override
+  public boolean hasCustomLoader(IResourceManager manager, ResourceLocation location) {
+    return true;
+  }
+
+  @Override
+  public boolean load(IResourceManager manager, ResourceLocation location) {
+    this.framesTextureData = Lists.newArrayList();
+    this.frameCounter = 0;
+    this.tickCounter = 0;
+
+    // get the base texture to work on
+    int[][] data;
+    if(baseTexture.getFrameCount() > 0) {
+      this.copyFrom(baseTexture);
+      int[][] original = baseTexture.getFrameTextureData(0);
+      data = new int[original.length][];
+      for(int i = 0; i < original.length; i++)
+        data[i] = Arrays.copyOf(original[i], original[i].length);
+
+      //data = baseTexture.getFrameTextureData(0);
+    }
+    else {
+      ResourceLocation resourcelocation = new ResourceLocation(baseTexture.getIconName());
+      data = backupLoadTexture(resourcelocation, manager);
+    }
+
+    // go over the base texture and color it
+    for(int mipmap = 0; mipmap < data.length; mipmap++) {
+      if(data[mipmap] == null)
+        continue;
+      for(int pxCoord = 0; pxCoord < data[mipmap].length; pxCoord++) {
+        // we're not working per pixel
+        // we take the information in the base texture to calculate the luminosity of the pixel
+        // and then color it accordingly with the materials color
+        data[mipmap][pxCoord] = colorPixel(data[mipmap][pxCoord], mipmap, pxCoord);
+      }
+    }
+
+    this.framesTextureData.add(data);
+
+    return false;
+  }
+
+  protected abstract int colorPixel(int pixel, int mipmap, int pxCoord);
+
+  // loads the base texture manually, same procedure as TextureMap
+  protected int[][] backupLoadTexture(ResourceLocation resourceLocation, IResourceManager resourceManager) {
+    ResourceLocation resourcelocation1 = this.completeResourceLocation(resourceLocation, 0);
+
+    try {
+      IResource iresource = resourceManager.getResource(resourcelocation1);
+      BufferedImage[] abufferedimage = new BufferedImage[1 + 4];
+      abufferedimage[0] = TextureUtil.readBufferedImage(iresource.getInputStream());
+
+      this.width = abufferedimage[0].getWidth();
+      this.height = abufferedimage[0].getHeight();
+
+      int[][] aint = new int[abufferedimage.length][];
+      for (int k = 0; k < abufferedimage.length; ++k)
+      {
+        BufferedImage bufferedimage = abufferedimage[k];
+
+        if (bufferedimage != null)
+        {
+          aint[k] = new int[bufferedimage.getWidth() * bufferedimage.getHeight()];
+          bufferedimage.getRGB(0, 0, bufferedimage.getWidth(), bufferedimage.getHeight(), aint[k], 0, bufferedimage.getWidth());
+        }
+      }
+
+      return aint;
+    }
+    catch (RuntimeException runtimeexception)
+    {
+      Log.error("Unable to parse metadata from " + resourcelocation1, runtimeexception);
+    }
+    catch (IOException ioexception1)
+    {
+      Log.error("Using missing texture, unable to load " + resourcelocation1, ioexception1);
+    }
+
+    return new int[0][];
+  }
+
+  private ResourceLocation completeResourceLocation(ResourceLocation location, int p_147634_2_)
+  {
+    if(p_147634_2_ == 0)
+      return new ResourceLocation(location.getResourceDomain(), String.format("%s/%s%s", "textures", location.getResourcePath(), ".png"));
+
+    return new ResourceLocation(location.getResourceDomain(), String.format("%s/mipmaps/%s.%d%s", "textures", location.getResourcePath(), p_147634_2_, ".png"));
+  }
+
+  // borrowed from Shadows of Physis
+  // Thanks TTFTCUTS! :)
+  public static int getPerceptualBrightness(int col) {
+    double r = red(col) / 255.0;
+    double g = green(col) / 255.0;
+    double b = blue(col) / 255.0;
+
+    return getPerceptualBrightness(r,g,b);
+  }
+
+  public static int getPerceptualBrightness(double r, double g, double b) {
+
+    double brightness = Math.sqrt(0.241 * r*r + 0.691 * g*g + 0.068 * b*b);
+
+    return (int)(brightness*255);
+  }
+
+  public static int compose(int r, int g, int b, int a) {
+    int rgb = a;
+    rgb = (rgb << 8) + r;
+    rgb = (rgb << 8) + g;
+    rgb = (rgb << 8) + b;
+    return rgb;
+  }
+
+  public static int compose(int[] col) {
+    int r = col[0];
+    int g = col[1];
+    int b = col[2];
+    int a = col.length > 3 ? col[3] : 255;
+    return compose(r,g,b,a);
+  }
+
+  public static int alpha(int c) {
+    return (c >> 24) & 0xFF;
+  }
+
+  public static int red(int c) {
+    return (c >> 16) & 0xFF;
+  }
+
+  public static int green(int c) {
+    return (c >> 8) & 0xFF;
+  }
+
+  public static int blue(int c) {
+    return (c) & 0xFF;
+  }
+
+  protected static int mult(int c1, int c2) {
+    return (int)(c1 * (c2/255.0));
+  }
+}
--- src/main/java/tconstruct/test/SimpleColoredTexture.java
+++ src/main/java/tconstruct/test/SimpleColoredTexture.java
@@ -1,121 +1,42 @@
 package tconstruct.test;
 
-import com.google.common.collect.Lists;
-
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureUtil;
-import net.minecraft.client.resources.IResource;
-import net.minecraft.client.resources.IResourceManager;
-import net.minecraft.client.resources.data.AnimationMetadataSection;
-import net.minecraft.client.resources.data.TextureMetadataSection;
-import net.minecraft.util.ResourceLocation;
-
-import sun.net.ResourceManager;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import tconstruct.library.tinkering.Material;
-import tconstruct.library.utils.Log;
 
-public class SimpleColoredTexture extends TextureAtlasSprite {
+public class SimpleColoredTexture extends AbstractColoredTexture {
   private final Material material;
-  private final TextureAtlasSprite baseTexture;
 
   protected SimpleColoredTexture(Material material, TextureAtlasSprite baseTexture,
                                  String spriteName) {
-    super(spriteName);
+    super(baseTexture, spriteName);
 
     this.material = material;
-    this.baseTexture = baseTexture;
   }
 
-  @Override
-  public boolean hasCustomLoader(IResourceManager manager, ResourceLocation location) {
-    return true;
-  }
+  protected int colorPixel(int pixel, int mipmap, int pxCoord) {
+    int a = alpha(pixel);
+    if(a == 0)
+      return pixel;
 
-  @Override
-  public boolean load(IResourceManager manager, ResourceLocation location) {
-    this.framesTextureData = Lists.newArrayList();
-    this.frameCounter = 0;
-    this.tickCounter = 0;
+    int brightness = getPerceptualBrightness(pixel);
+    int c = material.colorMid;
+    if(brightness < 128)
+      c = material.colorLow;
+    else if(brightness > 192)
+      c = material.colorHigh;
 
-    // get the base texture to work on
-    int[][] data;
-    if(baseTexture.getFrameCount() > 0) {
-      this.copyFrom(baseTexture);
-      data = baseTexture.getFrameTextureData(0);
-    }
-    else {
-      data = backupLoadTexture(manager);
-    }
 
-    // go over the base texture and color it
-    for(int mipmap = 0; mipmap < data.length; mipmap++) {
-      if(data[mipmap] == null)
-        continue;
-      for(int pxCoord = 0; pxCoord < data[mipmap].length; pxCoord++) {
-        // we're not working per pixel
-        // we take the information in the base texture to calculate the luminosity of the pixel
-        // and then color it accordingly with the materials color
-        data[mipmap][pxCoord] = colorPixel(data[mipmap][pxCoord]);
-      }
-    }
+    // multiply in the color
+    int r = red(c);
+    int b = blue(c);
+    int g = green(c);
 
-    this.framesTextureData.add(data);
+    r = mult(r,brightness) & 0xff;
+    g = mult(g,brightness) & 0xff;
+    b = mult(b,brightness) & 0xff;
 
-    return false;
-  }
-
-  protected int colorPixel(int pixel) {
-    return material.colorMid | (pixel & (255 << 24));
-  }
-
-  // loads the base texture manually, same procedure as TextureMap
-  protected int[][] backupLoadTexture(IResourceManager resourceManager) {
-    ResourceLocation resourcelocation = new ResourceLocation(baseTexture.getIconName());
-    ResourceLocation resourcelocation1 = this.completeResourceLocation(resourcelocation, 0);
-
-    try {
-      IResource iresource = resourceManager.getResource(resourcelocation1);
-      BufferedImage[] abufferedimage = new BufferedImage[1 + 4];
-      abufferedimage[0] = TextureUtil.readBufferedImage(iresource.getInputStream());
-
-      this.width = abufferedimage[0].getWidth();
-      this.height = abufferedimage[0].getHeight();
-
-      int[][] aint = new int[abufferedimage.length][];
-      for (int k = 0; k < abufferedimage.length; ++k)
-      {
-        BufferedImage bufferedimage = abufferedimage[k];
-
-        if (bufferedimage != null)
-        {
-          aint[k] = new int[bufferedimage.getWidth() * bufferedimage.getHeight()];
-          bufferedimage.getRGB(0, 0, bufferedimage.getWidth(), bufferedimage.getHeight(), aint[k], 0, bufferedimage.getWidth());
-        }
-      }
-
-      return aint;
-    }
-    catch (RuntimeException runtimeexception)
-    {
-      Log.error("Unable to parse metadata from " + resourcelocation1, runtimeexception);
-    }
-    catch (IOException ioexception1)
-    {
-      Log.error("Using missing texture, unable to load " + resourcelocation1, ioexception1);
-    }
-
-    return new int[0][];
-  }
-
-  private ResourceLocation completeResourceLocation(ResourceLocation location, int p_147634_2_)
-  {
-    if(p_147634_2_ == 0)
-      return new ResourceLocation(location.getResourceDomain(), String.format("%s/%s%s", "textures", location.getResourcePath(), ".png"));
-
-    return new ResourceLocation(location.getResourceDomain(), String.format("%s/mipmaps/%s.%d%s", "textures", location.getResourcePath(), p_147634_2_, ".png"));
+    // put it back together
+    return compose(r,g,b,a);
   }
 }
