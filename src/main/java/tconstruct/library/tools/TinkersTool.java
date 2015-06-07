5152c2f9ba7cb444df237fb88f9fb2b988535f98
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-06-07 14:46:15 +0200

    Remove the subclasses of TinkerTool in favor of a flat approach using the categories

--- src/main/java/tconstruct/library/tools/TinkersTool.java
+++ src/main/java/tconstruct/library/tools/TinkersTool.java
@@ -1,15 +1,26 @@
 package tconstruct.library.tools;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+import java.util.List;
 
 import tconstruct.library.TinkerRegistry;
 import tconstruct.library.tinkering.Category;
-import tconstruct.library.tinkering.PartMaterialWrapper;
+import tconstruct.library.tinkering.Material;
+import tconstruct.library.tinkering.PartMaterialType;
 import tconstruct.library.tinkering.TinkersItem;
+import tconstruct.library.utils.TagUtil;
+import tconstruct.library.utils.Tags;
+import tconstruct.library.utils.ToolBuilder;
+import tconstruct.library.utils.ToolTagUtil;
 import tconstruct.library.utils.TooltipBuilder;
+import tconstruct.tools.TinkerMaterials;
 
 /**
  * Intermediate abstraction layer for all tools/melee weapons. This class has all the callbacks for blocks and enemies
@@ -17,7 +28,7 @@ import tconstruct.library.utils.TooltipBuilder;
  */
 public abstract class TinkersTool extends TinkersItem {
 
-  public TinkersTool(PartMaterialWrapper... requiredComponents) {
+  public TinkersTool(PartMaterialType... requiredComponents) {
     super(requiredComponents);
 
     TinkerRegistry.addTool(this);
@@ -40,10 +51,41 @@ public abstract class TinkersTool extends TinkersItem {
     TooltipBuilder info = new TooltipBuilder(stack);
 
     info.addDurability();
-    info.addHarvestLevel();
-    info.addMiningSpeed();
-    info.addAttack();
+    if(hasCategory(Category.HARVEST)) {
+      info.addHarvestLevel();
+      info.addMiningSpeed();
+    }
+    if(hasCategory(Category.WEAPON)) {
+      info.addAttack();
+    }
 
     return info.getTooltip();
+  }
+
+  @Override
+  protected NBTTagCompound buildTag(Material[] materials) {
+    // assumse a simple Head + Handle tool
+    return ToolBuilder.buildSimpleTool(materials[0], materials[1]);
+  }
+
+  // Creative tab items
+
+
+  @Override
+  public void getSubItems(Item itemIn, CreativeTabs tab, List subItems) {
+    for (Material head : TinkerRegistry.getAllMaterials()) {
+      subItems.add(buildItem(new Material[]{head, TinkerMaterials.wood, TinkerMaterials.stone}));
+    }
+  }
+
+  @Override
+  public int getHarvestLevel(ItemStack stack, String toolClass) {
+    if (this.getToolClasses(stack).contains(toolClass)) {
+      NBTTagCompound tag = TagUtil.getToolTag(stack);
+      if (tag != null) {
+        return ToolTagUtil.getHarvestLevel(tag);
+      }
+    }
+    return super.getHarvestLevel(stack, toolClass);
   }
 }
