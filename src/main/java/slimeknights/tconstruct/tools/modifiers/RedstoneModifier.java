beac7ca776775730fe63bb56a64fb516acc9599a
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-08-24 14:34:42 +0200

    New experimental redstone modifier implementation - speed is solely based on base stats

--- src/main/java/slimeknights/tconstruct/tools/modifiers/RedstoneModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/RedstoneModifier.java
@@ -4,9 +4,11 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MathHelper;
 
 import slimeknights.tconstruct.library.modifiers.ModifierAspect;
 import slimeknights.tconstruct.library.modifiers.ModifierNBT;
+import slimeknights.tconstruct.library.tools.ToolNBT;
 import slimeknights.tconstruct.library.utils.TagUtil;
 import slimeknights.tconstruct.library.utils.Tags;
 import slimeknights.tconstruct.library.modifiers.Modifier;
@@ -35,10 +37,29 @@ public class RedstoneModifier extends Modifier {
   public void applyEffect(NBTTagCompound rootCompound, NBTTagCompound modifierTag) {
     ModifierNBT.IntegerNBT data = ModifierNBT.readInteger(modifierTag);
 
-    // todo: This is a TEST IMPLEMENTATION!
+    ToolNBT toolData = TagUtil.getOriginalToolStats(rootCompound);
+    float speed = toolData.speed;
+    int level = data.current / max;
+    for(int count = data.current; count > 0; count--) {
+      if(speed <= 10f) {
+        // linear scaling from 0.08 to 0.06 per piece till 10 miningspeed
+        speed += 0.08f - 0.02f * speed/10f;
+      }
+      else if(speed <= 20f) {
+        speed += 0.06f - 0.04 * speed/20f;
+      }
+      else {
+        speed += 0.01;
+      }
+    }
+
+    // each full level gives a flat 0.1 bonus, not influenced by dimishing returns
+    speed += level * 0.1f;
+
+    // save it to the tool
     NBTTagCompound tag = TagUtil.getToolTag(rootCompound);
-    float speed = tag.getFloat(Tags.MININGSPEED);
-    speed += data.current / 10f;
+    speed -= toolData.speed;
+    speed += tag.getFloat(Tags.MININGSPEED);
     tag.setFloat(Tags.MININGSPEED, speed);
   }
 }
