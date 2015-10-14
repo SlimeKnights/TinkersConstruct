ed78dbee744be63abc3cc9521df234793ed0e219
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-10-15 01:00:27 +0200

    Sanity check for modifiers on startup

--- src/main/java/slimeknights/tconstruct/debug/TinkerDebug.java
+++ src/main/java/slimeknights/tconstruct/debug/TinkerDebug.java
@@ -2,16 +2,24 @@ package slimeknights.tconstruct.debug;
 
 import com.google.common.eventbus.Subscribe;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
+import org.apache.logging.log4j.Logger;
+
 import mantle.pulsar.pulse.Pulse;
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.Util;
+import slimeknights.tconstruct.library.modifiers.IModifier;
 
 // todo: deactivate by default
 @Pulse(id=TinkerDebug.PulseId, description = "Debug utilities")
 public class TinkerDebug {
   public static final String PulseId = "TinkerDebug";
+  static final Logger log = Util.getLogger(PulseId);
 
   @Subscribe
   public void postInit(FMLPostInitializationEvent event) {
@@ -26,5 +34,20 @@ public class TinkerDebug {
     event.registerServerCommand(new DumpMaterialTest());
     event.registerServerCommand(new FindBestTool());
     event.registerServerCommand(new DamageTool());
+
+    sanityCheck();
+  }
+
+  public static void sanityCheck() {
+    // check all modifiers if they can be applied
+    for(IModifier modifier : TinkerRegistry.getAllModifiers()) {
+      try {
+        modifier.matches(new ItemStack[] {new ItemStack(Items.stick)});
+        modifier.matches(new ItemStack[1]);
+      } catch(Exception e) {
+        log.error("Caught exception in modifier " + modifier.getIdentifier());
+        log.error(e);
+      }
+    }
   }
 }
