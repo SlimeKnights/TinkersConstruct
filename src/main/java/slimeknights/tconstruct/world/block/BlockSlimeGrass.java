87658cfc47ea2d5e7c2b49d78934a8190ad8bc28
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-10-25 19:22:37 +0100

    Slimegrass spreads and returns the correct dirt on harvesting

--- src/main/java/slimeknights/tconstruct/world/block/BlockSlimeGrass.java
+++ src/main/java/slimeknights/tconstruct/world/block/BlockSlimeGrass.java
@@ -1,10 +1,13 @@
 package slimeknights.tconstruct.world.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockGrass;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
@@ -19,6 +22,7 @@ import java.util.Random;
 
 import slimeknights.mantle.block.EnumBlock;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.world.TinkerWorld;
 import slimeknights.tconstruct.world.client.SlimeColorizer;
 
 public class BlockSlimeGrass extends BlockGrass {
@@ -28,18 +32,50 @@ public class BlockSlimeGrass extends BlockGrass {
     this.setCreativeTab(TinkerRegistry.tabWorld);
   }
 
+  @SideOnly(Side.CLIENT)
+  @Override
+  public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
+    for(DirtType type : DirtType.values()) {
+      list.add(new ItemStack(this, 1, type.meta));
+    }
+  }
+
   @Override
   public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
     // todo: grow slime thingies :D
     super.grow(worldIn, rand, pos, state);
   }
 
-
-  @SideOnly(Side.CLIENT)
   @Override
-  public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
-    for(DirtType type : DirtType.values()) {
-      list.add(new ItemStack(this, 1, type.meta));
+  public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+    if(worldIn.isRemote) {
+      return;
+    }
+
+    if (worldIn.getLightFromNeighbors(pos.up()) < 4 && worldIn.getBlockState(pos.up()).getBlock().getLightOpacity(worldIn, pos.up()) > 2)
+    {
+      // convert grass back to dirt of the corresponding type
+      worldIn.setBlockState(pos, getDirtState(state));
+    }
+    else
+    {
+      // spread to surrounding blocks
+      if (worldIn.getLightFromNeighbors(pos.up()) >= 9)
+      {
+        for (int i = 0; i < 4; ++i)
+        {
+          BlockPos pos1 = pos.add(rand.nextInt(3) - 1, rand.nextInt(5) - 3, rand.nextInt(3) - 1);
+          Block block = worldIn.getBlockState(pos1.up()).getBlock();
+          IBlockState state1 = worldIn.getBlockState(pos1);
+
+          if(worldIn.getLightFromNeighbors(pos1.up()) >= 4 && block.getLightOpacity(worldIn, pos1.up()) <= 2) {
+            IBlockState newState = getStateFromDirt(state1);
+            if(newState != null) {
+              worldIn.setBlockState(pos1, newState);
+            }
+          }
+        }
+      }
     }
   }
 
@@ -60,7 +96,51 @@ public class BlockSlimeGrass extends BlockGrass {
 
   @Override
   public int damageDropped(IBlockState state) {
-    return getMetaFromState(state);
+    DirtType type = (DirtType) state.getValue(TYPE);
+    if(type == DirtType.VANILLA)
+      return 0;
+
+    return ((BlockSlimeDirt.DirtType)getDirtState(state).getValue(BlockSlimeDirt.TYPE)).getMeta();
+  }
+
+  @Override
+  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+    return Item.getItemFromBlock(getDirtState(state).getBlock());
+  }
+
+  /** Returns the blockstate for the dirt underneath the grass */
+  protected IBlockState getDirtState(IBlockState grassState) {
+    DirtType type = (DirtType) grassState.getValue(TYPE);
+    switch(type) {
+      case VANILLA:
+        return Blocks.dirt.getDefaultState();
+      case GREEN:
+        return TinkerWorld.slimeDirt.getStateFromMeta(BlockSlimeDirt.DirtType.GREEN.getMeta());
+      case BLUE:
+        return TinkerWorld.slimeDirt.getStateFromMeta(BlockSlimeDirt.DirtType.BLUE.getMeta());
+    }
+    return Blocks.dirt.getDefaultState();
+  }
+
+  /** Returns the grass blockstate for the given dirt type or null */
+  protected IBlockState getStateFromDirt(IBlockState dirtState) {
+    // vanilla dirt?
+    if(dirtState.getBlock() == Blocks.dirt && dirtState.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT) {
+      return this.getDefaultState().withProperty(TYPE, DirtType.VANILLA);
+    }
+    // slimedirt block?
+    if(dirtState.getBlock() == TinkerWorld.slimeDirt) {
+      // green slimedirt
+      if(dirtState.getValue(BlockSlimeDirt.TYPE) == BlockSlimeDirt.DirtType.GREEN) {
+        return this.getDefaultState().withProperty(TYPE, DirtType.GREEN);
+      }
+      // blue slimedirt
+      else if(dirtState.getValue(BlockSlimeDirt.TYPE) == BlockSlimeDirt.DirtType.BLUE) {
+        return this.getDefaultState().withProperty(TYPE, DirtType.BLUE);
+      }
+    }
+
+    return null;
   }
 
   @Override
@@ -75,7 +155,7 @@ public class BlockSlimeGrass extends BlockGrass {
       z = tmp;
     }
 
-    return SlimeColorizer.getColor(x,z);
+    return SlimeColorizer.getColor(x, z);
   }
 
   public enum DirtType implements IStringSerializable, EnumBlock.IEnumMeta {
