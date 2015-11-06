2a799258aca3a39f2863f3afcc18def1458bc355
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-11-06 16:48:15 +0100

    Change aridiculous to also affect damage and to also take wetness of the biome into account

--- src/main/java/slimeknights/tconstruct/tools/modifiers/TraitAridiculous.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/TraitAridiculous.java
@@ -1,5 +1,6 @@
 package slimeknights.tconstruct.tools.modifiers;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -17,7 +18,18 @@ public class TraitAridiculous extends AbstractTrait {
   public void miningSpeed(ItemStack tool, PlayerEvent.BreakSpeed event) {
     BiomeGenBase biome = event.entityPlayer.worldObj.getBiomeGenForCoords(event.pos);
     // speedup or slowdown depending on biome temperature. hotter areas are much faster
-    double coeff = ((Math.pow(1.25, 3d*biome.temperature) - 1.25d) / 10d);
+    float coeff = calcAridiculousness(biome) / 10f; // /10 = 10% for a coeff of 1. But can be bigger.
     event.newSpeed += event.originalSpeed * coeff;
+  }
+
+  @Override
+  public float onHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
+    BiomeGenBase biome = player.worldObj.getBiomeGenForCoords(player.getPosition());
+    float extraDamage = 2f * calcAridiculousness(biome);
+    return extraDamage + super.onHit(tool, player, target, damage, newDamage, isCritical);
+  }
+
+  protected float calcAridiculousness(BiomeGenBase biome) {
+    return (float) (Math.pow(1.25, 3d * (biome.temperature - biome.rainfall)) - 1.25d);
   }
 }
