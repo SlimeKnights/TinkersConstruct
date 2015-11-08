36ad70c297e544a2918b1284ad4035fea0b76cbb
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-11-08 19:21:40 +0100

    Change Aridiculoous to be affected by rain

--- src/main/java/slimeknights/tconstruct/tools/modifiers/TraitAridiculous.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/TraitAridiculous.java
@@ -2,12 +2,17 @@ package slimeknights.tconstruct.tools.modifiers;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 
+/**
+ * Increases mining speed and damage in hot/dry areas, decreases a bit in dry areas
+ */
 public class TraitAridiculous extends AbstractTrait {
 
   public TraitAridiculous() {
@@ -16,20 +21,20 @@ public class TraitAridiculous extends AbstractTrait {
 
   @Override
   public void miningSpeed(ItemStack tool, PlayerEvent.BreakSpeed event) {
-    BiomeGenBase biome = event.entityPlayer.worldObj.getBiomeGenForCoords(event.pos);
     // speedup or slowdown depending on biome temperature. hotter areas are much faster
-    float coeff = calcAridiculousness(biome) / 10f; // /10 = 10% for a coeff of 1. But can be bigger.
+    float coeff = calcAridiculousness(event.entityPlayer.worldObj, event.pos) / 10f; // /10 = 10% for a coeff of 1. But can be bigger.
     event.newSpeed += event.originalSpeed * coeff;
   }
 
   @Override
   public float onHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
-    BiomeGenBase biome = player.worldObj.getBiomeGenForCoords(player.getPosition());
-    float extraDamage = 2f * calcAridiculousness(biome);
+    float extraDamage = 2f * calcAridiculousness(player.worldObj, player.getPosition());
     return extraDamage + super.onHit(tool, player, target, damage, newDamage, isCritical);
   }
 
-  protected float calcAridiculousness(BiomeGenBase biome) {
-    return (float) (Math.pow(1.25, 3d * (biome.temperature - biome.rainfall)) - 1.25d);
+  protected float calcAridiculousness(World world, BlockPos pos) {
+    BiomeGenBase biome = world.getBiomeGenForCoords(pos);
+    float rain = world.isRaining() ? biome.getFloatRainfall()/2f : 0f;
+    return (float) (Math.pow(1.25, 3d * (0.5f + biome.temperature - biome.rainfall)) - 1.25d) - rain;
   }
 }
