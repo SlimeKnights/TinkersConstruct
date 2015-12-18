6a0b3ff77b629e045b86dcef63a8fa8e37d69a04
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-12-18 20:21:23 +0100

    Fix sharpening kit not showing up in creative

--- src/main/java/slimeknights/tconstruct/tools/item/SharpeningKit.java
+++ src/main/java/slimeknights/tconstruct/tools/item/SharpeningKit.java
@@ -1,10 +1,13 @@
 package slimeknights.tconstruct.tools.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import java.util.List;
 
+import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.Util;
 import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.materials.ToolMaterialStats;
@@ -14,6 +17,14 @@ public class SharpeningKit extends ToolPart {
 
   public SharpeningKit() {
     super(Material.VALUE_Shard*3);
+  }
+
+  @Override
+  public void getSubItems(Item itemIn, CreativeTabs tab, List subItems) {
+    // this adds a variant of each material to the creative menu
+    for(Material mat : TinkerRegistry.getAllMaterials()) {
+      subItems.add(getItemstackWithMaterial(mat));
+    }
   }
 
   @Override
