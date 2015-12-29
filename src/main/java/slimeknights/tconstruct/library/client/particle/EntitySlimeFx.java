1485747f61f9ccfc8e6e08b058dfd384d6c4f05b
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2015-12-29 13:47:36 +0100

    Spawn correct slime particle for blueslime

---
+++ src/main/java/slimeknights/tconstruct/library/client/particle/EntitySlimeFx.java
@@ -0,0 +1,16 @@
+package slimeknights.tconstruct.library.client.particle;
+
+import net.minecraft.client.particle.EntityBreakingFX;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+
+public class EntitySlimeFx extends EntityBreakingFX {
+
+  public EntitySlimeFx(World worldIn, double posXIn, double posYIn, double posZIn, Item item, int meta) {
+    super(worldIn, posXIn, posYIn, posZIn, item, meta);
+  }
+
+  public EntitySlimeFx(World worldIn, double posXIn, double posYIn, double posZIn, double xSpeedIn, double ySpeedIn, double zSpeedIn, Item item, int meta) {
+    super(worldIn, posXIn, posYIn, posZIn, xSpeedIn, ySpeedIn, zSpeedIn, item, meta);
+  }
+}
--- src/main/java/slimeknights/tconstruct/world/entity/EntityBlueSlime.java
+++ src/main/java/slimeknights/tconstruct/world/entity/EntityBlueSlime.java
@@ -1,5 +1,7 @@
 package slimeknights.tconstruct.world.entity;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.EntityBreakingFX;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.item.Item;
@@ -8,6 +10,7 @@ import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
+import slimeknights.tconstruct.library.client.particle.EntitySlimeFx;
 import slimeknights.tconstruct.shared.TinkerCommons;
 import slimeknights.tconstruct.shared.TinkerFluids;
 import slimeknights.tconstruct.world.TinkerWorld;
@@ -44,18 +47,18 @@ public class EntityBlueSlime extends EntitySlime {
 
   @Override
   protected boolean spawnCustomParticles() {
-    int i = this.getSlimeSize();
-    for (int j = 0; j < i * 8; ++j)
-    {
-      float f = this.rand.nextFloat() * (float)Math.PI * 2.0F;
-      float f1 = this.rand.nextFloat() * 0.5F + 0.5F;
-      float f2 = MathHelper.sin(f) * (float)i * 0.5F * f1;
-      float f3 = MathHelper.cos(f) * (float)i * 0.5F * f1;
-      World world = this.worldObj;
-      EnumParticleTypes enumparticletypes = EnumParticleTypes.VILLAGER_HAPPY;
-      double d0 = this.posX + (double)f2;
-      double d1 = this.posZ + (double)f3;
-      world.spawnParticle(enumparticletypes, d0, this.getEntityBoundingBox().minY, d1, 0.0D, 0.0D, 0.0D, new int[0]);
+    if(this.worldObj.isRemote) {
+      int i = this.getSlimeSize();
+      for(int j = 0; j < i * 8; ++j) {
+        float f = this.rand.nextFloat() * (float) Math.PI * 2.0F;
+        float f1 = this.rand.nextFloat() * 0.5F + 0.5F;
+        float f2 = MathHelper.sin(f) * (float) i * 0.5F * f1;
+        float f3 = MathHelper.cos(f) * (float) i * 0.5F * f1;
+        double d0 = this.posX + (double) f2;
+        double d1 = this.posZ + (double) f3;
+        double d2 = this.getEntityBoundingBox().minY;
+        Minecraft.getMinecraft().effectRenderer.addEffect(new EntitySlimeFx(this.worldObj, d0, d2, d1, TinkerCommons.matSlimeBallBlue.getItem(), TinkerCommons.matSlimeBallBlue.getItemDamage()));
+      }
     }
     return true;
   }
