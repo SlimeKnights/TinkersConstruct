7ecb1fc4e223cb6e2fe3d13abda4a628078d11fe
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-01-11 18:11:32 +0100

    Fix crude being inverse. Now deals bonus damage to unarmed

--- src/main/java/slimeknights/tconstruct/tools/modifiers/traits/TraitCrude.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/traits/TraitCrude.java
@@ -15,7 +15,7 @@ public class TraitCrude extends AbstractTrait {
   @Override
   public float onHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
     boolean hasArmor = target.getTotalArmorValue() > 0;
-    if(hasArmor) {
+    if(!hasArmor) {
       // 10% damage boost against unarmed targets!
       return newDamage + damage * 0.1f;
     }
