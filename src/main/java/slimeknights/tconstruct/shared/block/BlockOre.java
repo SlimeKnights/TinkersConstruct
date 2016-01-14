6bfe29b5c73d7e706e3cd7ac0c8f98cd44036db7
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-01-14 17:44:41 +0100

    Add xp drops to cobalt and ardite ore

--- src/main/java/slimeknights/tconstruct/shared/block/BlockOre.java
+++ src/main/java/slimeknights/tconstruct/shared/block/BlockOre.java
@@ -2,10 +2,16 @@ package slimeknights.tconstruct.shared.block;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 import slimeknights.mantle.block.EnumBlock;
 import slimeknights.tconstruct.library.TinkerRegistry;
@@ -25,6 +31,12 @@ public class BlockOre extends EnumBlock<BlockOre.OreTypes> {
     setHardness(10f);
     setHarvestLevel("pickaxe", HarvestLevels.COBALT);
     setCreativeTab(TinkerRegistry.tabWorld);
+  }
+
+  @Override
+  public int getExpDrop(IBlockAccess world, BlockPos pos, int fortune) {
+    Random rand = world instanceof World ? ((World)world).rand : new Random();
+    return MathHelper.getRandomIntegerInRange(rand, 4, 6);
   }
 
   @SideOnly(Side.CLIENT)
