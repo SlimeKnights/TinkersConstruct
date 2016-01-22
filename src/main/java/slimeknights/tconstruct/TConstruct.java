ba17599ad0b66a6371346f72c713d006405b08e4
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-01-22 21:23:56 +0100

    Version bump to 2.0.2 and requires mantle 0.7

--- src/main/java/slimeknights/tconstruct/TConstruct.java
+++ src/main/java/slimeknights/tconstruct/TConstruct.java
@@ -51,7 +51,7 @@ import slimeknights.tconstruct.world.TinkerWorld;
     version = TConstruct.modVersion,
     guiFactory = "slimeknights.tconstruct.common.config.ConfigGui$ConfigGuiFactory",
     dependencies = "required-after:Forge@[11.15.0.1700,);"
-                   + "required-after:mantle@[1.8.8-0.6,)",
+                   + "required-after:mantle@[1.8.8-0.7,)",
     acceptedMinecraftVersions = "1.8.9")
 public class TConstruct {
 
