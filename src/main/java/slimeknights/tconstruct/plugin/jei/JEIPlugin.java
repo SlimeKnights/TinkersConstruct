32ed1e9bafb4ba72e3ccd38c516b824ce48ee878
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-02-07 13:08:10 +0100

    Update JEI and Mantle for the GUI fix with JEI

--- src/main/java/slimeknights/tconstruct/plugin/jei/JEIPlugin.java
+++ src/main/java/slimeknights/tconstruct/plugin/jei/JEIPlugin.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IItemRegistry;
 import mezz.jei.api.IJeiHelpers;
+import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.IRecipeRegistry;
@@ -93,5 +94,11 @@ public class JEIPlugin implements IModPlugin {
   }
 
   @Override
+  @Deprecated
   public void onRecipeRegistryAvailable(IRecipeRegistry recipeRegistry) {}
+
+  @Override
+  public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+
+  }
 }
