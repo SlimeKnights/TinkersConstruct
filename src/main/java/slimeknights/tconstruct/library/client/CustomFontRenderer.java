de7c32a48846c95daf14ddeafdd138aedb8179f9
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-02-11 19:44:48 +0100

    Fix custom color in text not being conserved across linebreaks

--- src/main/java/slimeknights/tconstruct/library/client/CustomFontRenderer.java
+++ src/main/java/slimeknights/tconstruct/library/client/CustomFontRenderer.java
@@ -1,11 +1,17 @@
 package slimeknights.tconstruct.library.client;
 
+import com.google.common.collect.Lists;
+
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -23,6 +29,61 @@ public class CustomFontRenderer extends FontRenderer {
 
   public CustomFontRenderer(GameSettings gameSettingsIn, ResourceLocation location, TextureManager textureManagerIn) {
     super(gameSettingsIn, location, textureManagerIn, true);
+  }
+
+  @Override
+  public List<String> listFormattedStringToWidth(String str, int wrapWidth) {
+    return Arrays.<String>asList(this.wrapFormattedStringToWidth(str, wrapWidth).split("\n"));
+  }
+
+  protected String wrapFormattedStringToWidth(String str, int wrapWidth) {
+    int i = this.sizeStringToWidth(str, wrapWidth);
+
+    if (str.length() <= i)
+    {
+      return str;
+    }
+    else
+    {
+      String s = str.substring(0, i);
+      char c0 = str.charAt(i);
+      boolean flag = c0 == 32 || c0 == 10;
+      String s1 = getCustomFormatFromString(s) + str.substring(i + (flag ? 1 : 0));
+      return s + "\n" + this.wrapFormattedStringToWidth(s1, wrapWidth);
+    }
+  }
+
+  public static String getCustomFormatFromString(String text) {
+    String s = "";
+    int i = 0;
+    int j = text.length();
+
+    while ((i < j-1))
+    {
+      char c = text.charAt(i);
+      // vanilla formatting
+      if(c == 167) {
+
+        char c0 = text.charAt(i + 1);
+
+        if(c0 >= 48 && c0 <= 57 || c0 >= 97 && c0 <= 102 || c0 >= 65 && c0 <= 70) {
+          s = "\u00a7" + c0;
+          i++;
+        }
+        else if(c0 >= 107 && c0 <= 111 || c0 >= 75 && c0 <= 79 || c0 == 114 || c0 == 82) {
+          s = s + "\u00a7" + c0;
+          i++;
+        }
+      }
+      // custom formatting
+      else if((int) c >= CustomFontColor.MARKER && (int) c <= CustomFontColor.MARKER + 0xFF) {
+        s = String.format("%s%s%s", c, text.charAt(i+1), text.charAt(i+2));
+        i += 2;
+      }
+      i++;
+    }
+
+    return s;
   }
 
   @Override
