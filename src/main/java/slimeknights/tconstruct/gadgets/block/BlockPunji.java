6d2923a87419618fcc48214082a728817df24066
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-03-13 19:54:05 +0100

    More punji work

--- src/main/java/slimeknights/tconstruct/gadgets/block/BlockPunji.java
+++ src/main/java/slimeknights/tconstruct/gadgets/block/BlockPunji.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
@@ -15,15 +16,23 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import scala.None;
 import slimeknights.tconstruct.library.TinkerRegistry;
 
 public class BlockPunji extends Block {
 
   public static final PropertyDirection FACING = PropertyDirection.create("facing");
-  public static final PropertyBool DUMMY = PropertyBool.create("dummy");
+  public static final PropertyEnum<ConnectionHorizontal> CON_HOR = PropertyEnum.create("connection_horizontal", ConnectionHorizontal.class);
+  public static final PropertyEnum<ConnectionDiagonal> CON_DIA = PropertyEnum.create("connection_diagonal", ConnectionDiagonal.class);
+  public static final PropertyEnum<ConnectionVertical> CON_VER = PropertyEnum.create("connection_vertical", ConnectionVertical.class);
+  //public static final PropertyBool NORTH = PropertyBool.create("north");
+  //public static final PropertyBool EAST = PropertyBool.create("east");
+  //public static final PropertyBool NORTHEAST = PropertyBool.create("northeast");
+  //public static final PropertyBool NORTHWEST = PropertyBool.create("northwest");
 
   public BlockPunji() {
     super(Material.plants);
@@ -38,7 +47,7 @@ public class BlockPunji extends Block {
 
   @Override
   protected BlockState createBlockState() {
-    return new BlockState(this, FACING);
+    return new BlockState(this, FACING, CON_HOR, CON_DIA, CON_VER);
   }
 
   /**
@@ -50,7 +59,7 @@ public class BlockPunji extends Block {
     }
     EnumFacing face = EnumFacing.values()[meta];
 
-    return this.getDefaultState().withProperty(FACING, face);
+    return this.getDefaultState().withProperty(FACING, face).withProperty(CON_HOR, ConnectionHorizontal.NONE).withProperty(CON_VER, ConnectionVertical.NONE);
   }
 
   /**
@@ -58,6 +67,80 @@ public class BlockPunji extends Block {
    */
   public int getMetaFromState(IBlockState state) {
     return state.getValue(FACING).ordinal();
+  }
+
+  @Override
+  public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+    ConnectionVertical ver = ConnectionVertical.NONE;
+    ConnectionDiagonal dia = ConnectionDiagonal.NONE;
+    ConnectionHorizontal hor = ConnectionHorizontal.NONE;
+    EnumFacing facing = state.getValue(FACING);
+
+    int off = -facing.ordinal() % 2;
+
+    EnumFacing face1 = EnumFacing.values()[(facing.ordinal() + 2) % 6];
+    EnumFacing face2 = EnumFacing.values()[(facing.ordinal() + 4 + off) % 6];
+
+    // North/East Connector
+    IBlockState north = worldIn.getBlockState(pos.offset(face1));
+    IBlockState east = worldIn.getBlockState(pos.offset(face2));
+    if(north.getBlock() == this && north.getValue(FACING) == facing) {
+      hor = ConnectionHorizontal.NORTH;
+    }
+    if(east.getBlock() == this && east.getValue(FACING) == facing) {
+      if(hor == ConnectionHorizontal.NORTH) {
+        hor = ConnectionHorizontal.BOTH;
+      }
+      else {
+        hor = ConnectionHorizontal.EAST;
+      }
+    }
+
+    // Diagonal connections
+    IBlockState northeast = worldIn.getBlockState(pos.offset(face1).offset(face2));
+    IBlockState southwest = worldIn.getBlockState(pos.offset(face1).offset(face2.getOpposite()));
+    if(northeast.getBlock() == this && northeast.getValue(FACING) == facing) {
+      dia = ConnectionDiagonal.NORTHEAST;
+    }
+    if(southwest.getBlock() == this && southwest.getValue(FACING) == facing) {
+      if(dia == ConnectionDiagonal.NORTHEAST) {
+        dia = ConnectionDiagonal.BOTH;
+      }
+      else {
+        dia = ConnectionDiagonal.SOUTHWEST;
+      }
+    }
+
+    // up/down model?
+    if(facing.getAxis() != EnumFacing.Axis.Y) {
+      for(EnumFacing dir : EnumFacing.Plane.HORIZONTAL.facings()) {
+        IBlockState stateDir = worldIn.getBlockState(pos.offset(dir));
+        if(stateDir.getBlock() == this) {
+          if(stateDir.getValue(FACING) == EnumFacing.DOWN) {
+            if(ver == ConnectionVertical.NONE) {
+              ver = ConnectionVertical.DOWN;
+            }
+            else {
+              ver = ConnectionVertical.BOTH;
+            }
+          }
+          if(stateDir.getValue(FACING) == EnumFacing.UP) {
+            if(ver == ConnectionVertical.NONE) {
+              ver = ConnectionVertical.UP;
+            }
+            else {
+              ver = ConnectionVertical.BOTH;
+            }
+          }
+        }
+      }
+    }
+
+    ver = ConnectionVertical.NONE;
+
+    return state.withProperty(CON_HOR, hor)
+                .withProperty(CON_DIA, dia)
+                .withProperty(CON_VER, ver);
   }
 
   /**
@@ -79,12 +162,12 @@ public class BlockPunji extends Block {
   public void setBlockBoundsBasedOnState(EnumFacing facing) {
     float h = 0.375f;
 
-    float xMin = 0.125F;
-    float xMax = 0.875F;
-    float zMin = 0.125F;
-    float zMax = 0.875F;
-    float yMin = 0.125F;
-    float yMax = 0.875F;
+    float xMin = 0.1875f;
+    float xMax = 1f - xMin;
+    float zMin = 0.1875f;
+    float zMax = 1f - zMin;
+    float yMin = 0.1875f;
+    float yMax = 1f - yMin;
 
     switch(facing) {
       case DOWN:
@@ -142,5 +225,41 @@ public class BlockPunji extends Block {
   @Override
   public boolean isFullCube() {
     return false;
+  }
+
+  private enum ConnectionHorizontal implements IStringSerializable {
+    NONE,
+    NORTH,
+    EAST,
+    BOTH;
+
+    @Override
+    public String getName() {
+      return this.toString();
+    }
+  }
+
+  private enum ConnectionDiagonal implements IStringSerializable {
+    NONE,
+    NORTHEAST,
+    SOUTHWEST,
+    BOTH;
+
+    @Override
+    public String getName() {
+      return this.toString();
+    }
+  }
+
+  private enum ConnectionVertical implements IStringSerializable {
+    NONE,
+    UP,
+    DOWN,
+    BOTH;
+
+    @Override
+    public String getName() {
+      return this.toString();
+    }
   }
 }
