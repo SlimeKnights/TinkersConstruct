dfc927e013d9c10b219d610ed8a8ef33f919d31e
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-04-24 17:27:00 +0200

    Properly prioritize held items over inventory when trying to find the matching itemstack for projectiles

--- src/main/java/slimeknights/tconstruct/library/tools/TinkerProjectileHandler.java
+++ src/main/java/slimeknights/tconstruct/library/tools/TinkerProjectileHandler.java
@@ -1,6 +1,7 @@
 package slimeknights.tconstruct.library.tools;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -48,8 +49,21 @@ public class TinkerProjectileHandler implements ITinkerProjectile, INBTSerializa
       return null;
     }
 
-    IItemHandler itemHandler = entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+    // try main and off hand first, because priority (yes they're also covered in the loop below.)
+    if(entity instanceof EntityLivingBase) {
+      ItemStack in = ((EntityLivingBase) entity).getHeldItemMainhand();
+      if(ToolCore.isEqualTinkersItem(in, parent) && (!damagedOnly || in.getItemDamage() > 0)) {
+        return in;
+      }
 
+      in = ((EntityLivingBase) entity).getHeldItemOffhand();
+      if(ToolCore.isEqualTinkersItem(in, parent) && (!damagedOnly || in.getItemDamage() > 0)) {
+        return in;
+      }
+    }
+
+
+    IItemHandler itemHandler = entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
 
     // find an itemstack that matches our input
     for(int i = 0; i < itemHandler.getSlots(); i++) {
