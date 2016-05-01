4c8c9270f74024691490bc3057925184e3685582
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-05-01 14:15:31 +0200

    Book model

--- src/main/java/slimeknights/tconstruct/common/item/ItemTinkerBook.java
+++ src/main/java/slimeknights/tconstruct/common/item/ItemTinkerBook.java
@@ -6,8 +6,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 
+import java.util.List;
+
+import slimeknights.mantle.util.LocUtils;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.book.TinkerBook;
 
@@ -15,11 +20,19 @@ public class ItemTinkerBook extends Item {
 
   public ItemTinkerBook() {
     this.setCreativeTab(TinkerRegistry.tabGeneral);
+    this.setMaxStackSize(1);
   }
 
   @Override
   public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
     TinkerBook.INSTANCE.openGui(itemStackIn);
     return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemStackIn);
+  }
+
+  @Override
+  public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+    if(I18n.canTranslate(super.getUnlocalizedName(stack) + ".tooltip")) {
+      tooltip.addAll(LocUtils.getTooltips(TextFormatting.GRAY.toString() + LocUtils.translateRecursive(super.getUnlocalizedName(stack) + ".tooltip")));
+    }
   }
 }
