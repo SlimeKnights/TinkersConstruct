7d0753f02d0dba39f0d864b7d6d1e4f8465e2501
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-06-13 20:56:25 +0200

    Fix NPE on word load

--- src/main/java/slimeknights/tconstruct/gadgets/tileentity/TileDryingRack.java
+++ src/main/java/slimeknights/tconstruct/gadgets/tileentity/TileDryingRack.java
@@ -63,7 +63,7 @@ public class TileDryingRack extends TileItemRack implements ITickable, ISidedInv
     if(slot == 0) {
       updateDryingTime();
     }
-    else {
+    else if(this.worldObj != null){
       // comparator update
       this.worldObj.notifyNeighborsOfStateChange(this.pos, this.getBlockType());
     }
