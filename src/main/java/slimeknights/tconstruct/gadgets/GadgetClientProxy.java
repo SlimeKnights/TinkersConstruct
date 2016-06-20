f13b608c50bfb8850454c3c552e28a83db7bc0e7
KnightMiner <kirbybat4774@gmail.com>
2016-06-19 22:09:47 -0500

    Add brownstone, a block that increases speed
    Based off of brownstone from 1.7.10, though it has a slightly different
    speed boost amount and more variants.
    
    It has a new recipe requiring sandstone, which as a result the textures
    were changed to sandstone based
    
    This commit also fixes an issue with magma slime grass losing its side
    texture when snowy

--- src/main/java/slimeknights/tconstruct/gadgets/GadgetClientProxy.java
+++ src/main/java/slimeknights/tconstruct/gadgets/GadgetClientProxy.java
@@ -29,9 +29,28 @@ public class GadgetClientProxy extends ClientProxy {
     registerItemModel(TinkerGadgets.rack);
 
     registerItemBlockMeta(TinkerGadgets.driedClay);
+    registerItemBlockMeta(TinkerGadgets.brownstone);
+
+    // slabs
     registerItemBlockMeta(TinkerGadgets.driedClaySlab);
+    registerItemBlockMeta(TinkerGadgets.brownstoneSlab);
+    registerItemBlockMeta(TinkerGadgets.brownstoneSlab2);
+
+    // stairs
     registerItemModel(TinkerGadgets.driedClayStairs);
     registerItemModel(TinkerGadgets.driedBrickStairs);
+    registerItemModel(TinkerGadgets.brownstoneStairsSmooth);
+    registerItemModel(TinkerGadgets.brownstoneStairsRough);
+    registerItemModel(TinkerGadgets.brownstoneStairsPaver);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrick);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrickCracked);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrickFancy);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrickSquare);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrickTriangle);
+    registerItemModel(TinkerGadgets.brownstoneStairsBrickSmall);
+    registerItemModel(TinkerGadgets.brownstoneStairsRoad);
+    registerItemModel(TinkerGadgets.brownstoneStairsTile);
+    registerItemModel(TinkerGadgets.brownstoneStairsCreeper);
 
     // Items
     registerItemModel(TinkerGadgets.slimeSling);
