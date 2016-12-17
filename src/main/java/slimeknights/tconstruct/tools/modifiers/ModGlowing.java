b8a58bf63cba7e9568dd2fed0b64d4f9a425a3aa
MightyCupcakes <gohydj2003@gmail.com>
2016-12-17 14:28:51 +0800

    Add a entity in water check to the glowing modifer
    With this commit, the glowing modifier will no longer trying to add glowing blocks around the entity if the said entity is in water. Fixes #2535

--- src/main/java/slimeknights/tconstruct/tools/modifiers/ModGlowing.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/ModGlowing.java
@@ -23,7 +23,7 @@ public class ModGlowing extends ModifierTrait {
     if(isSelected && !world.isRemote && !ToolHelper.isBroken(tool)) {
       BlockPos pos = entity.getPosition();
       // check light level at entity
-      if(world.getLight(pos) < 8) {
+      if(world.getLight(pos) < 8 && !entity.isInWater()) {
         for(BlockPos candidate : new BlockPos[]{pos, pos.up(), pos.north(), pos.east(), pos.south(), pos.west(), pos.down()}) {
           // addGlow tries all directions if the passed one doesn't work
           if(TinkerCommons.blockGlow.addGlow(world, candidate, EnumFacing.values()[random.nextInt(6)])) {
