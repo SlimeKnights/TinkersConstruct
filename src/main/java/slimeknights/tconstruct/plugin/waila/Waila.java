c993f78df27481500a1fd001760c9e1e94f94b79
KnightMiner <knightminer4@gmail.com>
2017-08-05 20:49:15 -0500

    Fix WAILA ID having the wrong case causing the plugin to fail (#3066)

--- src/main/java/slimeknights/tconstruct/plugin/waila/Waila.java
+++ src/main/java/slimeknights/tconstruct/plugin/waila/Waila.java
@@ -10,7 +10,7 @@ import slimeknights.mantle.pulsar.pulse.Pulse;
 @Pulse(id = Waila.PulseId, modsRequired = Waila.modid, defaultEnable = true)
 public class Waila {
 
-  public static final String modid = "Waila";
+  public static final String modid = "waila";
   public static final String PulseId = modid + "Integration";
 
   @Subscribe
