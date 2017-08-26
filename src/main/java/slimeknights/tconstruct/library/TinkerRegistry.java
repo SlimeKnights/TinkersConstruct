012b15076cd6c100f589ff1332d25897ffd8ab34
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2017-08-26 16:45:44 +0200

    Don't allow double registration of modifiers, overwriting already registered modifiers #3122

--- src/main/java/slimeknights/tconstruct/library/TinkerRegistry.java
+++ src/main/java/slimeknights/tconstruct/library/TinkerRegistry.java
@@ -463,6 +463,9 @@ public final class TinkerRegistry {
 
   /** Registers an alternate name for a modifier. This is used for multi-level modifiers/traits where multiple exist, but one specific is needed for access */
   public static void registerModifierAlias(IModifier modifier, String alias) {
+    if(modifiers.containsKey(alias)) {
+      throw new TinkerAPIException("Trying to register a modifier with the name " + alias + " but it already is registered");
+    }
     if(new TinkerRegisterEvent.ModifierRegisterEvent(modifier).fire()) {
       modifiers.put(alias, modifier);
     }
