224d28ac585766b1cdc97610e2c33f4ff48c9a76
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2019-09-18 16:49:32 +0200

    Fix wooden hoppers deleting contents on rotation update #3947

--- src/main/java/slimeknights/tconstruct/gadgets/tileentity/TileWoodenHopper.java
+++ src/main/java/slimeknights/tconstruct/gadgets/tileentity/TileWoodenHopper.java
@@ -1,8 +1,11 @@
 package slimeknights.tconstruct.gadgets.tileentity;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityHopper;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TileWoodenHopper extends TileEntityHopper {
 
@@ -13,5 +16,12 @@ public class TileWoodenHopper extends TileEntityHopper {
   @Override
   public void setTransferCooldown(int ticks) {
     super.setTransferCooldown(ticks*2);
+  }
+
+  @Override
+  public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+    // has to use the vanilla method. We could also check if only the rotation differs, but we only have 1 property
+    // so it should be sufficient to only use the block itself
+    return oldState.getBlock() != newSate.getBlock();
   }
 }
