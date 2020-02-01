c0c825a6cbad3edbd5b2834e8021359641686b73
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2020-02-01 17:56:56 +0100

    Remove data generators for PR (readded after PR)

--- src/main/java/slimeknights/tconstruct/tools/data/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package slimeknights.tconstruct.tools.data;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
