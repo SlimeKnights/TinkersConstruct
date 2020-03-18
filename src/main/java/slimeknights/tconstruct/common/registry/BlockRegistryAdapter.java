a4ee361c8157ef36503f6d1d852061f86cf74885
alexbegt <traincrazyb@aol.com>
2020-03-18 00:03:27 -0400

    Begin working on the smeltery for 1.15.

--- src/main/java/slimeknights/tconstruct/common/registry/BlockRegistryAdapter.java
+++ src/main/java/slimeknights/tconstruct/common/registry/BlockRegistryAdapter.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraftforge.registries.IForgeRegistry;
+import slimeknights.tconstruct.smeltery.block.SearedSlabBlock;
+import slimeknights.tconstruct.smeltery.block.SearedStairsBlock;
 
 // MANTLE
 // TODO: move to mantle
@@ -29,5 +31,19 @@ public class BlockRegistryAdapter extends BaseRegistryAdapter<Block> {
     this.register(block, registryName);
     this.register(new SlabBlock(Block.Properties.from(block)), registryName + "_slab");
     this.register(new StairsBlock(block::getDefaultState, Block.Properties.from(block)), registryName + "_stairs");
+  }
+
+  /**
+   * Registers the given block as well as a slab and a stair variant for it.
+   * Uses the vanilla slab and stair blocks. Uses the passed blocks properties for both.
+   * Slabs and stairs are registered with a "_slab" and "_stairs" prefix
+   *
+   * @param block        The main block to register and whose properties to use
+   * @param registryName The registry name to use for the block and as base for the slab and stairs
+   */
+  public void registerSmelterySlabsAndStairs(Block block, String registryName) {
+    this.register(block, registryName);
+    this.register(new SearedSlabBlock(Block.Properties.from(block)), registryName + "_slab");
+    this.register(new SearedStairsBlock(block::getDefaultState, Block.Properties.from(block)), registryName + "_stairs");
   }
 }
