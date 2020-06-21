42d424da04bf7c4c0394060d41caa1d45289b45c
alexbegt <traincrazyb@aol.com>
2020-06-21 19:57:07 -0400

    Fix the part chest not scrolling the side bars

--- src/main/java/slimeknights/tconstruct/tables/client/inventory/chest/PartChestScreen.java
+++ src/main/java/slimeknights/tconstruct/tables/client/inventory/chest/PartChestScreen.java
@@ -5,8 +5,8 @@ import net.minecraft.util.text.ITextComponent;
 import slimeknights.mantle.client.screen.ScalableElementScreen;
 import slimeknights.tconstruct.tables.client.inventory.TinkerStationScreen;
 import slimeknights.tconstruct.tables.client.inventory.library.ScalingChestScreen;
-import slimeknights.tconstruct.tables.inventory.chest.PartChestContainer;
 import slimeknights.tconstruct.tables.inventory.TinkerStationContainer;
+import slimeknights.tconstruct.tables.inventory.chest.PartChestContainer;
 import slimeknights.tconstruct.tables.tileentity.chest.PartChestTileEntity;
 
 public class PartChestScreen extends TinkerStationScreen<PartChestTileEntity, TinkerStationContainer<PartChestTileEntity>> {
@@ -29,5 +29,41 @@ public class PartChestScreen extends TinkerStationScreen<PartChestTileEntity, Ti
     this.scalingChestScreen.update(mouseX, mouseY);
 
     super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+  }
+
+  @Override
+  public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+    if(this.scalingChestScreen.handleMouseClicked(mouseX, mouseY, mouseButton)) {
+      return false;
+    }
+
+    return super.mouseClicked(mouseX, mouseY, mouseButton);
+  }
+
+  @Override
+  public boolean mouseDragged(double mouseX, double mouseY, int clickedMouseButton, double timeSinceLastClick, double unknown) {
+    if (this.scalingChestScreen.handleMouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick)) {
+      return false;
+    }
+
+    return super.mouseDragged(mouseX, mouseY, clickedMouseButton, timeSinceLastClick, unknown);
+  }
+
+  @Override
+  public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+    if(this.scalingChestScreen.handleMouseScrolled(mouseX, mouseY, delta)) {
+      return false;
+    }
+
+    return super.mouseScrolled(mouseX, mouseY, delta);
+  }
+
+  @Override
+  public boolean mouseReleased(double mouseX, double mouseY, int state) {
+    if (this.scalingChestScreen.handleMouseReleased(mouseX, mouseY, state)) {
+      return false;
+    }
+
+    return super.mouseReleased(mouseX, mouseY, state);
   }
 }
