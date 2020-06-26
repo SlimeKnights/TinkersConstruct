dc60249a84f1b9dc3f115bc941438acf3b47431e
KnightMiner <knightminer4@gmail.com>
2020-06-26 18:19:45 -0500

    Remove recipe type committed too early

--- src/main/java/slimeknights/tconstruct/library/recipe/RecipeTypes.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/RecipeTypes.java
@@ -3,13 +3,11 @@ package slimeknights.tconstruct.library.recipe;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeType;
 import slimeknights.tconstruct.TConstruct;
-import slimeknights.tconstruct.library.recipe.melting.IMeltingRecipe;
 
 /**
  * Class containing all of Tinkers Construct recipe types
  */
 public interface RecipeTypes {
-  IRecipeType<IMeltingRecipe> MELTING = register("melting");
 
   /**
    * Registers a new recipe type, prefixing with the mod ID
