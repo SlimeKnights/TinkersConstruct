95321fa4a62b49a8234d8c01684081c8c9016f75
mDiyo <merdiwendiyo@gmail.com>
2020-07-10 15:03:31 -0700

    Update tests to work with new modifier tags

--- src/test/java/slimeknights/tconstruct/library/tools/nbt/StatsNBTTest.java
+++ src/test/java/slimeknights/tconstruct/library/tools/nbt/StatsNBTTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MaterialRegistryExtension.class)
 class StatsNBTTest extends BaseMcTest {
 
-  private final StatsNBT testStatsNBT = new StatsNBT(1, 2, 3, 4, 5, 6, true);
+  private final StatsNBT testStatsNBT = new StatsNBT(1, 2, 3, 4, 5, 6, 7, 8, true);
 
   @Test
   void serialize() {
@@ -23,7 +23,9 @@ class StatsNBTTest extends BaseMcTest {
     assertThat(nbt.getFloat(StatsNBT.TAG_ATTACK)).isEqualTo(3);
     assertThat(nbt.getFloat(StatsNBT.TAG_MININGSPEED)).isEqualTo(4);
     assertThat(nbt.getFloat(StatsNBT.TAG_ATTACKSPEEDMULTIPLIER)).isEqualTo(5);
-    assertThat(nbt.getInt(StatsNBT.TAG_FREE_MODIFIERS)).isEqualTo(6);
+    assertThat(nbt.getInt(StatsNBT.TAG_FREE_MOD_SLOTS)).isEqualTo(6);
+    assertThat(nbt.getInt(StatsNBT.TAG_FREE_ABILITY_SLOTS)).isEqualTo(7);
+    assertThat(nbt.getInt(StatsNBT.TAG_FREE_ARMOR_SLOTS)).isEqualTo(8);
     assertThat(nbt.getBoolean(StatsNBT.TAG_BROKEN)).isTrue();
   }
 
@@ -31,28 +33,32 @@ class StatsNBTTest extends BaseMcTest {
   void serializeEmpty_emptyList() {
     CompoundNBT nbt = StatsNBT.EMPTY.serializeToNBT();
 
-    assertThat(nbt.size()).isEqualTo(7);
+    assertThat(nbt.size()).isEqualTo(9);
   }
 
   @Test
   void deserialize() {
     CompoundNBT nbt = new CompoundNBT();
-    nbt.putInt(StatsNBT.TAG_DURABILITY, 6);
-    nbt.putInt(StatsNBT.TAG_HARVESTLEVEL, 5);
-    nbt.putFloat(StatsNBT.TAG_ATTACK, 4);
-    nbt.putFloat(StatsNBT.TAG_MININGSPEED, 3);
-    nbt.putFloat(StatsNBT.TAG_ATTACKSPEEDMULTIPLIER, 2);
-    nbt.putInt(StatsNBT.TAG_FREE_MODIFIERS, 1);
+    nbt.putInt(StatsNBT.TAG_DURABILITY, 8);
+    nbt.putInt(StatsNBT.TAG_HARVESTLEVEL, 7);
+    nbt.putFloat(StatsNBT.TAG_ATTACK, 6);
+    nbt.putFloat(StatsNBT.TAG_MININGSPEED, 5);
+    nbt.putFloat(StatsNBT.TAG_ATTACKSPEEDMULTIPLIER, 4);
+    nbt.putInt(StatsNBT.TAG_FREE_MOD_SLOTS, 3);
+    nbt.putInt(StatsNBT.TAG_FREE_ABILITY_SLOTS, 2);
+    nbt.putInt(StatsNBT.TAG_FREE_ARMOR_SLOTS, 1);
     nbt.putBoolean(StatsNBT.TAG_BROKEN, false);
 
     StatsNBT statsNBT = StatsNBT.readFromNBT(nbt);
 
-    assertThat(statsNBT.durability).isEqualTo(6);
-    assertThat(statsNBT.harvestLevel).isEqualTo(5);
-    assertThat(statsNBT.attack).isEqualTo(4);
-    assertThat(statsNBT.miningSpeed).isEqualTo(3);
-    assertThat(statsNBT.attackSpeedMultiplier).isEqualTo(2);
-    assertThat(statsNBT.freeModifiers).isEqualTo(1);
+    assertThat(statsNBT.durability).isEqualTo(8);
+    assertThat(statsNBT.harvestLevel).isEqualTo(7);
+    assertThat(statsNBT.attack).isEqualTo(6);
+    assertThat(statsNBT.miningSpeed).isEqualTo(5);
+    assertThat(statsNBT.attackSpeedMultiplier).isEqualTo(4);
+    assertThat(statsNBT.freeModSlots).isEqualTo(3);
+    assertThat(statsNBT.freeAbilitySlots).isEqualTo(2);
+    assertThat(statsNBT.freeArmorSlots).isEqualTo(1);
     assertThat(statsNBT.broken).isFalse();
   }
 
