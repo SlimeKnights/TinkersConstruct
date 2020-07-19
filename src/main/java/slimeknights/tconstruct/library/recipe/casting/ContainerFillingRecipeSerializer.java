0f1170453c1fc96ffaef8430ab64afc2513c724e
KnightMiner <knightminer4@gmail.com>
2020-07-18 21:24:26 -0500

    Cleanup container filling recipe recipe a bit
    Mostly to use JSONUtils to fetch the item, so we have examples for the future

--- src/main/java/slimeknights/tconstruct/library/recipe/casting/ContainerFillingRecipeSerializer.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/casting/ContainerFillingRecipeSerializer.java
@@ -1,14 +1,12 @@
 package slimeknights.tconstruct.library.recipe.casting;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import lombok.AllArgsConstructor;
 import net.minecraft.item.Item;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.recipe.RecipeUtil;
@@ -24,11 +22,7 @@ public class ContainerFillingRecipeSerializer<T extends ContainerFillingRecipe> 
   public T read(ResourceLocation recipeId, JsonObject json) {
     String group = JSONUtils.getString(json, "group", "");
     int fluidAmount = JSONUtils.getInt(json, "fluid_amount");
-    String itemName = JSONUtils.getString(json, "container");
-    Item result = ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName));
-    if (result == null) {
-      throw new JsonSyntaxException("Unknown item '" + itemName + "'");
-    }
+    Item result = JSONUtils.getItem(json, "container");
     return this.factory.create(recipeId, group, fluidAmount, result);
   }
 
