7310822a1c01b8f422b841643dfa89cfbad96821
RCXcrafter <rcxcrafter@gmail.com>
2020-08-04 22:22:34 +0200

    edit slime block/item textures to 1.14 style

--- src/main/java/slimeknights/tconstruct/gadgets/item/SlimeBootsItem.java
+++ src/main/java/slimeknights/tconstruct/gadgets/item/SlimeBootsItem.java
@@ -5,17 +5,14 @@ import com.google.common.collect.Multimap;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
-import net.minecraft.item.IDyeableArmorItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import slimeknights.mantle.item.ArmorTooltipItem;
 import slimeknights.tconstruct.library.Util;
-import slimeknights.tconstruct.shared.block.SlimeBlock;
 
-public class SlimeBootsItem extends ArmorTooltipItem implements IDyeableArmorItem {
+public class SlimeBootsItem extends ArmorTooltipItem {
 
   private static final IArmorMaterial SLIME = new IArmorMaterial() {
     private final Ingredient empty_repair_material = Ingredient.fromItems(Items.AIR);
@@ -56,21 +53,8 @@ public class SlimeBootsItem extends ArmorTooltipItem implements IDyeableArmorIte
     }
   };
 
-  private final SlimeBlock.SlimeType slimeType;
-
-  public SlimeBootsItem(SlimeBlock.SlimeType slimeType, Properties props) {
+  public SlimeBootsItem(Properties props) {
     super(SLIME, EquipmentSlotType.FEET, props);
-    this.slimeType = slimeType;
-  }
-
-  @Override
-  public boolean hasColor(ItemStack stack) {
-    return true;
-  }
-
-  @Override
-  public int getColor(ItemStack stack) {
-    return this.slimeType.getBallColor();
   }
 
   @Override
