71be3174e804aa78a8fda7e248b248a6c7b6b217
Marty E. Plummer <hanetzer@startmail.com>
2020-08-29 07:37:46 -0500

    multiblock: port MultiblockTinker
    Currently a 1:1 port from 1.12
    
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

---
+++ src/main/java/slimeknights/tconstruct/smeltery/multiblock/MultiblockTinker.java
@@ -0,0 +1,36 @@
+package slimeknights.tconstruct.smeltery.multiblock;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import slimeknights.mantle.multiblock.MultiServantLogic;
+import slimeknights.tconstruct.smeltery.tileentity.MultiblockTile;
+
+public abstract class MultiblockTinker extends MultiblockCuboid {
+
+  public final MultiblockTile<?> tile;
+
+  public MultiblockTinker(MultiblockTile<?> tile, boolean hasFloor, boolean hasFrame, boolean hasCeiling) {
+    super(hasFloor, hasFrame, hasCeiling);
+    this.tile = tile;
+  }
+
+  protected boolean isValidSlave(World world, BlockPos pos) {
+    if(!world.isBlockLoaded(pos)) {
+      return false;
+    }
+    TileEntity te = world.getTileEntity(pos);
+
+    // slave-blocks are only allowed if they already belong to this smeltery
+    if(te instanceof MultiServantLogic) {
+      MultiServantLogic slave = (MultiServantLogic) te;
+      if(slave.hasValidMaster()) {
+        if(!tile.getPos().equals(slave.getMasterPosition())) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
