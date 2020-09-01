b5898267cae26539d4fa1e4c6aaa433000562bb2
alexbegt <traincrazyb@aol.com>
2020-09-01 10:43:00 -0400

    Move away from Vanilla's getRemainingItems in place for our own consumeInputs

--- src/main/java/slimeknights/tconstruct/library/recipe/tinkerstation/ITinkerStationRecipe.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/tinkerstation/ITinkerStationRecipe.java
@@ -2,11 +2,15 @@ package slimeknights.tconstruct.library.recipe.tinkerstation;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import slimeknights.mantle.recipe.ICommonRecipe;
 import slimeknights.tconstruct.library.recipe.RecipeTypes;
 import slimeknights.tconstruct.library.recipe.ValidationResult;
 import slimeknights.tconstruct.tables.tileentity.table.tinkerstation.ITinkerStationInventory;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public interface ITinkerStationRecipe extends ICommonRecipe<ITinkerStationInventory> {
   /* Recipe data */
@@ -30,7 +34,17 @@ public interface ITinkerStationRecipe extends ICommonRecipe<ITinkerStationInvent
     return getRecipeOutput().copy();
   }
 
-  default int getAmountUsed(int index) {
-    return 1;
+  /**
+   * Handles subtracting the inputs used by the recipe and passes any extra items (container items) to the consumer
+   *
+   * @param inventory the actual items in the inventory
+   * @param extraStackConsumer handles the extra stacks if they are unable to be added to the inventory
+   */
+  void consumeInputs(List<ItemStack> inventory, Consumer<ItemStack> extraStackConsumer);
+
+  /** Unused, please call consumeInputs */
+  @Deprecated
+  default NonNullList<ItemStack> getRemainingItems(ITinkerStationInventory inv) {
+    return NonNullList.from(ItemStack.EMPTY);
   }
 }
