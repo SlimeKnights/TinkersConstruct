18d34d0c44fae94012665c24a98dbb93b95505bc
KnightMiner <knightminer4@gmail.com>
2020-10-18 17:49:27 -0400

    Rework tinker station recipe interface to better support repair and modifier recipes
    Turns out a list with no empty stacks simply made the code more complex. A standard slot index makes things a ton easier.

---
+++ src/main/java/slimeknights/tconstruct/library/recipe/tinkerstation/IMutableTinkerStationInventory.java
@@ -0,0 +1,50 @@
+package slimeknights.tconstruct.library.recipe.tinkerstation;
+
+import net.minecraft.item.ItemStack;
+
+/**
+ * Extension of {@link ITinkerStationInventory} to allow modifying inventory contents
+ */
+public interface IMutableTinkerStationInventory extends ITinkerStationInventory {
+  /**
+   * Sets the input in the given slot
+   * @param index  Slot to set
+   * @param stack  New stack
+   */
+  void setInput(int index, ItemStack stack);
+
+  /**
+   * Gives an extra recipe result to the user of the inventory
+   * @param stack  Extra stack
+   */
+  void giveItem(ItemStack stack);
+
+  /**
+   * Shrinks a slot by the given count, returning the properly sized container
+   * @param slot    Slot to shrink
+   * @param amount  Amount to shrink by
+   */
+  default void shrinkInput(int slot, int amount) {
+    ItemStack stack = getInput(slot);
+    if (!stack.isEmpty()) {
+      // determine how large to make the container
+      int count = stack.getCount();
+      ItemStack container = stack.getContainerItem();
+      if (!container.isEmpty()) {
+        container.setCount(Math.min(count, amount));
+      }
+      // if removing all items, set the slot to the container. If container is empty that is fine
+      if (amount >= count) {
+        setInput(slot, container);
+      } else {
+        // otherwise, shrink the stack and add the container
+        stack = stack.copy();
+        stack.shrink(amount);
+        setInput(slot, stack);
+        if (!container.isEmpty()) {
+          giveItem(container);
+        }
+      }
+    }
+  }
+}
