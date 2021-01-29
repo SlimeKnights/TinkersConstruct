1f7770af08afff276786e3c28cb8be1620d25a0f
KnightMiner <knightminer4@gmail.com>
2021-01-29 18:09:15 -0500

    Added crafting station transfer handler

--- src/main/java/slimeknights/tconstruct/plugin/jei/JEIPlugin.java
+++ src/main/java/slimeknights/tconstruct/plugin/jei/JEIPlugin.java
@@ -14,6 +14,7 @@ import mezz.jei.api.registration.IModIngredientRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
@@ -58,6 +59,7 @@ import slimeknights.tconstruct.smeltery.client.inventory.MelterScreen;
 import slimeknights.tconstruct.smeltery.client.inventory.SmelteryScreen;
 import slimeknights.tconstruct.smeltery.data.SmelteryCompat;
 import slimeknights.tconstruct.smeltery.item.CopperCanItem;
+import slimeknights.tconstruct.tables.inventory.table.CraftingStationContainer;
 import slimeknights.tconstruct.tools.TinkerToolParts;
 import slimeknights.tconstruct.tools.TinkerTools;
 
@@ -210,6 +212,10 @@ public class JEIPlugin implements IModPlugin {
     registration.addGenericGuiContainerHandler(SmelteryScreen.class, new GuiContainerTankHandler<>());
   }
 
+  @Override
+  public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+    registration.addRecipeTransferHandler(CraftingStationContainer.class, VanillaRecipeCategoryUid.CRAFTING, 0, 9, 10, 36);
+  }
 
   @Override
   public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
