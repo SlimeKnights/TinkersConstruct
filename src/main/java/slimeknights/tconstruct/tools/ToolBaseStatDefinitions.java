8a53401bc03369b8ad13b292d0032b313fecd8cf
KnightMiner <knightminer4@gmail.com>
2021-02-06 20:43:07 -0500

    Stat rebalancing
    Lowered mining speed and attack damage on many material heads to better line up with vanilla
    Notably, in tier 3 you need a diamond modifier to match vanilla. Slimesteel and the diamond modifier is roughly diamond durability and attack, and bronze with diamond gives roughly diamond mining speed
    This is pre-handle modifiers, its based on wooden handles
    
    In addition, most tools were switched from damage multiplier to damage bonus, to better line up with vanilla You should get exact same stats with relevant head, wooden handle, and in tier 3 adding the diamond modifier

--- src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
+++ src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
@@ -7,23 +7,23 @@ import slimeknights.tconstruct.library.tools.ToolBaseStatDefinition;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ToolBaseStatDefinitions {
   // pickaxes
-  static final ToolBaseStatDefinition PICKAXE = new ToolBaseStatDefinition.Builder().setDamageModifier(1).setAttackSpeed(1.2f).build();
+  static final ToolBaseStatDefinition PICKAXE = new ToolBaseStatDefinition.Builder().setDamageBonus(1f).setAttackSpeed(1.2f).build();
   static final ToolBaseStatDefinition HAMMER = new ToolBaseStatDefinition.Builder()
-    .setDamageModifier(1.2f).setAttackSpeed(0.8f).setMiningSpeedModifier(0.4f).setDurabilityModifier(2.5f).build();
+    .setDamageBonus(1f).setDamageModifier(1.25f).setAttackSpeed(0.8f).setMiningSpeedModifier(0.4f).setDurabilityModifier(2.5f).build();
 
   // shovels
-  static final ToolBaseStatDefinition SHOVEL = new ToolBaseStatDefinition.Builder().setDamageModifier(0.9f).setAttackSpeed(1f).build();
+  static final ToolBaseStatDefinition SHOVEL = new ToolBaseStatDefinition.Builder().setDamageBonus(1.5f).setAttackSpeed(1f).build();
   static final ToolBaseStatDefinition EXCAVATOR = new ToolBaseStatDefinition.Builder()
-    .setDamageModifier(1.25f).setAttackSpeed(0.7f).setMiningSpeedModifier(0.28f).setDurabilityModifier(1.75f).build();
+    .setDamageBonus(1.5f).setDamageModifier(1.1f).setAttackSpeed(0.7f).setMiningSpeedModifier(0.28f).setDurabilityModifier(1.75f).build();
 
   // axes
   static final ToolBaseStatDefinition AXE = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(0.5f).setDamageModifier(1.1f).setAttackSpeed(1.1f).setKnockbackModifier(1.3f).build();
+    .setDamageBonus(5.0f).setAttackSpeed(1.1f).setKnockbackModifier(1.3f).build();
 
   // scythes
-  static final ToolBaseStatDefinition KAMA = new ToolBaseStatDefinition.Builder().setDamageModifier(1.0f).setAttackSpeed(1.3f).build();
+  static final ToolBaseStatDefinition KAMA = new ToolBaseStatDefinition.Builder().setDamageBonus(1.25f).setAttackSpeed(1.3f).build();
 
   // swords
   static final ToolBaseStatDefinition BROADSWORD = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(1).setDamageModifier(1).setAttackSpeed(1.6f).setMiningSpeedModifier(0.5f).setDurabilityModifier(1.1f).build();
+    .setDamageBonus(3f).setAttackSpeed(1.6f).setMiningSpeedModifier(0.5f).setDurabilityModifier(1.1f).build();
 }
