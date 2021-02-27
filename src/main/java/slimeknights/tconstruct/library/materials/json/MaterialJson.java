3c09e5820b61ea103ed72cfedcc30dc1aec6a93e
KnightMiner <knightminer4@gmail.com>
2021-02-27 02:39:31 -0500

    Add condition to material loading, skip materials that do not match condition

--- src/main/java/slimeknights/tconstruct/library/materials/json/MaterialJson.java
+++ src/main/java/slimeknights/tconstruct/library/materials/json/MaterialJson.java
@@ -2,12 +2,15 @@ package slimeknights.tconstruct.library.materials.json;
 
 import lombok.Data;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.conditions.ICondition;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 
 import javax.annotation.Nullable;
 
 @Data
 public class MaterialJson {
+  @Nullable
+  private final ICondition condition;
   @Nullable
   private final Boolean craftable;
   @Nullable
--- src/main/java/slimeknights/tconstruct/tools/data/Materials.java
+++ src/main/java/slimeknights/tconstruct/tools/data/Materials.java
@@ -1,10 +1,12 @@
 package slimeknights.tconstruct.tools.data;
 
+import com.mojang.datafixers.util.Pair;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.util.text.Color;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.NotCondition;
+import net.minecraftforge.common.crafting.conditions.TagEmptyCondition;
 import slimeknights.tconstruct.fluids.TinkerFluids;
 import slimeknights.tconstruct.library.materials.IMaterial;
 import slimeknights.tconstruct.library.materials.MaterialId;
@@ -13,6 +15,7 @@ import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,7 +39,7 @@ final class Materials {
   private static final int ORDER_NETHER = 7;
   /** Order for end materials in tiers 1-4 */
   private static final int ORDER_END = 10;
-  static final List<IMaterial> allMaterials = new ArrayList<>();
+  static final List<Pair<IMaterial,ICondition>> allMaterials = new ArrayList<>();
 
   // tier 1
   public static final IMaterial wood  = mat(MaterialIds.wood,  1, ORDER_GENERAL, true, 0x8e661b, TinkerModifiers.cultivated);
@@ -47,7 +50,8 @@ final class Materials {
   public static final IMaterial iron        = mat(MaterialIds.iron,        2, ORDER_GENERAL, TinkerFluids.moltenIron, false, 0xcacaca, TinkerModifiers.reinforced);
   public static final IMaterial copper      = mat(MaterialIds.copper,      2, ORDER_HARVEST, TinkerFluids.moltenCopper, true, 0xfba165, TinkerModifiers.dwarfish);
   public static final IMaterial searedStone = mat(MaterialIds.searedStone, 2, ORDER_WEAPON,  TinkerFluids.searedStone, false, 0x3f3f3f, TinkerModifiers.searing);
-  public static final IMaterial slimewood   = mat(MaterialIds.slimewood,   2, ORDER_SPECIAL, 0x82c873, TinkerModifiers.overgrowth, TinkerModifiers.overslime);
+  private static final Supplier<List<ModifierEntry>> SLIMEWOOD_TRAITS = () -> Arrays.asList(new ModifierEntry(TinkerModifiers.overgrowth.get(), 1), new ModifierEntry(TinkerModifiers.overslime.get(), 1));
+  public static final IMaterial slimewood   = mat(new DataMaterial(MaterialIds.slimewood, 2, ORDER_SPECIAL, false, 0x82c873, SLIMEWOOD_TRAITS));
   // tier 3
   public static final IMaterial slimesteel    = mat(MaterialIds.slimesteel,    3, ORDER_GENERAL, TinkerFluids.moltenSlimesteel, false, 0x74c8c7, TinkerModifiers.overcast);
   public static final IMaterial tinkersBronze = mat(MaterialIds.tinkersBronze, 3, ORDER_HARVEST, TinkerFluids.moltenTinkersBronze, false, 0xf9cf72, TinkerModifiers.wellMaintained);
@@ -61,21 +65,21 @@ final class Materials {
   public static final IMaterial cobalt = mat(MaterialIds.cobalt, 3, ORDER_NETHER, TinkerFluids.moltenCobalt, false, 0x2376dd, TinkerModifiers.lightweight);
   // tier 4
   public static final IMaterial queensSlime = mat(MaterialIds.queensSlime, 4, ORDER_GENERAL, TinkerFluids.moltenQueensSlime, false, 0x236c45, TinkerModifiers.overlord);
-  public static final IMaterial hepatizon   = mat(MaterialIds.hepatizon,   4, ORDER_HARVEST, TinkerFluids.moltenHepatizon,   false, 0x60496b);
-  public static final IMaterial manyullyn   = mat(MaterialIds.manyullyn,   4, ORDER_WEAPON,  TinkerFluids.moltenManyullyn,   false, 0x9261cc);
+  public static final IMaterial hepatizon   = mat(MaterialIds.hepatizon,   4, ORDER_HARVEST, TinkerFluids.moltenHepatizon,   false, 0x60496b, null);
+  public static final IMaterial manyullyn   = mat(MaterialIds.manyullyn,   4, ORDER_WEAPON,  TinkerFluids.moltenManyullyn,   false, 0x9261cc, null);
   //public static final IMaterial soulsteel   = mat(MaterialIds.soulsteel, 4, ORDER_SPECIAL, TinkerFluids.moltenSoulsteel, false, 0x6a5244);
 
   // tier 2 (end)
   //public static final IMaterial endstone = mat(MaterialIds.endstone, true, 0xe0d890);
 
   // tier 2 (mod integration)
-  public static final IMaterial lead     = mat(MaterialIds.lead,     2, ORDER_COMPAT, TinkerFluids.moltenLead,     false, 0x4d4968, TinkerModifiers.heavy);
-  public static final IMaterial silver   = mat(MaterialIds.silver,   2, ORDER_COMPAT, TinkerFluids.moltenSilver,   false, 0xd1ecf6, TinkerModifiers.smite);
+  public static final IMaterial lead     = compatMat(MaterialIds.lead,   2, ORDER_COMPAT, TinkerFluids.moltenLead,   TinkerModifiers.heavy);
+  public static final IMaterial silver   = compatMat(MaterialIds.silver, 2, ORDER_COMPAT, TinkerFluids.moltenSilver, TinkerModifiers.smite);
   // tier 3 (mod integration)
-  public static final IMaterial electrum   = mat(MaterialIds.electrum,   3, ORDER_COMPAT,  TinkerFluids.moltenElectrum,   false, 0xe8db49, TinkerModifiers.experienced);
-  public static final IMaterial bronze     = mat(MaterialIds.bronze,     3, ORDER_HARVEST, TinkerFluids.moltenBronze,     false, 0xcea179, TinkerModifiers.wellMaintained2);
-  public static final IMaterial steel      = mat(MaterialIds.steel,      3, ORDER_GENERAL, TinkerFluids.moltenSteel,      false, 0xa7a7a7, TinkerModifiers.sturdy);
-  public static final IMaterial constantan = mat(MaterialIds.constantan, 3, ORDER_COMPAT,  TinkerFluids.moltenConstantan, false, 0xff9e7f);
+  public static final IMaterial electrum   = compatMat(MaterialIds.electrum,   3, ORDER_COMPAT,  TinkerFluids.moltenElectrum,   TinkerModifiers.experienced);
+  public static final IMaterial bronze     = compatMat(MaterialIds.bronze,     3, ORDER_HARVEST, TinkerFluids.moltenBronze,     TinkerModifiers.wellMaintained2);
+  public static final IMaterial steel      = compatMat(MaterialIds.steel,      3, ORDER_GENERAL, TinkerFluids.moltenSteel,      TinkerModifiers.sturdy);
+  public static final IMaterial constantan = compatMat(MaterialIds.constantan, 3, ORDER_COMPAT,  TinkerFluids.moltenConstantan, null);
 
   // bowstring IMaterials
 //  public static final IMaterial string = mat(MaterialIds.string, true, 0xeeeeee);
@@ -83,39 +87,40 @@ final class Materials {
 //  public static final IMaterial slimevine_blue = mat(MaterialIds.slimevine_blue, true, 0x74c8c7);
 //  public static final IMaterial slimevine_purple = mat(MaterialIds.slimevine_purple, true, 0xc873c8);
 
+  /** Creates a supplier for the given trait */
+  private static Supplier<List<ModifierEntry>> traitSupplier(@Nullable Supplier<? extends Modifier> trait) {
+    if (trait == null) {
+      return Collections::emptyList;
+    }
+    return () -> Collections.singletonList(new ModifierEntry(trait.get(), 1));
+  }
 
-  /** Creates a material with no fluid and two traits */
-  private static IMaterial mat(MaterialId location, int tier, int order, int color, Supplier<? extends Modifier> trait1, Supplier<? extends Modifier> trait2) {
-    // all our materials use ingot value right now, so not much need to make a constructor parameter - option is mainly for addons
-    Supplier<List<ModifierEntry>> traitSupplier = () -> Arrays.asList(new ModifierEntry(trait1.get(), 1), new ModifierEntry(trait2.get(), 1));
-    IMaterial material = new DataMaterial(location, tier, order, () -> Fluids.EMPTY, 0, true, Color.fromInt(color), traitSupplier);
-    allMaterials.add(material);
+  /** Creates a new material with a tag requirement */
+  private static IMaterial mat(IMaterial material, @Nullable ICondition condition) {
+    allMaterials.add(Pair.of(material, condition));
     return material;
   }
 
-  /** Creates a material with a fluid and a trait */
-  private static IMaterial mat(MaterialId location, int tier, int order, Supplier<? extends Fluid> fluid, boolean craftable, int color, Supplier<? extends Modifier> trait) {
+  /** Creates a new material */
+  private static IMaterial mat(IMaterial material) {
+    return mat(material, null);
+  }
+
+  /** Creates a material with a fluid */
+  private static IMaterial mat(MaterialId location, int tier, int order, Supplier<? extends Fluid> fluid, boolean craftable, int color, @Nullable Supplier<? extends Modifier> trait) {
     // all our materials use ingot value right now, so not much need to make a constructor parameter - option is mainly for addons
-    Supplier<List<ModifierEntry>> traitSupplier = () -> Collections.singletonList(new ModifierEntry(trait.get(), 1));
-    IMaterial material = new DataMaterial(location, tier, order, fluid, MaterialValues.INGOT, craftable, Color.fromInt(color), traitSupplier);
-    allMaterials.add(material);
-    return material;
+    return mat(new DataMaterial(location, tier, order, fluid, MaterialValues.INGOT, craftable, color, traitSupplier(trait)));
   }
 
   /** Creates a material with no fluid */
-  private static IMaterial mat(MaterialId location, int tier, int order, boolean craftable, int color, Supplier<? extends Modifier> trait) {
-    return mat(location, tier, order, Fluids.EMPTY.delegate, craftable, color, trait);
+  private static IMaterial mat(MaterialId location, int tier, int order, boolean craftable, int color, @Nullable Supplier<? extends Modifier> trait) {
+    return mat(new DataMaterial(location, tier, order, craftable, color, traitSupplier(trait)));
   }
 
-  /** Creates a material with a fluid and no trait */
-  private static IMaterial mat(MaterialId location, int tier, int order, Supplier<? extends Fluid> fluid, boolean craftable, int color) {
-    IMaterial material = new DataMaterial(location, tier, order, fluid, MaterialValues.INGOT, craftable, Color.fromInt(color), Collections::emptyList);
-    allMaterials.add(material);
-    return material;
-  }
-
-  /** Creates a material with no fluid and no trait */
-  private static IMaterial mat(MaterialId location, int tier, int order, boolean craftable, int color) {
-    return mat(location, tier, order, Fluids.EMPTY.delegate, craftable, color);
+  /** Creates a new compat material */
+  private static IMaterial compatMat(MaterialId location, int tier, int order, Supplier<? extends Fluid> fluid, @Nullable Supplier<? extends Modifier> trait) {
+    // all our addon materials use ingot value right now, so not much need to make a constructor parameter - option is mainly for addons
+    ICondition condition = new NotCondition(new TagEmptyCondition("forge", "ingots/" + location.getPath()));
+    return mat(new DataMaterial(location, tier, order, fluid, MaterialValues.INGOT, false, fluid.get().getAttributes().getColor() & 0xFFFFFF, traitSupplier(trait)), condition);
   }
 }
