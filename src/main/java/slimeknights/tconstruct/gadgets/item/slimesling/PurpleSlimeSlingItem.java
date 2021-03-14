ee839e74dcae2c5f810887d6a305fc22b4965dac
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2021-03-13 23:47:29 -0600

    Teleport sound
    Also little cleanup

--- src/main/java/slimeknights/tconstruct/gadgets/item/slimesling/PurpleSlimeSlingItem.java
+++ src/main/java/slimeknights/tconstruct/gadgets/item/slimesling/PurpleSlimeSlingItem.java
@@ -3,13 +3,16 @@ package slimeknights.tconstruct.gadgets.item.slimesling;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import slimeknights.tconstruct.common.Sounds;
 import slimeknights.tconstruct.library.network.TinkerNetwork;
 import slimeknights.tconstruct.tools.common.network.EntityMovementChangePacket;
+
+import java.util.Random;
 
 public class PurpleSlimeSlingItem extends BaseSlimeSlingItem {
 
@@ -39,6 +42,13 @@ public class PurpleSlimeSlingItem extends BaseSlimeSlingItem {
       TinkerNetwork.getInstance().sendTo(new EntityMovementChangePacket(player), playerMP);
     }
 
+    // particle effect from EnderPearlEntity
+    Random rand = new Random();
+    for (int i = 0; i < 32; ++i) {
+      worldIn.addParticle(ParticleTypes.PORTAL, player.getPosX(), player.getPosY() + rand.nextDouble() * 2.0D, player.getPosZ(), rand.nextGaussian(), 0.0D, rand.nextGaussian());
+    }
+
     player.playSound(Sounds.SLIME_SLING.getSound(), 1f, 1f);
+    player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
   }
 }
