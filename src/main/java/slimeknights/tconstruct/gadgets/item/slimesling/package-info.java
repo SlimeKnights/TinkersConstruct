f2f5bb20a2ecbc2f3820f6eb755071f55d4d3340
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2021-03-19 13:15:31 -0500

    Use ProjectileHelper.rayTraceEntities and add miss sound

---
+++ src/main/java/slimeknights/tconstruct/gadgets/item/slimesling/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package slimeknights.tconstruct.gadgets.item.slimesling;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
