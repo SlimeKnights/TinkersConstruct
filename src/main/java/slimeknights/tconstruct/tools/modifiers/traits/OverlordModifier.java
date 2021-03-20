02b72496961c44ea50ef6d63aaaa81e29b7b45de
KnightMiner <knightminer4@gmail.com>
2021-03-20 00:12:12 -0400

    Rework a couple of overslime traits
    +75 is really more tier 3 than tier 4, so granted to slimesteel
    Overslime is instead a pro/con, giving you a ton of overslime at the cost of regular durability

--- src/main/java/slimeknights/tconstruct/tools/modifiers/traits/OverlordModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/traits/OverlordModifier.java
@@ -3,6 +3,8 @@ package slimeknights.tconstruct.tools.modifiers.traits;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.tools.nbt.IModDataReadOnly;
 import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import slimeknights.tconstruct.tools.ToolStatsModifierBuilder;
 import slimeknights.tconstruct.tools.modifiers.free.OverslimeModifier;
 
 public class OverlordModifier extends Modifier {
@@ -10,9 +12,21 @@ public class OverlordModifier extends Modifier {
     super(0x236c45);
   }
 
+  /** Gets the durability boost per level */
+  private int getBoost(IModDataReadOnly volatileData, int level) {
+    return (int)(volatileData.getInt(ToolStack.ORIGINAL_DURABILITY_KEY) * .15f * level);
+  }
+
   @Override
   public void addVolatileData(IModDataReadOnly persistentData, int level, ModDataNBT volatileData) {
     volatileData.putBoolean(OverslimeModifier.KEY_OVERSLIME_FRIEND, true);
-    OverslimeModifier.addCap(volatileData, level * 75);
+    // gains +15% of the durability per level
+    OverslimeModifier.addCap(volatileData, getBoost(volatileData, level));
+  }
+
+  @Override
+  public void addToolStats(IModDataReadOnly persistentData, IModDataReadOnly volatileData, int level, ToolStatsModifierBuilder builder) {
+    // at most subtract 90% durability
+    builder.addDurability(-getBoost(volatileData, Math.min(level, 6)));
   }
 }
