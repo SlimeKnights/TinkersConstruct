bc8cef1ae0d041b372c3830b228e91147a250894
KnightMiner <knightminer4@gmail.com>
2021-03-20 02:51:17 -0400

    Emeralds now deal bonus damage to illagers

--- src/main/java/slimeknights/tconstruct/tools/modifiers/upgrades/EmeraldModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/upgrades/EmeraldModifier.java
@@ -1,7 +1,10 @@
 package slimeknights.tconstruct.tools.modifiers.upgrades;
 
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.LivingEntity;
 import slimeknights.tconstruct.library.modifiers.SingleUseModifier;
 import slimeknights.tconstruct.library.tools.nbt.IModDataReadOnly;
+import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 import slimeknights.tconstruct.library.utils.HarvestLevels;
 import slimeknights.tconstruct.tools.ToolStatsModifierBuilder;
 
@@ -14,5 +17,13 @@ public class EmeraldModifier extends SingleUseModifier {
   public void addToolStats(IModDataReadOnly persistentData, IModDataReadOnly volatileData, int level, ToolStatsModifierBuilder builder) {
     builder.multiplyDurability(1.5f);
     builder.setHarvestLevel(HarvestLevels.IRON);
+  }
+
+  @Override
+  public float applyLivingDamage(IModifierToolStack tool, int level, LivingEntity attacker, LivingEntity target, float baseDamage, float damage, boolean isCritical, boolean fullyCharged) {
+    if (target.getCreatureAttribute() == CreatureAttribute.ILLAGER) {
+      damage += 2.5f;
+    }
+    return damage;
   }
 }
