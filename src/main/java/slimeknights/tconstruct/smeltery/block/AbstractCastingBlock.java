bf12cb254c5b9b8a327fce9c3b5f4c4b1977cd76
KnightMiner <knightminer4@gmail.com>
2021-03-21 16:31:40 -0400

    Add comparator interactions to basins and tables

--- src/main/java/slimeknights/tconstruct/smeltery/block/AbstractCastingBlock.java
+++ src/main/java/slimeknights/tconstruct/smeltery/block/AbstractCastingBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
+import slimeknights.mantle.util.TileEntityHelper;
 import slimeknights.tconstruct.shared.block.TableBlock;
 import slimeknights.tconstruct.smeltery.tileentity.CastingTileEntity;
 
@@ -33,5 +34,23 @@ public abstract class AbstractCastingBlock extends TableBlock {
   @Override
   protected boolean openGui(PlayerEntity playerEntity, World world, BlockPos blockPos) {
     return false;
+  }
+
+  @Override
+  public boolean hasComparatorInputOverride(BlockState state) {
+    return true;
+  }
+
+  @Override
+  public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
+    return TileEntityHelper.getTile(CastingTileEntity.class, worldIn, pos).map(te -> {
+      if (te.isStackInSlot(CastingTileEntity.OUTPUT)) {
+        return 15;
+      }
+      if (te.isStackInSlot(CastingTileEntity.INPUT)) {
+        return 1;
+      }
+      return 0;
+    }).orElse(0);
   }
 }
