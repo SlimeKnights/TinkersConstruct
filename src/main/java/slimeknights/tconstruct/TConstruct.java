0b8fbe65ebf970afdd3abb93ccb8fca21d3af9ee
KnightMiner <knightminer4@gmail.com>
2021-03-21 01:18:26 -0400

    Remove debug command
    Been long since not needed, and was never a great implementation

--- src/main/java/slimeknights/tconstruct/TConstruct.java
+++ src/main/java/slimeknights/tconstruct/TConstruct.java
@@ -1,22 +1,11 @@
 package slimeknights.tconstruct;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.block.Block;
-import net.minecraft.client.gui.ScreenManager;
-import net.minecraft.command.CommandSource;
-import net.minecraft.command.Commands;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.util.IItemProvider;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.common.MinecraftForge;
@@ -28,10 +17,8 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -43,8 +30,6 @@ import slimeknights.tconstruct.common.data.tags.TConstructBlockTagsProvider;
 import slimeknights.tconstruct.common.data.tags.TConstructEntityTypeTagsProvider;
 import slimeknights.tconstruct.common.data.tags.TConstructFluidTagsProvider;
 import slimeknights.tconstruct.common.data.tags.TConstructItemTagsProvider;
-import slimeknights.tconstruct.debug.ToolDebugContainer;
-import slimeknights.tconstruct.debug.ToolDebugScreen;
 import slimeknights.tconstruct.fluids.TinkerFluids;
 import slimeknights.tconstruct.gadgets.TinkerGadgets;
 import slimeknights.tconstruct.library.MaterialRegistry;
@@ -121,12 +106,6 @@ public class TConstruct {
   }
 
   @SubscribeEvent
-  static void clientSetup(final FMLClientSetupEvent event) {
-    // TODO: this belongs in the debug module, not here
-    ScreenManager.registerFactory(ToolDebugContainer.TOOL_DEBUG_CONTAINER_TYPE, ToolDebugScreen::new);
-  }
-
-  @SubscribeEvent
   static void gatherData(final GatherDataEvent event) {
     if (event.includeServer()) {
       DataGenerator datagenerator = event.getGenerator();
@@ -138,28 +117,6 @@ public class TConstruct {
       datagenerator.addProvider(new TConstructEntityTypeTagsProvider(datagenerator, existingFileHelper));
       datagenerator.addProvider(new TConstructLootTableProvider(datagenerator));
     }
-  }
-
-  @SubscribeEvent
-  void onServerStarting(final FMLServerStartingEvent event) {
-    LiteralArgumentBuilder<CommandSource> executes = Commands.literal("tic_debug")
-      .requires(commandSource -> commandSource.hasPermissionLevel(4))
-      .executes(context -> {
-        context.getSource().sendFeedback(new StringTextComponent("TiC debug"), false);
-        context.getSource().asPlayer().openContainer(new INamedContainerProvider() {
-          @Override
-          public ITextComponent getDisplayName() {
-            return new StringTextComponent("debug");
-          }
-
-          @Override
-          public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
-            return new ToolDebugContainer(p_createMenu_1_, p_createMenu_2_);
-          }
-        });
-        return Command.SINGLE_SUCCESS;
-      });
-    event.getServer().getCommandManager().getDispatcher().register(executes);
   }
 
   @Nullable
--- src/main/java/slimeknights/tconstruct/debug/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package slimeknights.tconstruct.debug;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
