4d3a2066a0c1c22eac574cfe7fb3b687b2061382
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2021-03-21 02:56:24 -0500

    replace attemptTeleport with custom algorithm

--- src/main/java/slimeknights/tconstruct/gadgets/item/slimesling/EnderSlimeSlingItem.java
+++ src/main/java/slimeknights/tconstruct/gadgets/item/slimesling/EnderSlimeSlingItem.java
@@ -6,11 +6,16 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import slimeknights.tconstruct.common.Sounds;
 import slimeknights.tconstruct.library.network.TinkerNetwork;
 import slimeknights.tconstruct.tools.common.network.EntityMovementChangePacket;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnderSlimeSlingItem extends BaseSlimeSlingItem {
 
@@ -29,13 +34,26 @@ public class EnderSlimeSlingItem extends BaseSlimeSlingItem {
     float f = getForce(stack, timeLeft);
 
     Vector3d look = player.getLookVec();
+    double offX = look.x * f;
+    double offY = look.y * f + 1; // add extra to help with bad collisions
+    double offZ = look.z * f;
 
-    int crouchingFactor = (player.isCrouching() ? 2 : 1);
-    double offX = look.x * f / crouchingFactor;
-    double offY = look.y * f / crouchingFactor + 1;
-    double offZ = look.z * f / crouchingFactor;
+    List<BlockPos> posToCheck = new ArrayList<>();
+    BlockPos posAttempt = new BlockPos(player.getPosX() + offX, player.getPosY() + offY, player.getPosZ() + offZ);
+    while (Math.abs(offX) > .5 || Math.abs(offY) > .5 || Math.abs(offZ) > .5) { // while not too close to player
+      posToCheck.add(posAttempt);
+      offX -= (Math.abs(offX) > .25 ? (offX >= 0 ? 1 : -1) * .25 : 0);
+      offY -= (Math.abs(offY) > .25 ? (offY >= 0 ? 1 : -1) * .25 : 0);
+      offZ -= (Math.abs(offZ) > .25 ? (offZ >= 0 ? 1 : -1) * .25 : 0);
+      posAttempt = new BlockPos(player.getPosX() + offX, player.getPosY() + offY, player.getPosZ() + offZ);
+    }
 
-    if (player.attemptTeleport(player.getPosX() + offX, player.getPosY() + offY, player.getPosZ() + offZ, true)) {
+    // filter to only teleportable blocks
+    posToCheck = posToCheck.stream().distinct().filter(block -> !worldIn.getBlockState(block).isSuffocating(worldIn, block)).collect(Collectors.toList());
+
+    if (!posToCheck.isEmpty()) {
+      player.setPosition(posToCheck.get(0).getX(), posToCheck.get(0).getY(), posToCheck.get(0).getZ());
+
       if (player instanceof ServerPlayerEntity) {
         ServerPlayerEntity playerMP = (ServerPlayerEntity) player;
         TinkerNetwork.getInstance().sendTo(new EntityMovementChangePacket(player), playerMP);
@@ -48,6 +66,8 @@ public class EnderSlimeSlingItem extends BaseSlimeSlingItem {
 
       player.playSound(Sounds.SLIME_SLING.getSound(), 1f, 1f);
       player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
+    } else {
+      player.playSound(Sounds.SLIME_SLING.getSound(), 1f, .5f);
     }
   }
 }
