4d028a573398d788c782422fdfe5af085985442d
KnightMiner <knightminer4@gmail.com>
2021-04-05 02:43:30 -0400

    Correct some weapon stats
    Axes are slower now, and do a bit more damage (more like vanilla axes)
    Hammers are a lot slower, and do a lot more damage
    Mattocks do slightly more damage now, as viable as a shovel

--- src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
+++ src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
@@ -9,13 +9,13 @@ public final class ToolBaseStatDefinitions {
   // pickaxes
   static final ToolBaseStatDefinition PICKAXE = new ToolBaseStatDefinition.Builder().setDamageBonus(1f).setAttackSpeed(1.2f).build();
   static final ToolBaseStatDefinition SLEDGE_HAMMER = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(1f).setDamageModifier(1.5f)
-    .setAttackSpeed(0.8f).setMiningSpeedModifier(0.4f)
+    .setDamageBonus(5f).setDamageModifier(1.5f)
+    .setAttackSpeed(0.5f).setMiningSpeedModifier(0.4f)
     .setDurabilityModifier(4f).setPrimaryHeadWeight(2)
     .setDefaultUpgrades(2).build();
 
   // shovels
-  static final ToolBaseStatDefinition MATTOCK = new ToolBaseStatDefinition.Builder().setDamageBonus(1f).setAttackSpeed(1f).build();
+  static final ToolBaseStatDefinition MATTOCK = new ToolBaseStatDefinition.Builder().setDamageBonus(1.5f).setAttackSpeed(1f).build();
   static final ToolBaseStatDefinition EXCAVATOR = new ToolBaseStatDefinition.Builder()
     .setDamageBonus(1.5f).setDamageModifier(1.25f)
     .setAttackSpeed(0.7f).setMiningSpeedModifier(0.3f)
@@ -24,7 +24,7 @@ public final class ToolBaseStatDefinitions {
     .build();
 
   // axes
-  static final ToolBaseStatDefinition AXE = new ToolBaseStatDefinition.Builder().setDamageBonus(5.0f).setAttackSpeed(1.1f).build();
+  static final ToolBaseStatDefinition AXE = new ToolBaseStatDefinition.Builder().setDamageBonus(6.0f).setAttackSpeed(0.9f).build();
 
   // scythes
   static final ToolBaseStatDefinition KAMA = new ToolBaseStatDefinition.Builder().setDamageBonus(1.25f).setAttackSpeed(1.3f).build();
