db69ba590042213559b21e5f6dcb2de2fb71cfa2
AlphaMode <AlphaModeYT@gmail.com>
2021-04-09 01:32:22 -0500

    last push of the night

--- src/main/java/slimeknights/tconstruct/shared/TinkerCommons.java
+++ src/main/java/slimeknights/tconstruct/shared/TinkerCommons.java
@@ -1,22 +1,10 @@
 package slimeknights.tconstruct.shared;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.GlassBlock;
-import net.minecraft.block.Material;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
-import net.minecraft.loot.condition.LootConditionType;
-import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.registry.Registry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import org.apache.logging.log4j.Logger;
 import slimeknights.mantle.item.EdibleItem;
@@ -36,6 +24,20 @@ import slimeknights.tconstruct.shared.block.ClearStainedGlassPaneBlock;
 import slimeknights.tconstruct.shared.block.GlowBlock;
 import slimeknights.tconstruct.shared.block.SlimeType;
 import slimeknights.tconstruct.shared.data.CommonRecipeProvider;
+import java.util.function.Function;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.GlassBlock;
+import net.minecraft.block.Material;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.loot.condition.LootConditionType;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.registry.Registry;
 
 /**
  * Contains items and blocks and stuff that is shared by multiple modules, but might be required individually
@@ -47,13 +49,13 @@ public final class TinkerCommons extends TinkerModule {
   /*
    * Blocks
    */
-  public static final GlowBlock glow = BLOCKS.registerNoItem("glow", () -> new GlowBlock(builder(Material.SUPPORTED, NO_TOOL, BlockSoundGroup.WOOL).strength(0.0F).luminance(s -> 14).nonOpaque()));
+  public static final GlowBlock glow = (GlowBlock) BLOCKS.registerNoItem("glow", () -> new GlowBlock(builder(Material.SUPPORTED, NO_TOOL, BlockSoundGroup.WOOL).strength(0.0F).luminance(s -> 14).nonOpaque()));
   public static final BuildingBlockObject mudBricks = BLOCKS.registerBuilding("mud_bricks", builder(Material.SOIL, ToolType.SHOVEL, BlockSoundGroup.GRAVEL).requiresTool().strength(2.0F), GENERAL_BLOCK_ITEM);
   // clay
   // TODO: moving to natura
   private static final AbstractBlock.Settings DRIED_CLAY = builder(Material.STONE, ToolType.PICKAXE, BlockSoundGroup.STONE).requiresTool().strength(1.5F, 20.0F);
-  public static final BuildingBlockObject driedClay = BLOCKS.registerBuilding("dried_clay", DRIED_CLAY, HIDDEN_BLOCK_ITEM);
-  public static final BuildingBlockObject driedClayBricks = BLOCKS.registerBuilding("dried_clay_bricks", DRIED_CLAY, HIDDEN_BLOCK_ITEM);
+  public static final BuildingBlockObject driedClay = BLOCKS.registerBuilding("dried_clay", DRIED_CLAY, (Function<Block, BlockItem>) HIDDEN_BLOCK_ITEM);
+  public static final BuildingBlockObject driedClayBricks = BLOCKS.registerBuilding("dried_clay_bricks", DRIED_CLAY, (Function<Block, BlockItem>) HIDDEN_BLOCK_ITEM);
   // glass
   public static final ItemObject<GlassBlock> clearGlass = BLOCKS.register("clear_glass", () -> new GlassBlock(GENERIC_GLASS_BLOCK), GENERAL_BLOCK_ITEM);
   public static final ItemObject<ClearGlassPaneBlock> clearGlassPane = BLOCKS.register("clear_glass_pane", () -> new ClearGlassPaneBlock(GENERIC_GLASS_BLOCK), GENERAL_BLOCK_ITEM);
@@ -62,7 +64,7 @@ public final class TinkerCommons extends TinkerModule {
   public static final ItemObject<GlassBlock> soulGlass = BLOCKS.register("soul_glass", () -> new GlassBlock(GENERIC_GLASS_BLOCK), GENERAL_BLOCK_ITEM);
   public static final ItemObject<ClearGlassPaneBlock> soulGlassPane = BLOCKS.register("soul_glass_pane", () -> new ClearGlassPaneBlock(GENERIC_GLASS_BLOCK), GENERAL_BLOCK_ITEM);
   // wood
-  private static final Block.Properties WOOD = builder(Material.WOOD, ToolType.AXE, BlockSoundGroup.WOOD).requiresTool().strength(2.0F, 7.0F).luminance(s -> 7);
+  private static final AbstractBlock.Settings WOOD = builder(Material.WOOD, ToolType.AXE, BlockSoundGroup.WOOD).requiresTool().strength(2.0F, 7.0F).luminance(s -> 7);
   public static final BuildingBlockObject lavawood = BLOCKS.registerBuilding("lavawood", WOOD, GENERAL_BLOCK_ITEM);
   public static final BuildingBlockObject blazewood = BLOCKS.registerBuilding("blazewood", WOOD, GENERAL_BLOCK_ITEM);
 
