c09a89650db49978eda8e90061255831a7c89669
KnightMiner <knightminer4@gmail.com>
2021-04-15 04:01:55 -0400

    Make sky slimes jump higher

--- src/main/java/slimeknights/tconstruct/world/entity/BlueSlimeEntity.java
+++ src/main/java/slimeknights/tconstruct/world/entity/BlueSlimeEntity.java
@@ -32,6 +32,11 @@ public class BlueSlimeEntity extends SlimeEntity {
   }
 
   @Override
+  protected float getJumpUpwardsMotion() {
+    return (float)Math.sqrt(this.getSlimeSize()) * this.getJumpFactor() / 2;
+  }
+
+  @Override
   protected IParticleData getSquishParticle() {
     return TinkerWorld.slimeParticle.get();
   }
