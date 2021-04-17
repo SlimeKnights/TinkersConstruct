d5736961b8bd3464380c14d3e7100f634b5bff83
KnightMiner <knightminer4@gmail.com>
2021-04-17 02:04:49 -0400

    Fancy frame fixes
    Fix entities not being localized, clear item frames not being transparent, and clear item frames still showing when an item is inside

--- src/main/java/slimeknights/tconstruct/gadgets/client/FancyItemFrameRenderer.java
+++ src/main/java/slimeknights/tconstruct/gadgets/client/FancyItemFrameRenderer.java
@@ -22,6 +22,8 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.storage.MapData;
+import net.minecraftforge.client.event.RenderItemInFrameEvent;
+import net.minecraftforge.common.MinecraftForge;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.gadgets.entity.FancyItemFrameEntity;
 import slimeknights.tconstruct.gadgets.entity.FrameType;
@@ -62,37 +64,43 @@ public class FancyItemFrameRenderer extends EntityRenderer<FancyItemFrameEntity>
   public void render(FancyItemFrameEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
     super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
     matrixStackIn.push();
-    Direction direction = entityIn.getHorizontalFacing();
     Vector3d vec3d = this.getRenderOffset(entityIn, partialTicks);
     matrixStackIn.translate(-vec3d.getX(), -vec3d.getY(), -vec3d.getZ());
+    Direction direction = entityIn.getHorizontalFacing();
     matrixStackIn.translate((double) direction.getXOffset() * 0.46875D, (double) direction.getYOffset() * 0.46875D, (double) direction.getZOffset() * 0.46875D);
     matrixStackIn.rotate(Vector3f.XP.rotationDegrees(entityIn.rotationPitch));
     matrixStackIn.rotate(Vector3f.YP.rotationDegrees(180.0F - entityIn.rotationYaw));
-    BlockRendererDispatcher blockrendererdispatcher = this.mc.getBlockRendererDispatcher();
-    ModelManager modelmanager = blockrendererdispatcher.getBlockModelShapes().getModelManager();
+
+    // render the frame
     FrameType frameType = entityIn.getFrameType();
-    ModelResourceLocation modelresourcelocation = entityIn.getDisplayedItem().getItem() instanceof FilledMapItem ? LOCATIONS_MODEL_MAP.get(frameType) : LOCATIONS_MODEL.get(frameType);
-    matrixStackIn.push();
-    matrixStackIn.translate(-0.5D, -0.5D, -0.5D);
-    blockrendererdispatcher.getBlockModelRenderer().renderModelBrightnessColor(matrixStackIn.getLast(), bufferIn.getBuffer(Atlases.getSolidBlockType()), null, modelmanager.getModel(modelresourcelocation), 1.0F, 1.0F, 1.0F, packedLightIn, OverlayTexture.NO_OVERLAY);
-    matrixStackIn.pop();
-    ItemStack itemstack = entityIn.getDisplayedItem();
-    if (!itemstack.isEmpty()) {
-      MapData mapdata = FilledMapItem.getMapData(itemstack, entityIn.world);
+    ItemStack stack = entityIn.getDisplayedItem();
+    // clear does not render the frame if filled
+    if (frameType != FrameType.CLEAR || stack.isEmpty()) {
+      BlockRendererDispatcher blockrendererdispatcher = this.mc.getBlockRendererDispatcher();
+      ModelManager modelmanager = blockrendererdispatcher.getBlockModelShapes().getModelManager();
+      ModelResourceLocation location = entityIn.getDisplayedItem().getItem() instanceof FilledMapItem ? LOCATIONS_MODEL_MAP.get(frameType) : LOCATIONS_MODEL.get(frameType);
+      matrixStackIn.push();
+      matrixStackIn.translate(-0.5D, -0.5D, -0.5D);
+      blockrendererdispatcher.getBlockModelRenderer().renderModelBrightnessColor(matrixStackIn.getLast(), bufferIn.getBuffer(Atlases.getCutoutBlockType()), null, modelmanager.getModel(location), 1.0F, 1.0F, 1.0F, packedLightIn, OverlayTexture.NO_OVERLAY);
+      matrixStackIn.pop();
+    }
+
+    // render the item
+    if (!stack.isEmpty()) {
+      MapData mapdata = FilledMapItem.getMapData(stack, entityIn.world);
       matrixStackIn.translate(0.0D, 0.0D, 0.4375D);
       int i = mapdata != null ? entityIn.getRotation() % 4 * 2 : entityIn.getRotation();
       matrixStackIn.rotate(Vector3f.ZP.rotationDegrees((float) i * 360.0F / 8.0F));
-      if (!net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.RenderItemInFrameEvent(entityIn, this.defaultRenderer, matrixStackIn, bufferIn, packedLightIn))) {
+      if (!MinecraftForge.EVENT_BUS.post(new RenderItemInFrameEvent(entityIn, this.defaultRenderer, matrixStackIn, bufferIn, packedLightIn))) {
         if (mapdata != null) {
           matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(180.0F));
-          float f = 0.0078125F;
           matrixStackIn.scale(0.0078125F, 0.0078125F, 0.0078125F);
           matrixStackIn.translate(-64.0D, -64.0D, 0.0D);
           matrixStackIn.translate(0.0D, 0.0D, -1.0D);
           this.mc.gameRenderer.getMapItemRenderer().renderMap(matrixStackIn, bufferIn, mapdata, true, packedLightIn);
         } else {
           matrixStackIn.scale(0.5F, 0.5F, 0.5F);
-          this.itemRenderer.renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED, packedLightIn, OverlayTexture.NO_OVERLAY, matrixStackIn, bufferIn);
+          this.itemRenderer.renderItem(stack, ItemCameraTransforms.TransformType.FIXED, packedLightIn, OverlayTexture.NO_OVERLAY, matrixStackIn, bufferIn);
         }
       }
     }
