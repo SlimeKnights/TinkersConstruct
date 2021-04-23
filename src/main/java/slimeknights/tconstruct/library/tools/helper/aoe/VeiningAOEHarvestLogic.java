0df64ec259a486c20f533c4151bad361dfead357
KnightMiner <knightminer4@gmail.com>
2021-04-23 00:12:34 -0400

    Pickaxe and mattock veining

---
+++ src/main/java/slimeknights/tconstruct/library/tools/helper/aoe/VeiningAOEHarvestLogic.java
@@ -0,0 +1,115 @@
+package slimeknights.tconstruct.library.tools.helper.aoe;
+
+import com.google.common.collect.AbstractIterator;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import slimeknights.tconstruct.library.tools.helper.ToolHarvestLogic;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import slimeknights.tconstruct.tools.TinkerModifiers;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * Harvest logic that breaks a block plus neighbors of the same type
+ */
+@RequiredArgsConstructor
+public class VeiningAOEHarvestLogic extends ToolHarvestLogic {
+  public static final VeiningAOEHarvestLogic SMALL = new VeiningAOEHarvestLogic(0);
+  private final int maxDistance;
+
+  @Override
+  public Iterable<BlockPos> getAOEBlocks(ToolStack tool, ItemStack stack, PlayerEntity player, BlockState state, World world, BlockPos origin, Direction sideHit, AOEMatchType matchType) {
+    if (!canAOE(tool, stack, state, matchType)) {
+      return Collections.emptyList();
+    }
+    int expanded = tool.getModifierLevel(TinkerModifiers.expanded.get());
+    return calculate(world, origin, maxDistance + expanded);
+  }
+
+  /**
+   * Calculates the blocks for veining
+   * @param world        World instance
+   * @param origin       Position origin
+   * @param maxDistance  Max distance to vein
+   * @return  Iterator for veining
+   */
+  public static Iterable<BlockPos> calculate(World world, BlockPos origin, int maxDistance) {
+    Block target = world.getBlockState(origin).getBlock();
+    return () -> new VeiningIterator(world, origin, target, maxDistance);
+  }
+
+  /** Iterator that navigates block and other similar blocks */
+  private static class VeiningIterator extends AbstractIterator<BlockPos> {
+    private final Set<BlockPos> visited = new HashSet<>();
+    private final Queue<DistancePos> queue = new ArrayDeque<>();
+
+    private final World world;
+    private final Block target;
+    private final int maxDistance;
+    private VeiningIterator(World world, BlockPos origin, Block target, int maxDistance) {
+      this.world = world;
+      this.target = target;
+      this.maxDistance = maxDistance;
+      // make use of origin
+      visited.add(origin);
+      if (maxDistance > 0) {
+        // start off the queue with the position in each direction
+        enqueueNeighbors(origin, 1);
+      }
+    }
+
+    /**
+     * Enqueues all neighbors of this position
+     * @param pos       Position
+     * @param distance  Distance for neighbors
+     */
+    private void enqueueNeighbors(BlockPos pos, int distance) {
+      for (Direction direction : Direction.values()) {
+        BlockPos offset = pos.offset(direction);
+        if (!visited.contains(offset)) {
+          visited.add(offset); // mark position visited to prevent adding again before we get to it
+          queue.add(new DistancePos(offset, distance));
+        }
+      }
+    }
+
+    @Override
+    protected BlockPos computeNext() {
+      while (!queue.isEmpty()) {
+        // grab the next queued position to check
+        DistancePos distancePos = queue.remove();
+        BlockPos pos = distancePos.getPos();
+        // must be a valid block
+        if (world.getBlockState(pos).isIn(target)) {
+          // if not at max distance yet, add blocks on all sides
+          int distance = distancePos.getDistance();
+          if (distance < maxDistance) {
+            enqueueNeighbors(pos, distance + 1);
+          }
+          // finally, return the position
+          return pos;
+        }
+      }
+      // queue ran out of data
+      return endOfData();
+    }
+  }
+
+  /** Helper data class */
+  @Data
+  private static class DistancePos {
+    private final BlockPos pos;
+    private final int distance;
+  }
+}
--- src/main/java/slimeknights/tconstruct/tools/harvest/PickaxeTool.java
+++ src/main/java/slimeknights/tconstruct/tools/harvest/PickaxeTool.java
@@ -1,15 +1,35 @@
 package slimeknights.tconstruct.tools.harvest;
 
 import com.google.common.collect.Sets;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import slimeknights.tconstruct.library.tools.ToolDefinition;
 import slimeknights.tconstruct.library.tools.helper.ToolHarvestLogic;
+import slimeknights.tconstruct.library.tools.helper.aoe.VeiningAOEHarvestLogic;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import slimeknights.tconstruct.tools.TinkerModifiers;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class PickaxeTool extends HarvestTool {
   protected static final Set<Material> EXTRA_MATERIALS = Sets.newHashSet(Material.ROCK, Material.IRON, Material.ANVIL);
-  public static final MaterialHarvestLogic HARVEST_LOGIC = new MaterialHarvestLogic(EXTRA_MATERIALS, 0, 0, 0);
+  public static final MaterialHarvestLogic HARVEST_LOGIC = new MaterialHarvestLogic(EXTRA_MATERIALS, 0, 0, 0) {
+    @Override
+    public Iterable<BlockPos> getAOEBlocks(ToolStack tool, ItemStack stack, PlayerEntity player, BlockState state, World world, BlockPos origin, Direction sideHit, AOEMatchType matchType) {
+      if (!canAOE(tool, stack, state, matchType)) {
+        return Collections.emptyList();
+      }
+      // veining block breaking
+      return VeiningAOEHarvestLogic.calculate(world, origin, tool.getModifierLevel(TinkerModifiers.expanded.get()));
+    }
+  };
+
   public PickaxeTool(Properties properties, ToolDefinition toolDefinition) {
     super(properties, toolDefinition);
   }
