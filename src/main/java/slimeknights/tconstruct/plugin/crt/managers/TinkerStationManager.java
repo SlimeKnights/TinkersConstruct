dac8686f4a7ddb8378c941f4fc0ba61928c73e4f
Jared <jaredlll08@gmail.com>
2021-04-26 06:44:41 +0200

    Update TinkerStationManager

--- src/main/java/slimeknights/tconstruct/plugin/crt/managers/TinkerStationManager.java
+++ src/main/java/slimeknights/tconstruct/plugin/crt/managers/TinkerStationManager.java
@@ -10,6 +10,7 @@ import com.blamejared.crafttweaker.api.managers.IRecipeManager;
 import com.blamejared.crafttweaker.impl.actions.recipes.ActionAddRecipe;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.util.ResourceLocation;
+import org.graalvm.compiler.lir.aarch64.AArch64AddressValue;
 import org.openzen.zencode.java.ZenCodeType;
 import slimeknights.mantle.recipe.SizedIngredient;
 import slimeknights.tconstruct.library.modifiers.Modifier;
@@ -29,7 +30,9 @@ import java.util.stream.Collectors;
 @ZenRegister
 @ZenCodeType.Name("mods.tconstruct.TinkerStation")
 public class TinkerStationManager implements IRecipeManager {
-  
+
+  private final String defaultError = "recipe.tconstruct.modifier.requirements_error";
+
   @ZenCodeType.Method
   public void addOverslimeModifierRecipe(String name, IIngredient ingredient, int restoreAmount) {
     name = fixRecipeName(name);
@@ -37,63 +40,79 @@ public class TinkerStationManager implements IRecipeManager {
     OverslimeModifierRecipe recipe = new OverslimeModifierRecipe(id, ingredient.asVanillaIngredient(), restoreAmount);
     CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe));
   }
-  
+
   @ZenCodeType.Method
-  public void addModifierRecipe(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots) {
+  public void addModifierRecipe(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addModifierRecipeInternal(name, inputs, toolRequired, modifierResult, modifierResultLevel, maxLevel, 0, 0, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @ZenCodeType.Method
+  public void addUpgradeModifierRecipe(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addModifierRecipeInternal(name, inputs, toolRequired, modifierResult, modifierResultLevel, maxLevel, upgradeSlots, 0, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @ZenCodeType.Method
+  public void addAbilityModifierRecipe(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, int abilitySlots, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addModifierRecipeInternal(name, inputs, toolRequired, modifierResult, modifierResultLevel, maxLevel, 0, abilitySlots, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @ZenCodeType.Method
+  public void addIncrementalModifierRecipe(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, IItemStack leftover, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addIncrementalModifierRecipeInternal(name, input, amountPerInput, neededPerLevel, toolRequirement, modifierResult, modifierResultLevel, maxLevel, 0, 0, leftover, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @ZenCodeType.Method
+  public void addIncrementalUpgradeModifierRecipe(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, IItemStack leftover, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addIncrementalModifierRecipeInternal(name, input, amountPerInput, neededPerLevel, toolRequirement, modifierResult, modifierResultLevel, maxLevel, upgradeSlots, 0, leftover, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @ZenCodeType.Method
+  public void addIncrementalAbilityModifierRecipe(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, int abilitySlots, IItemStack leftover, @ZenCodeType.Optional IData modifierRequirements, @ZenCodeType.OptionalInt(-1) int minMatch, @ZenCodeType.OptionalString(defaultError) String requirementsError) {
+    addIncrementalModifierRecipeInternal(name, input, amountPerInput, neededPerLevel, toolRequirement, modifierResult, modifierResultLevel, maxLevel, 0, abilitySlots, leftover, modifierRequirements, minMatch, requirementsError);
+  }
+
+  @Override
+  public void removeRecipe(IItemStack output) {
+    throw new IllegalArgumentException("Cannot remove Tinker Station Recipes by an IItemStack output! Use `removeByName(String name)` instead!");
+  }
+
+  @Override
+  public IRecipeType<ITinkerStationRecipe> getRecipeType() {
+    return RecipeTypes.TINKER_STATION;
+  }
+
+  private ModifierMatch makeMatch(IData modifierRequirements, int minMatch) {
+    ModifierMatch entry = ModifierMatch.ALWAYS;
+    if (modifierRequirements != null && !modifierRequirements.asMap().isEmpty()) {
+
+      ModifierMatch[] modifierMatches = modifierRequirements.asMap().entrySet().stream().map(stringIDataEntry -> ModifierMatch.entry(CRTHelper.getModifier(stringIDataEntry.getKey()), stringIDataEntry.getValue().asNumber().getInt())).toArray(ModifierMatch[]::new);
+      if (minMatch < 0) {
+        minMatch = modifierMatches.length;
+      }
+      entry = ModifierMatch.list(minMatch, modifierMatches);
+    }
+    return entry;
+  }
+
+  private void addIncrementalModifierRecipeInternal(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots, IItemStack leftover, IData modifierRequirements, int minMatch, String requirementsError) {
     name = fixRecipeName(name);
     Modifier resultModifier = CRTHelper.getModifier(modifierResult);
     ResourceLocation id = new ResourceLocation("crafttweaker", name);
-    List<SizedIngredient> collect = Arrays.stream(inputs).map(iIngredientWithAmount -> SizedIngredient.of(iIngredientWithAmount.getIngredient().asVanillaIngredient(), iIngredientWithAmount.getAmount())).collect(Collectors.toList());
-    ModifierMatch entry = ModifierMatch.ALWAYS;
+    ModifierMatch entry = makeMatch(modifierRequirements, minMatch);
     ModifierEntry result = new ModifierEntry(resultModifier, modifierResultLevel);
-    ModifierRecipe recipe = new ModifierRecipe(id, collect, toolRequired.asVanillaIngredient(), entry, "", result, maxLevel, upgradeSlots, abilitySlots);
+    IncrementalModifierRecipe recipe = new IncrementalModifierRecipe(id, input.asVanillaIngredient(), amountPerInput, neededPerLevel, toolRequirement.asVanillaIngredient(), entry, requirementsError, result, maxLevel, upgradeSlots, abilitySlots, leftover.getInternal());
     CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe));
   }
-  
-  @ZenCodeType.Method
-  public void addModifierRecipe(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots, IData modifierRequirements, int minMatch, String requirementsError) {
+
+  private void addModifierRecipeInternal(String name, IIngredientWithAmount[] inputs, IIngredient toolRequired, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots, IData modifierRequirements, int minMatch, String requirementsError) {
     name = fixRecipeName(name);
     Modifier resultModifier = CRTHelper.getModifier(modifierResult);
-    ModifierMatch[] modifierMatches = modifierRequirements.asMap().entrySet().stream().map(stringIDataEntry -> ModifierMatch.entry(CRTHelper.getModifier(stringIDataEntry.getKey()), stringIDataEntry.getValue().asNumber().getInt())).toArray(ModifierMatch[]::new);
-    ModifierMatch entry = ModifierMatch.list(minMatch, modifierMatches);
+    ModifierMatch entry = makeMatch(modifierRequirements, minMatch);
+
     ResourceLocation id = new ResourceLocation("crafttweaker", name);
     List<SizedIngredient> collect = Arrays.stream(inputs).map(iIngredientWithAmount -> SizedIngredient.of(iIngredientWithAmount.getIngredient().asVanillaIngredient(), iIngredientWithAmount.getAmount())).collect(Collectors.toList());
     ModifierEntry result = new ModifierEntry(resultModifier, modifierResultLevel);
     ModifierRecipe recipe = new ModifierRecipe(id, collect, toolRequired.asVanillaIngredient(), entry, requirementsError, result, maxLevel, upgradeSlots, abilitySlots);
     CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe));
   }
-  
-  @ZenCodeType.Method
-  public void addIncrementalModifierRecipe(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots, IItemStack leftover) {
-    name = fixRecipeName(name);
-    Modifier resultModifier = CRTHelper.getModifier(modifierResult);
-    ResourceLocation id = new ResourceLocation("crafttweaker", name);
-    ModifierMatch entry = ModifierMatch.ALWAYS;
-    ModifierEntry result = new ModifierEntry(resultModifier, modifierResultLevel);
-    IncrementalModifierRecipe recipe = new IncrementalModifierRecipe(id, input.asVanillaIngredient(), amountPerInput, neededPerLevel, toolRequirement.asVanillaIngredient(), entry, "", result, maxLevel, upgradeSlots, abilitySlots, leftover.getInternal());
-    CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe));
-  }
-  
-  @ZenCodeType.Method
-  public void addIncrementalModifierRecipe(String name, IIngredient input, int amountPerInput, int neededPerLevel, IIngredient toolRequirement, String modifierResult, int modifierResultLevel, int maxLevel, int upgradeSlots, int abilitySlots, IItemStack leftover, IData modifierRequirements, int minMatch, String requirementsError) {
-    name = fixRecipeName(name);
-    Modifier resultModifier = CRTHelper.getModifier(modifierResult);
-    ResourceLocation id = new ResourceLocation("crafttweaker", name);
-    ModifierMatch[] modifierMatches = modifierRequirements.asMap().entrySet().stream().map(stringIDataEntry -> ModifierMatch.entry(CRTHelper.getModifier(stringIDataEntry.getKey()), stringIDataEntry.getValue().asNumber().getInt())).toArray(ModifierMatch[]::new);
-    ModifierMatch entry = ModifierMatch.list(minMatch, modifierMatches);
-    ModifierEntry result = new ModifierEntry(resultModifier, modifierResultLevel);
-    IncrementalModifierRecipe recipe = new IncrementalModifierRecipe(id, input.asVanillaIngredient(), amountPerInput, neededPerLevel, toolRequirement.asVanillaIngredient(), entry, requirementsError, result, maxLevel, upgradeSlots, abilitySlots, leftover.getInternal());
-    CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe));
-  }
-  
-  @Override
-  public void removeRecipe(IItemStack output) {
-    throw new IllegalArgumentException("Cannot remove Tinker Station Recipes by an IItemStack output! Use `removeByName(String name)` instead!");
-  }
-  
-  @Override
-  public IRecipeType<ITinkerStationRecipe> getRecipeType() {
-    return RecipeTypes.TINKER_STATION;
-  }
-  
 }
