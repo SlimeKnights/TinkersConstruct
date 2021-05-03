90a69232b2bd8f528d1351780a385f40d30f4aba
KnightMiner <knightminer4@gmail.com>
2021-05-03 15:26:27 -0400

    Adjust a few tool stats (closes #4421)
    Sledge hammer now attacks a bit faster, putting it between hand axe and broad axe in speed
    Vein hammer does slightly more damage and is slightly better, making it the fastest broad tool
    Excavator is less slow as well, making it a slightly better mattock as a weapon
    Cleaver is a bit slower, as its the second best damag weapon

--- src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
+++ src/main/java/slimeknights/tconstruct/tools/ToolBaseStatDefinitions.java
@@ -10,20 +10,20 @@ public final class ToolBaseStatDefinitions {
   static final ToolBaseStatDefinition PICKAXE = new ToolBaseStatDefinition.Builder().setDamageBonus(1f).setAttackSpeed(1.2f).build();
   static final ToolBaseStatDefinition SLEDGE_HAMMER = new ToolBaseStatDefinition.Builder()
     .setDamageBonus(5f).setDamageModifier(1.5f)
-    .setAttackSpeed(0.5f).setMiningSpeedModifier(0.4f)
+    .setAttackSpeed(0.75f).setMiningSpeedModifier(0.4f)
     .setDurabilityModifier(4f).setPrimaryHeadWeight(2)
     .setDefaultUpgrades(2).build();
   static final ToolBaseStatDefinition VEIN_HAMMER = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(2f).setDamageModifier(1.25f)
-    .setAttackSpeed(1.0f).setMiningSpeedModifier(0.3f)
+    .setDamageBonus(3f).setDamageModifier(1.25f)
+    .setAttackSpeed(1.1f).setMiningSpeedModifier(0.3f)
     .setDurabilityModifier(5.0f).setPrimaryHeadWeight(2)
     .setDefaultUpgrades(2).build();
 
   // shovels
   static final ToolBaseStatDefinition MATTOCK = new ToolBaseStatDefinition.Builder().setDamageBonus(1.5f).setAttackSpeed(1f).build();
   static final ToolBaseStatDefinition EXCAVATOR = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(1.5f).setDamageModifier(1.25f)
-    .setAttackSpeed(0.7f).setMiningSpeedModifier(0.3f)
+    .setDamageBonus(1.0f).setDamageModifier(1.25f)
+    .setAttackSpeed(1.0f).setMiningSpeedModifier(0.3f)
     .setDurabilityModifier(3.75f)
     .setKnockbackBonus(1f)
     .setDefaultUpgrades(2).build();
@@ -31,27 +31,29 @@ public final class ToolBaseStatDefinitions {
   // axes
   static final ToolBaseStatDefinition HAND_AXE = new ToolBaseStatDefinition.Builder().setDamageBonus(6.0f).setAttackSpeed(0.9f).build();
   static final ToolBaseStatDefinition BROAD_AXE = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(2f).setDamageModifier(2f)
+    .setDamageBonus(3f).setDamageModifier(2f)
     .setAttackSpeed(0.6f).setMiningSpeedModifier(0.3f)
     .setDurabilityModifier(4.25f).setPrimaryHeadWeight(2)
     .setDefaultUpgrades(2).build();
 
   // scythes
-  static final ToolBaseStatDefinition KAMA = new ToolBaseStatDefinition.Builder().setDamageBonus(1f).setAttackSpeed(1.3f).build();
+  static final ToolBaseStatDefinition KAMA = new ToolBaseStatDefinition.Builder()
+    .setDamageBonus(1f).setDamageModifier(0.75f).setAttackSpeed(2.0f).build();
   static final ToolBaseStatDefinition SCYTHE = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(1f).setDamageModifier(1.25f)
+    .setDamageBonus(1f)
     .setAttackSpeed(0.8f).setMiningSpeedModifier(0.45f)
     .setDurabilityModifier(2.5f)
     .setDefaultUpgrades(2).build();
 
   // swords
   static final ToolBaseStatDefinition BROADSWORD = new ToolBaseStatDefinition.Builder()
-    .setDamageBonus(3f).setAttackSpeed(1.6f)
+    .setDamageBonus(3f)
+    .setAttackSpeed(1.6f)
     .setMiningSpeedModifier(0.5f).setReachBonus(0.5f)
     .setDurabilityModifier(1.1f).build();
   static final ToolBaseStatDefinition CLEAVER = new ToolBaseStatDefinition.Builder()
     .setDamageBonus(3f).setDamageModifier(1.75f)
-    .setAttackSpeed(1.2f)
+    .setAttackSpeed(0.9f)
     .setMiningSpeedModifier(0.25f).setReachBonus(1f)
     .setDurabilityModifier(3.5f).setPrimaryHeadWeight(2)
     .setDefaultUpgrades(2).build();
