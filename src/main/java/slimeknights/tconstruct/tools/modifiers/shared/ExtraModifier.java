917f0172fa7f9817cb3568e42b1f69382922f72c
brickhouse20 <84705698+brickhouse20@users.noreply.github.com>
2021-05-24 03:05:18 -0500

    Update ExtraModifier.java

--- src/main/java/slimeknights/tconstruct/tools/modifiers/shared/ExtraModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/shared/ExtraModifier.java
@@ -22,7 +22,7 @@ public class ExtraModifier extends Modifier {
   }
 
   public ExtraModifier(int color, ExtraType type, ModifierSource source) {
-    this(color, type, source, 1);
+    this(color, type, source, 5);
   }
 
   public ExtraModifier(int color) {
