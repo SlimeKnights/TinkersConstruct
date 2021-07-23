9012209dc71a618452e65d6d45f5f532bbdd9944
KnightMiner <knightminer4@gmail.com>
2021-07-22 23:56:46 -0400

    Correct instatible
    Turns out I doubled it instead of reducing it, so now its +2 damage per level. Its also only 8 hits

--- src/main/java/slimeknights/tconstruct/tools/modifiers/traits/InsatibleModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/traits/InsatibleModifier.java
@@ -19,15 +19,15 @@ public class InsatibleModifier extends Modifier {
   public float getEntityDamage(IModifierToolStack tool, int level, ToolAttackContext context, float baseDamage, float damage) {
     // gives +2 damage per level at max
     int effectLevel = TinkerModifiers.insatiableEffect.get().getLevel(context.getAttacker()) + 1;
-    return damage + (level * effectLevel / 2f * tool.getModifier(ToolStats.ATTACK_DAMAGE));
+    return damage + (level * effectLevel / 4f * tool.getModifier(ToolStats.ATTACK_DAMAGE));
   }
 
   @Override
   public int afterEntityHit(IModifierToolStack tool, int level, ToolAttackContext context, float damageDealt) {
-    // 16 hits gets you to max, levels faster at higher levels
-    if (!context.isExtraAttack()) {
+    // 8 hits gets you to max, levels faster at higher levels
+    if (!context.isExtraAttack() && context.isFullyCharged()) {
       LivingEntity attacker = context.getAttacker();
-      int effectLevel = Math.min(8, TinkerModifiers.insatiableEffect.get().getLevel(attacker) + 1);
+      int effectLevel = Math.min(7, TinkerModifiers.insatiableEffect.get().getLevel(attacker) + 1);
       TinkerModifiers.insatiableEffect.get().apply(attacker, 5 * 20, effectLevel);
     }
     return 0;
