3fc778d55d6ec6210e816f9e9449353c2a7822fd
KnightMiner <knightminer4@gmail.com>
2021-11-27 23:52:21 -0500

    Make blast resistant reduce knockback from explosions

--- src/main/java/slimeknights/tconstruct/tools/modifiers/defense/BlastProtectionModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/defense/BlastProtectionModifier.java
@@ -1,18 +1,33 @@
 package slimeknights.tconstruct.tools.modifiers.defense;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.inventory.EquipmentSlotType.Group;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.Explosion;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.world.ExplosionEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.IncrementalModifier;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability.TinkerDataKey;
+import slimeknights.tconstruct.library.tools.context.EquipmentChangeContext;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
-import slimeknights.tconstruct.library.utils.TooltipFlag;
-
-import java.util.List;
+import slimeknights.tconstruct.tools.logic.ModifierMaxLevel;
 
 public class BlastProtectionModifier extends IncrementalModifier {
+  /** Entity data key for the data associated with this modifier */
+  private static final TinkerDataKey<BlastData> BLAST_DATA = TConstruct.createKey("blast_protection");
   public BlastProtectionModifier() {
     super(0x17DD62);
+    MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, ExplosionEvent.Detonate.class, BlastProtectionModifier::onExplosionDetonate);
+    MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, LivingUpdateEvent.class, BlastProtectionModifier::livingTick);
   }
 
   @Override
@@ -24,9 +39,98 @@ public class BlastProtectionModifier extends IncrementalModifier {
   }
 
   @Override
-  public void addInformation(IModifierToolStack tool, int level, List<ITextComponent> tooltip, TooltipFlag tooltipFlag) {
-    ProtectionModifier.addResistanceTooltip(this, tool, level, 2f, tooltip);
+  public void onUnequip(IModifierToolStack tool, int level, EquipmentChangeContext context) {
+    LivingEntity entity = context.getEntity();
+    EquipmentSlotType slot = context.getChangedSlot();
+    if (slot.getSlotType() == Group.ARMOR && !entity.getEntityWorld().isRemote) {
+      entity.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(data -> {
+        BlastData blastData = data.get(BLAST_DATA);
+        if (blastData != null) {
+          blastData.level.set(slot, 0);
+          blastData.wasKnockback = false;
+        }
+      });
+    }
   }
 
-  // TODO: explosion knockback resistance
+  @Override
+  public void onEquip(IModifierToolStack tool, int level, EquipmentChangeContext context) {
+    LivingEntity entity = context.getEntity();
+    EquipmentSlotType slot = context.getChangedSlot();
+    if (!entity.getEntityWorld().isRemote && slot.getSlotType() == Group.ARMOR && !tool.isBroken()) {
+      float scaledLevel = getScaledLevel(tool, level);
+      entity.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(data -> {
+        BlastData blastData = data.get(BLAST_DATA);
+        if (blastData == null) {
+          // not calculated yet? add all vanilla values to the tracker
+          blastData = new BlastData();
+          data.put(BLAST_DATA, blastData);
+        }
+        // add ourself to the data
+        blastData.level.set(slot, scaledLevel);
+      });
+    }
+  }
+
+  /** On explosion, checks if any blast protected entity is involved, if so marks them for knockback update next tick */
+  private static void onExplosionDetonate(ExplosionEvent.Detonate event) {
+    Explosion explosion = event.getExplosion();
+    Vector3d center = explosion.getPosition();
+    float diameter = explosion.size * 2;
+    // search the entities for someone protection by blast protection
+    for (Entity entity : event.getAffectedEntities()) {
+      if (!entity.isImmuneToExplosions()) {
+        entity.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(data -> {
+          // if the entity has blast protection and the blast protection level is bigger than vanilla, time to process
+          BlastData blastData = data.get(BLAST_DATA);
+          if (blastData != null && blastData.level.getMax() > 0) {
+            // explosion is valid as long as the entity's eye is not directly on the explosion
+            double x = entity.getPosX() - center.x;
+            double z = entity.getPosZ() - center.z;
+            if (x != 0 || z != 0 || (entity.getPosYEye() - center.y) != 0) {
+              // we need two numbers to calculate the knockback: distance to explosion and block density
+              double y = entity.getPosY() - center.y;
+              double distance = MathHelper.sqrt(x*x + y*y + z*z) / diameter;
+              if (distance <= 1) {
+                blastData.wasKnockback = true;
+              }
+            }
+          }
+        });
+      }
+    }
+  }
+
+  /** If the entity is marked for knockback update, adjust velocity */
+  private static void livingTick(LivingUpdateEvent event) {
+    LivingEntity living = event.getEntityLiving();
+    if (!living.getEntityWorld().isRemote) {
+      living.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(data -> {
+        BlastData blastData = data.get(BLAST_DATA);
+        if (blastData != null && blastData.wasKnockback) {
+          blastData.wasKnockback = false;
+          float max = blastData.level.getMax();
+          if (max > 0) {
+            // due to MC-198809, vanilla does not actually reduce the knockback except on levels higher than obtainable in survival (blast prot VII)
+            // thus, we only care about our own level for reducing
+            double scale = 1 - (blastData.level.getMax() * 0.15f);
+            if (scale <= 0) {
+              living.setMotion(Vector3d.ZERO);
+            } else {
+              living.setMotion(living.getMotion().mul(scale, scale, scale));
+            }
+            living.velocityChanged = true;
+          }
+        }
+      });
+    }
+  }
+
+  /** Data object for the modifier */
+  private static class BlastData {
+    /** Max level of the modifier */
+    ModifierMaxLevel level = new ModifierMaxLevel();
+    /** If true, the entity was knocked back and needs their velocity adjusted */
+    boolean wasKnockback = false;
+  }
 }
--- src/main/java/slimeknights/tconstruct/tools/modifiers/defense/FireProtectionModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/defense/FireProtectionModifier.java
@@ -8,10 +8,9 @@ import net.minecraft.inventory.EquipmentSlotType.Group;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.EventPriority;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.IncrementalModifier;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
@@ -21,17 +20,17 @@ import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.definition.ModifiableArmorMaterial;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 import slimeknights.tconstruct.library.utils.TooltipFlag;
+import slimeknights.tconstruct.tools.logic.ModifierMaxLevel;
+import slimeknights.tconstruct.tools.logic.VanillaMaxLevel;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
-@EventBusSubscriber(modid = TConstruct.MOD_ID, bus = Bus.FORGE)
 public class FireProtectionModifier extends IncrementalModifier {
   /** Entity data key for the data associated with this modifier */
   private static final TinkerDataKey<FireData> FIRE_DATA = TConstruct.createKey("fire_protection");
-
   public FireProtectionModifier() {
     super(0x4F4A47);
+    MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, LivingUpdateEvent.class, FireProtectionModifier::livingTick);
   }
 
   @Override
@@ -53,7 +52,7 @@ public class FireProtectionModifier extends IncrementalModifier {
    */
 
   /** Gets the level of fire aspect for a particular slot */
-  private static int getFireAspectLevel(EquipmentChangeContext context, EquipmentSlotType slotType) {
+  private int getEnchantmentLevel(EquipmentChangeContext context, EquipmentSlotType slotType) {
     if (context.getToolInSlot(slotType) == null) {
       return EnchantmentHelper.getEnchantmentLevel(Enchantments.FIRE_PROTECTION, context.getEntity().getItemStackFromSlot(slotType));
     }
@@ -69,14 +68,13 @@ public class FireProtectionModifier extends IncrementalModifier {
         FireData fireData = data.get(FIRE_DATA);
         if (fireData != null) {
           // clear level
-          fireData.setLevel(slot, 0);
+          fireData.modifier.set(slot, 0);
 
-          // if this was the last fire protection, stop tracking
+          // if this was the last, stop tracking
           // no need to update vanilla here, the slot in charge will take care of it in equipment change
-          if (fireData.maxLevel == 0) {
+          if (fireData.modifier.getMax() == 0) {
             data.remove(FIRE_DATA);
           }
-
         }
       });
     }
@@ -94,13 +92,14 @@ public class FireProtectionModifier extends IncrementalModifier {
           // not calculated yet? add all vanilla values to the tracker
           fireData = new FireData();
           for (EquipmentSlotType slotType : ModifiableArmorMaterial.ARMOR_SLOTS) {
-            fireData.setVanilla(slotType, getFireAspectLevel(context, slotType));
+            fireData.vanilla.set(slotType, getEnchantmentLevel(context, slotType));
+          }
           }
           data.put(FIRE_DATA, fireData);
         }
         // add ourself to the data
-        fireData.setLevel(slot, scaledLevel);
-        fireData.setVanilla(slot, 0);
+        fireData.modifier.set(slot, scaledLevel);
+        fireData.vanilla.set(slot, 0);
       });
     }
   }
@@ -114,20 +113,19 @@ public class FireProtectionModifier extends IncrementalModifier {
       entity.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(data -> {
         FireData fireData = data.get(FIRE_DATA);
         // only need to check if we are in charge of fire data
-        if (fireData != null && fireData.maxSlot == slotType) {
-          fireData.setVanilla(slot, getFireAspectLevel(context, slot));
+        if (fireData != null && fireData.modifier.getMaxSlot() == slotType) {
+          fireData.vanilla.set(slot, getEnchantmentLevel(context, slot));
         }
       });
     }
   }
 
-  /* Ticking
+  /**
+   * Ticking
    * checks each tick if the player has more fire than we remember, if so means they are recently on fire and fire should be reduced
    * does not handle forced fire ticks well, but all vanilla uses of that method are forcing it to 1
    */
-
-  @SubscribeEvent
-  static void livingTick(LivingUpdateEvent event) {
+  private static void livingTick(LivingUpdateEvent event) {
     // handled on entity tick as we don't need any tool data, its all cached on the player
     // plus, saves us doing slot checks every tool with the modifier
     LivingEntity entity = event.getEntityLiving();
@@ -137,7 +135,9 @@ public class FireProtectionModifier extends IncrementalModifier {
         FireData fireData = data.get(FIRE_DATA);
         if (fireData != null) {
           // if the vanilla level is greater than ours, nothing to do
-          if (fireData.maxVanilla < fireData.maxLevel) {
+          int maxVanilla = fireData.vanilla.getMax();
+          float maxLevel = fireData.modifier.getMax();
+          if (maxVanilla < maxLevel) {
             // if the entity is not on fire, make sure fire finish is before the current time
             // if not, could miss getting lit on fire later
             int currentFire = entity.getFireTimer();
@@ -150,13 +150,13 @@ public class FireProtectionModifier extends IncrementalModifier {
             } else if (currentFire + entity.ticksExisted > fireData.finish) {
               // alright, time to reduce fire amount, if vanilla already did it reduce less
               int newFire = currentFire;
-              if (fireData.maxVanilla > 0) {
+              if (maxVanilla > 0) {
                 // we already removed 15% of fire per vanilla level, we want to remove an additional 15% of the original per our level
                 // means calculating the fire before vanilla made changes and then taking the difference between us and vanilla out
-                newFire -= MathHelper.floor((currentFire / (1 - fireData.maxVanilla * 0.15f)) * (fireData.maxLevel - fireData.maxVanilla) * 0.15f);
+                newFire -= MathHelper.floor((currentFire / (1 - maxVanilla * 0.15f)) * (maxLevel - maxVanilla) * 0.15f);
               } else {
                 // remove 15% of fire per level
-                newFire -= MathHelper.floor(currentFire * fireData.maxLevel * 0.15f);
+                newFire -= MathHelper.floor(currentFire * maxLevel * 0.15f);
               }
               entity.forceFireTicks(newFire);
               fireData.finish = newFire + entity.ticksExisted + 1; // set 1 higher make it slightly faster if increasing fire multiple times in the same tick (fire blocks)
@@ -168,65 +168,12 @@ public class FireProtectionModifier extends IncrementalModifier {
   }
 
   /** Data class for fire protection */
-  private static class FireData {
-    @Nullable
-    private EquipmentSlotType maxSlot = null;
-
-    /** Level for each slot, for tracking the largest */
-    private final float[] tinkerLevels = new float[4];
-    /** Highest modifier level */
-    float maxLevel;
-
-    /** Vanilla level for each slot, for tracking the largest */
-    private final int[] vanillaLevels = new int[4];
-    /** Level of the vanilla modifier */
-    int maxVanilla = 0;
-
+  protected static class FireData {
+    /** Level of the modifier */
+    public final ModifierMaxLevel modifier = new ModifierMaxLevel();
+    /** Level of the vanilla enchantment */
+    public final VanillaMaxLevel vanilla = new VanillaMaxLevel();
     /** Tick time when the fire is expected to finish, to determine if fire changed */
     int finish = 0;
-
-    /** Sets the given value in the structure */
-    void setLevel(EquipmentSlotType slot, float level) {
-      float oldLevel = tinkerLevels[slot.getIndex()];
-      if (level != oldLevel) {
-        // first, update level
-        tinkerLevels[slot.getIndex()] = level;
-        // if larger than max, new max
-        if (level >= maxLevel) {
-          maxLevel = level;
-          maxSlot = slot;
-        } else if (slot == maxSlot) {
-          // if the old level was max, find new max
-          maxLevel = 0;
-          for (EquipmentSlotType armorSlot : ModifiableArmorMaterial.ARMOR_SLOTS) {
-            float value = tinkerLevels[armorSlot.getIndex()];
-            if (value > maxLevel) {
-              maxLevel = value;
-              maxSlot = armorSlot;
-            }
-          }
-        }
-      }
-    }
-
-    /** Sets the given vanilla level in the structure */
-    void setVanilla(EquipmentSlotType slot, int level) {
-      int oldLevel = vanillaLevels[slot.getIndex()];
-      if (level != oldLevel) {
-        vanillaLevels[slot.getIndex()] = level;
-        // if new max, update max
-        if (level > maxVanilla) {
-          maxVanilla = level;
-        } else if (maxVanilla == oldLevel) {
-          // if was max before, search for replacement max
-          maxVanilla = 0;
-          for (int value : vanillaLevels) {
-            if (value > maxVanilla) {
-              maxVanilla = value;
-            }
-          }
-        }
-      }
-    }
   }
 }
