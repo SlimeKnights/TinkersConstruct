de4e7437667ddf04795bf1278e308d6182941e6b
KnightMiner <knightminer4@gmail.com>
2021-12-04 16:27:04 -0500

    Added fluid container ingredient
    Will move to Mantle later, but easier to test here

--- src/main/java/slimeknights/tconstruct/shared/TinkerCommons.java
+++ src/main/java/slimeknights/tconstruct/shared/TinkerCommons.java
@@ -33,6 +33,7 @@ import slimeknights.tconstruct.common.json.ConfigEnabledCondition;
 import slimeknights.tconstruct.common.json.SetFluidLootFunction;
 import slimeknights.tconstruct.common.recipe.BlockOrEntityCondition;
 import slimeknights.tconstruct.common.recipe.RecipeCacheInvalidator;
+import slimeknights.tconstruct.library.recipe.ingredient.FluidContainerIngredient;
 import slimeknights.tconstruct.library.utils.SlimeBounceHandler;
 import slimeknights.tconstruct.library.utils.Util;
 import slimeknights.tconstruct.shared.block.BetterPaneBlock;
@@ -122,6 +123,7 @@ public final class TinkerCommons extends TinkerModule {
     lootBlockOrEntity = Registry.register(Registry.LOOT_CONDITION_TYPE, BlockOrEntityCondition.ID, new LootConditionType(BlockOrEntityCondition.SERIALIZER));
     lootSetFluid = Registry.register(Registry.LOOT_FUNCTION_TYPE, SetFluidLootFunction.ID, new LootFunctionType(new SetFluidLootFunction.Serializer()));
     CriteriaTriggers.register(CONTAINER_OPENED_TRIGGER);
+    CraftingHelper.register(FluidContainerIngredient.ID, FluidContainerIngredient.SERIALIZER);
   }
 
   @SubscribeEvent
