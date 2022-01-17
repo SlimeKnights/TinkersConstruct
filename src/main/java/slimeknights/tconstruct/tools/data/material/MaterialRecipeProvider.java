aa705815525fa80be45644d349ec0215d5c4c86a
KnightMiner <knightminer4@gmail.com>
2022-01-17 17:03:38 -0500

    Adjust slimeskull stats so repairing makes more sense
    Gunpowder now is 2 per material instead of 4, and repairs 1.33x more (15 each instead of 5)
    Rabbit hide is 1 per material instead of 2, and repairs 1.5x more (50 each instead of ~15)
    Spider eye is 1 per material instead of 4 and repairs almost 2x more (46 each instead of 6)
    Ender pearls repair 0.8x as much (60 instead of 75)
    Phantom repairs 2.8x as much (108 instead of 38)
    Rotten flesh repairs 0.3x as much (15 instead of 29)
    Venom repairs 3x as much (266 per bucket instead of 86)
    Ender slime repairs slightly more (10 durability instead of 8)
    See also #4714

--- src/main/java/slimeknights/tconstruct/tools/data/material/MaterialRecipeProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/material/MaterialRecipeProvider.java
@@ -111,9 +111,9 @@ public class MaterialRecipeProvider extends BaseRecipeProvider implements IMater
 
     // slimeskull
     metalMaterialRecipe(consumer, MaterialIds.gold, folder, "gold", false);
-    materialRecipe(consumer, MaterialIds.gunpowder,   Ingredient.of(Tags.Items.GUNPOWDER),                      1, 4, folder + "gunpowder");
+    materialRecipe(consumer, MaterialIds.gunpowder,   Ingredient.of(Tags.Items.GUNPOWDER),                      1, 2, folder + "gunpowder");
     materialRecipe(consumer, MaterialIds.enderPearl,  Ingredient.of(Tags.Items.ENDER_PEARLS),                   1, 1, folder + "ender_pearl");
-    materialRecipe(consumer, MaterialIds.spider,      Ingredient.of(Items.SPIDER_EYE),                        1, 4, folder + "spider");
+    materialRecipe(consumer, MaterialIds.spider,      Ingredient.of(Items.SPIDER_EYE),                        1, 1, folder + "spider");
     materialRecipe(consumer, MaterialIds.venom,       FluidContainerIngredient.fromFluid(TinkerFluids.venom, false), 4, 1, folder + "venom_bucket");
     materialRecipe(consumer, MaterialIds.rottenFlesh, Ingredient.of(Items.ROTTEN_FLESH),                      1, 1, folder + "rotten_flesh");
     // slimesuit
@@ -122,7 +122,7 @@ public class MaterialRecipeProvider extends BaseRecipeProvider implements IMater
     materialRecipe(consumer, MaterialIds.enderslime, Ingredient.of(TinkerWorld.slime.get(SlimeType.ENDER)),          9, 1, folder + "enderslime/block");
     materialRecipe(consumer, MaterialIds.phantom,    Ingredient.of(Items.PHANTOM_MEMBRANE),    1, 1, folder + "phantom_membrane");
     materialRecipe(consumer, MaterialIds.chorus,     Ingredient.of(Items.POPPED_CHORUS_FRUIT), 1, 1, folder + "chorus_popped");
-    materialRecipe(consumer, MaterialIds.rabbit,     Ingredient.of(Items.RABBIT_HIDE),         1, 2, folder + "rabbit_hide");
+    materialRecipe(consumer, MaterialIds.rabbit,     Ingredient.of(Items.RABBIT_HIDE),         1, 1, folder + "rabbit_hide");
   }
 
   private void addMaterialSmeltery(Consumer<FinishedRecipe> consumer) {
