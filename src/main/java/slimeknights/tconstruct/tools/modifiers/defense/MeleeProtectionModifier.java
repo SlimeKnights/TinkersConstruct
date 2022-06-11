eead9a5395896e73e353c904db9eedd988be0233
KnightMiner <knightminer4@gmail.com>
2022-06-11 04:31:36 -0400

    Rework melee protection to be more consistent with guardian thorns and other melee modifiers

--- src/main/java/slimeknights/tconstruct/tools/modifiers/defense/MeleeProtectionModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/defense/MeleeProtectionModifier.java
@@ -2,6 +2,7 @@ package slimeknights.tconstruct.tools.modifiers.defense;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.EntityDamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.TooltipFlag;
@@ -14,10 +15,24 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class MeleeProtectionModifier extends IncrementalModifier {
+  private static boolean doesApply(DamageSource source) {
+    if (source.isBypassMagic() || source.isProjectile() || source.isBypassInvul()) {
+      return false;
+    }
+    // if its caused by an entity, require it to simply not be thorns
+    // meets most normal melee attacks, like zombies, but also means a melee fire or melee magic attack will work
+    if (source.getEntity() != null) {
+      return source instanceof EntityDamageSource entityDamage && !entityDamage.isThorns();
+    } else {
+      // for non-entity damage, require it to not be any other type
+      // blocks dall damage, falling blocks, cactus, but not starving, drowning, freezing
+      return (source.isFall() || !source.isBypassArmor()) && !source.isFire() && !source.isMagic() && !source.isExplosion();
+    }
+  }
+
   @Override
   public float getProtectionModifier(IToolStackView tool, int level, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float modifierValue) {
-    // by melee, we mean not projectiles, fire, magic, or explosions
-    if (!source.isBypassMagic() && !source.isBypassInvul() && !source.isFire() && !source.isMagic() && !source.isProjectile() && !source.isExplosion()) {
+    if (doesApply(source)) {
       modifierValue += getScaledLevel(tool, level) * 2;
     }
     return modifierValue;
