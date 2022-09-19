18cc46fbda80e1a30d031e4769e0295dd564647a
KnightMiner <knightminer4@gmail.com>
2022-09-19 19:26:37 -0400

    Use new "extraData" for material tier sections
    Means books can now use any tier instead of needing to register one per tier

--- src/main/java/slimeknights/tconstruct/library/client/book/sectiontransformer/materials/AbstractMaterialSectionTransformer.java
+++ src/main/java/slimeknights/tconstruct/library/client/book/sectiontransformer/materials/AbstractMaterialSectionTransformer.java
@@ -1,8 +1,10 @@
 package slimeknights.tconstruct.library.client.book.sectiontransformer.materials;
 
+import net.minecraft.resources.ResourceLocation;
 import slimeknights.mantle.client.book.data.BookData;
 import slimeknights.mantle.client.book.data.PageData;
 import slimeknights.mantle.client.book.data.SectionData;
+import slimeknights.mantle.client.book.data.content.PageContent;
 import slimeknights.mantle.client.book.repository.BookRepository;
 import slimeknights.mantle.client.book.transformer.SectionTransformer;
 import slimeknights.mantle.client.screen.book.element.ItemElement;
@@ -16,6 +18,8 @@ import slimeknights.tconstruct.library.materials.definition.MaterialVariantId;
 
 import java.util.List;
 import java.util.ListIterator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public abstract class AbstractMaterialSectionTransformer extends SectionTransformer {
 
@@ -43,24 +47,50 @@ public abstract class AbstractMaterialSectionTransformer extends SectionTransfor
 
   @Override
   public void transform(BookData book, SectionData sectionData) {
+    createPages(book, sectionData, this::isValidMaterial, this::getPageContent);
+  }
+
+  /** Helper to add a page to the section */
+  private static PageData addPageStatic(SectionData data, String name, ResourceLocation type, PageContent content) {
+    PageData page = new PageData(true);
+    page.source = data.source;
+    page.parent = data;
+    page.name = name;
+    page.type = type;
+    page.content = content;
+    page.load();
+
+    data.pages.add(page);
+
+    return page;
+  }
+
+  /**
+   * Creates all the pages for the materials
+   * @param book            Book data
+   * @param sectionData     Section data
+   * @param validMaterial   Predicate to validate materials
+   * @param pageCreator     Logic to create a page
+   */
+  public static void createPages(BookData book, SectionData sectionData, Predicate<IMaterial> validMaterial, Function<MaterialVariantId, ContentMaterial> pageCreator) {
     sectionData.source = BookRepository.DUMMY;
     sectionData.parent = book;
 
-    List<IMaterial> materialList = MaterialRegistry.getMaterials().stream().filter(this::isValidMaterial).toList();
+    List<IMaterial> materialList = MaterialRegistry.getMaterials().stream().filter(validMaterial).toList();
     if (materialList.isEmpty()) {
       return;
     }
 
     // calculate pages needed
-    List<ContentPageIconList> listPages = ContentPageIconList.getPagesNeededForItemCount(materialList.size(), sectionData, book.translate(this.sectionName), book.strings.get(String.format("%s.subtext", this.sectionName)));
+    List<ContentPageIconList> listPages = ContentPageIconList.getPagesNeededForItemCount(materialList.size(), sectionData, book.translate(sectionData.name), book.strings.get(String.format("%s.subtext", sectionData.name)));
 
     ListIterator<ContentPageIconList> iter = listPages.listIterator();
     ContentPageIconList overview = iter.next();
 
     for (IMaterial material : materialList) {
       MaterialId materialId = material.getIdentifier();
-      ContentMaterial contentMaterial = this.getPageContent(materialId);
-      PageData page = this.addPage(sectionData, materialId.toString(), ContentMaterial.ID, contentMaterial);
+      ContentMaterial contentMaterial = pageCreator.apply(materialId);
+      PageData page = addPageStatic(sectionData, materialId.toString(), ContentMaterial.ID, contentMaterial);
 
       SizedBookElement icon = new ItemElement(0, 0, 1f, contentMaterial.getDisplayStacks());
       while (!overview.addLink(icon, contentMaterial.getTitleComponent(), page)) {
