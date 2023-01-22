c5a4708808ac739e46311cb35fbf33a15ac3bc53
KnightMiner <knightminer4@gmail.com>
2023-01-22 15:37:20 -0500

    Fix modifier extraction allowing you to create an incremental modifier crystal without paying the full modifier cost

--- src/main/java/slimeknights/tconstruct/tools/recipe/ExtractModifierRecipe.java
+++ src/main/java/slimeknights/tconstruct/tools/recipe/ExtractModifierRecipe.java
@@ -9,8 +9,10 @@ import slimeknights.mantle.recipe.ingredient.SizedIngredient;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
+import slimeknights.tconstruct.library.modifiers.impl.IncrementalModifier;
 import slimeknights.tconstruct.library.recipe.ITinkerableContainer;
 import slimeknights.tconstruct.library.recipe.ITinkerableContainer.Mutable;
+import slimeknights.tconstruct.library.recipe.modifiers.ModifierRecipeLookup;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 import slimeknights.tconstruct.tools.item.ModifierCrystalItem;
@@ -35,10 +37,28 @@ public class ExtractModifierRecipe extends ModifierRemovalRecipe {
 
   @Override
   public Component getDescription(@Nullable ITinkerableContainer inv) {
-    if (inv != null && inv.getTinkerable().getUpgrades().getModifiers().stream().noneMatch(entryPredicate)) {
-      return NO_MODIFIERS;
+    if (inv != null) {
+      IToolStackView tool = inv.getTinkerable();
+      if (filter(tool, tool.getModifierList()).isEmpty()) {
+        return NO_MODIFIERS;
+      }
     }
     return DESCRIPTION;
+  }
+
+  @Override
+  protected List<ModifierEntry> filter(@Nullable IToolStackView tool, List<ModifierEntry> modifiers) {
+    if (tool != null) {
+      // filter out incremental modifiers at level 1 with only a partial level to prevent exploiting the recipe to get a lot of crystals
+      return modifiers.stream().filter(entryPredicate).filter(entry -> {
+        if (entry.getLevel() == 1) {
+          int neededPerLevel = ModifierRecipeLookup.getNeededPerLevel(entry.getId());
+          return neededPerLevel <= 0 || IncrementalModifier.getAmount(tool, entry.getId()) >= neededPerLevel;
+        }
+        return true;
+      }).toList();
+    }
+    return super.filter(tool, modifiers);
   }
 
   @Override
