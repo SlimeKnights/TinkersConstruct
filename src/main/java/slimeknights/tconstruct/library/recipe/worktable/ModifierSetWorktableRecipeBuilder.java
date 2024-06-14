38d32873e4270222a704f4bc7e3d183a0bfdf971
Waterpicker <waterpickerenternity@gmail.com>
2024-06-13 19:02:47 -0500

    Forgot to fix key -> dataKey

--- src/main/java/slimeknights/tconstruct/library/recipe/worktable/ModifierSetWorktableRecipeBuilder.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/worktable/ModifierSetWorktableRecipeBuilder.java
@@ -20,7 +20,7 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ModifierSetWorktableRecipeBuilder extends AbstractSizedIngredientRecipeBuilder<ModifierSetWorktableRecipeBuilder> {
-  private final ResourceLocation key;
+  private final ResourceLocation dataKey;
   @Setter @Accessors(fluent = true)
   private IJsonPredicate<ModifierId> modifierPredicate = ModifierPredicate.ANY;
   private final boolean addToSet;
@@ -56,7 +56,7 @@ public class ModifierSetWorktableRecipeBuilder extends AbstractSizedIngredientRe
 
   @Override
   public void save(Consumer<FinishedRecipe> consumer) {
-    save(consumer, key);
+    save(consumer, dataKey);
   }
 
   @Override
@@ -68,6 +68,6 @@ public class ModifierSetWorktableRecipeBuilder extends AbstractSizedIngredientRe
       throw new IllegalStateException("Tools cannot be empty");
     }
     ResourceLocation advancementId = buildOptionalAdvancement(id, "modifiers");
-    consumer.accept(new LoadableFinishedRecipe<>(new ModifierSetWorktableRecipe(id, key, inputs, tools, modifierPredicate, addToSet, allowTraits), ModifierSetWorktableRecipe.LOADER, advancementId));
+    consumer.accept(new LoadableFinishedRecipe<>(new ModifierSetWorktableRecipe(id, dataKey, inputs, tools, modifierPredicate, addToSet, allowTraits), ModifierSetWorktableRecipe.LOADER, advancementId));
   }
 }
