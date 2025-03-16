6c59c0afacb968efb016f1bbbdcbdc3c9ceae36a
IACTU <john@jbdm.org>
2025-03-16 12:02:44 -0500

    Started on casting tank block and entity

---
+++ src/main/java/slimeknights/tconstruct/smeltery/block/CastingTankBlock.java
@@ -0,0 +1,113 @@
+package slimeknights.tconstruct.smeltery.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.fluids.FluidStack;
+import slimeknights.mantle.block.InventoryBlock;
+import slimeknights.mantle.fluid.FluidTransferHelper;
+import slimeknights.mantle.util.BlockEntityHelper;
+import slimeknights.tconstruct.library.utils.NBTTags;
+import slimeknights.tconstruct.smeltery.block.entity.CastingTankBlockEntity;
+import slimeknights.tconstruct.smeltery.block.entity.CastingTankBlockEntity.ITankBlock;
+import slimeknights.tconstruct.smeltery.block.entity.ITankBlockEntity;
+
+import javax.annotation.Nullable;
+
+public class CastingTankBlock extends InventoryBlock implements ITankBlock, EntityBlock {
+  private final PushReaction pushReaction;
+  public CastingTankBlock(Properties properties, PushReaction pushReaction) {
+    super(properties);
+    this.pushReaction = pushReaction;
+  }
+
+  public CastingTankBlock(Properties properties) {
+    this(properties, PushReaction.BLOCK);
+  }
+
+  @Override
+  protected boolean openGui(Player player, Level world, BlockPos pos) {
+    return false;
+  }
+
+  @Override
+  public PushReaction getPistonPushReaction(BlockState pState) {
+    return pushReaction;
+  }
+
+  @Deprecated
+  @Override
+  public float getShadeBrightness(BlockState state, BlockGetter worldIn, BlockPos pos) {
+    return 1.0F;
+  }
+
+  @Override
+  @Nullable
+  public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+    return new CastingTankBlockEntity(pPos, pState, this);
+  }
+
+  @Deprecated
+  @Override
+  public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+    if (FluidTransferHelper.interactWithTank(world, pos, player, hand, hit)) {
+      return InteractionResult.SUCCESS;
+    }
+    return super.use(state, world, pos, player, hand, hit);
+  }
+
+  @Override
+  public int getLightEmission(BlockState state, BlockGetter world, BlockPos pos) {
+    BlockEntity te = world.getBlockEntity(pos);
+    if (te instanceof ITankBlockEntity) {
+      FluidStack fluid = ((ITankBlockEntity) te).getTank().getFluid();
+      return fluid.getFluid().getFluidType().getLightLevel(fluid);
+    }
+    return super.getLightEmission(state, world, pos);
+  }
+
+  @Override
+  public void setPlacedBy(Level worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+    CompoundTag nbt = stack.getTag();
+    if (nbt != null) {
+      BlockEntityHelper.get(CastingTankBlockEntity.class, worldIn, pos).ifPresent(te -> te.updateTank(nbt.getCompound(NBTTags.TANK)));
+    }
+    super.setPlacedBy(worldIn, pos, state, placer, stack);
+  }
+
+  @Deprecated
+  @Override
+  public boolean hasAnalogOutputSignal(BlockState state) {
+    return true;
+  }
+
+  @Deprecated
+  @Override
+  public int getAnalogOutputSignal(BlockState blockState, Level worldIn, BlockPos pos) {
+    return ITankBlockEntity.getComparatorInputOverride(worldIn, pos);
+  }
+
+  @Override
+  public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter world, BlockPos pos, Player player) {
+    ItemStack stack = new ItemStack(this);
+    BlockEntityHelper.get(CastingTankBlockEntity.class, world, pos).ifPresent(te -> te.setTankTag(stack));
+    return stack;
+  }
+
+  @Override
+  public int getCapacity() {
+    return CastingTankBlockEntity.DEFAULT_CAPACITY;
+  }
+}
---
+++ src/main/java/slimeknights/tconstruct/smeltery/block/entity/CastingTankBlockEntity.java
@@ -0,0 +1,193 @@
+package slimeknights.tconstruct.smeltery.block.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.data.ModelData;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.client.model.ModelProperties;
+import slimeknights.tconstruct.library.fluid.FluidTankAnimated;
+import slimeknights.tconstruct.library.utils.NBTTags;
+import slimeknights.tconstruct.shared.block.entity.TableBlockEntity;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+import slimeknights.tconstruct.smeltery.item.TankItem;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class CastingTankBlockEntity extends TableBlockEntity implements ITankBlockEntity { // TODO needs item handler
+  /** Max capacity for the tank */
+  public static final int DEFAULT_CAPACITY = FluidType.BUCKET_VOLUME * 2;
+
+  private static final Component NAME = TConstruct.makeTranslation("gui", "casting");
+
+  /**
+   * Gets the capacity for the given block
+   * @param block  block
+   * @return  Capacity
+   */
+  public static int getCapacity(Block block) {
+    if (block instanceof ITankBlock) {
+      return ((ITankBlock) block).getCapacity();
+    }
+    return DEFAULT_CAPACITY;
+  }
+
+  /**
+   * Gets the capacity for the given item
+   * @param item  item
+   * @return  Capacity
+   */
+  public static int getCapacity(Item item) {
+    if (item instanceof BlockItem) {
+      return getCapacity(((BlockItem) item).getBlock());
+    }
+    return DEFAULT_CAPACITY;
+  }
+
+  /** Internal fluid tank instance */
+  @Getter
+  protected final FluidTankAnimated tank;
+  /** Capability holder for the tank */
+  private final LazyOptional<IFluidHandler> holder;
+  /** Last comparator strength to reduce block updates */
+  @Getter @Setter
+  private int lastStrength = -1;
+
+  /** Main constructor */
+  public CastingTankBlockEntity(BlockPos pos, BlockState state, ITankBlock block) {
+    this(TinkerSmeltery.tank.get(), pos, state, block);
+  }
+
+  /** Extendable constructor */
+  @SuppressWarnings("WeakerAccess")
+  protected CastingTankBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state, ITankBlock block) {
+    super(type, pos, state, NAME, 2, 1);
+    tank = new FluidTankAnimated(block.getCapacity(), this);
+    holder = LazyOptional.of(() -> tank);
+  }
+
+
+  /*
+   * Tank methods
+   */
+
+  @Override
+  @Nonnull
+  public <T> LazyOptional<T> getCapability(Capability<T> capability, @Nullable Direction facing) {
+    if (capability == ForgeCapabilities.FLUID_HANDLER) {
+      return holder.cast();
+    }
+    return super.getCapability(capability, facing);
+  }
+
+  @Override
+  public void invalidateCaps() {
+    super.invalidateCaps();
+    holder.invalidate();
+  }
+
+  @Nonnull
+  @Override
+  public ModelData getModelData() {
+    return ModelData.builder()
+      .with(ModelProperties.FLUID_STACK, tank.getFluid())
+      .with(ModelProperties.TANK_CAPACITY, tank.getCapacity()).build();
+  }
+
+  @Override
+  public void onTankContentsChanged() {
+    ITankBlockEntity.super.onTankContentsChanged();
+    if (this.level != null) {
+      level.getLightEngine().checkBlock(this.worldPosition);
+      this.requestModelDataUpdate();
+    }
+  }
+
+  @Override
+  public void updateFluidTo(FluidStack fluid) {
+    ITankBlockEntity.super.updateFluidTo(fluid);
+    // update light if the fluid changes
+    if (this.level != null) {
+      level.getLightEngine().checkBlock(this.worldPosition);
+    }
+  }
+
+
+  /*
+   * NBT
+   */
+
+  /**
+   * Sets the tag on the stack based on the contained tank
+   * @param stack  Stack
+   */
+  public void setTankTag(ItemStack stack) {
+    TankItem.setTank(stack, tank);
+  }
+
+  /**
+   * Updates the tank from an NBT tag, used in the block
+   * @param nbt  tank NBT
+   */
+  public void updateTank(CompoundTag nbt) {
+    if (nbt.isEmpty()) {
+      tank.setFluid(FluidStack.EMPTY);
+    } else {
+      tank.readFromNBT(nbt);
+      if (level != null) {
+        level.getLightEngine().checkBlock(worldPosition);
+      }
+    }
+  }
+
+  @Override
+  protected boolean shouldSyncOnUpdate() {
+    return true;
+  }
+
+  @Override
+  public void load(CompoundTag tag) {
+    tank.setCapacity(getCapacity(getBlockState().getBlock()));
+    updateTank(tag.getCompound(NBTTags.TANK));
+    super.load(tag);
+  }
+
+  @Override
+  public void saveSynced(CompoundTag tag) {
+    super.saveSynced(tag);
+    // want tank on the client on world load
+    if (!tank.isEmpty()) {
+      tag.put(NBTTags.TANK, tank.writeToNBT(new CompoundTag()));
+    }
+  }
+
+  @Override
+  public AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+    return null;
+  }
+
+  /** Interface for blocks to return their capacity */
+  public interface ITankBlock {
+    /** Gets the capacity for this tank */
+    int getCapacity();
+  }
+}
