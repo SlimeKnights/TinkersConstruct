312053bef11ef895596faf1678913ce2a18308a3
KnightMiner <knightminer4@gmail.com>
2026-09-08 03:45:18 -0400

    Hide repair kits from the part builder display
    Its just an internal type used for some materials

--- src/main/java/slimeknights/tconstruct/tables/client/inventory/PartBuilderScreen.java
+++ src/main/java/slimeknights/tconstruct/tables/client/inventory/PartBuilderScreen.java
@@ -28,6 +28,7 @@ import slimeknights.tconstruct.library.recipe.partbuilder.Pattern;
 import slimeknights.tconstruct.library.utils.Util;
 import slimeknights.tconstruct.tables.block.entity.table.PartBuilderBlockEntity;
 import slimeknights.tconstruct.tables.menu.PartBuilderContainerMenu;
+import slimeknights.tconstruct.tools.stats.StatlessMaterialStats;
 
 import java.util.List;
 import java.util.function.Function;
@@ -279,6 +280,8 @@ public class PartBuilderScreen extends BaseTabbedScreen<PartBuilderBlockEntity,P
 
     MaterialId id = materialVariant.getId();
     for (IMaterialStats stat : MaterialRegistry.getInstance().getAllStats(id)) {
+      // skip repair kit, its just an internal marker and the traits are inaccurate
+      if (stat == StatlessMaterialStats.REPAIR_KIT) continue;
       List<Component> info = stat.getLocalizedInfo();
 
       if (!info.isEmpty()) {
