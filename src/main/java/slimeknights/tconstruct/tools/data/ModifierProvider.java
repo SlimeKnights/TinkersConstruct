1f3620702ec756811092d8be3d4a2c0bf8bcbdcb
KnightMiner <knightminer4@gmail.com>
2026-09-10 04:49:17 -0400

    Tweak enderdodging scaling
    Now is a flat 7.5% chance per level, same cap on enderslime but starts a little weaker.
    Means rebalanced slimesuit is notably better than travelers.
    
    Apologizes to translators if I made a mess of updating that in your language; converting "X%+Y% per level" was mostly straight forward but I had to guess at the grammar

--- src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
@@ -1075,9 +1075,9 @@ public class ModifierProvider extends AbstractModifierProvider implements ICondi
     buildModifier(ModifierIds.enderdodging).priority(50) // after recurrent
       .levelDisplay(ModifierLevelDisplay.SINGLE_LEVEL)
       // projectiles: 20% chance per piece, 30% when rebalanced. 10 second cooldown
-      .addModule(TeleportDodgeModule.builder().damageSource(isProjectile).chance(new LevelingValue(0.1f, 0.1f)).flat(10 * 20))
+      .addModule(TeleportDodgeModule.builder().damageSource(isProjectile).chance(LevelingValue.eachLevel(0.15f)).flat(10 * 20))
       // entity caused damage: 10% chance per piece, 15% when rebalanced. 10 second cooldown
-      .addModule(TeleportDodgeModule.builder().damageSource(DamageSourcePredicate.and(DamageSourcePredicate.HAS_ENTITY, isProjectile.inverted())).chance(new LevelingValue(0.05f, 0.05f)).flat(10 * 20));
+      .addModule(TeleportDodgeModule.builder().damageSource(DamageSourcePredicate.and(DamageSourcePredicate.HAS_ENTITY, isProjectile.inverted())).chance(LevelingValue.eachLevel(0.075f)).flat(10 * 20));
     buildModifier(ModifierIds.forming)
       .levelDisplay(ModifierLevelDisplay.SINGLE_LEVEL)
       .addModule(StatBoostModule.add(ToolStats.ARMOR).eachLevel(1))
