f245299127495a41018985c2417afda1514c2a5b
KnightMiner <knightminer4@gmail.com>
2026-09-10 20:56:12 -0400

    Allow making clay repair kits in the part builder

--- src/main/java/slimeknights/tconstruct/tools/data/material/MaterialDataProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/material/MaterialDataProvider.java
@@ -124,7 +124,7 @@ public class MaterialDataProvider extends AbstractMaterialDataProvider {
     material(MaterialIds.ancient    ).tier(5).sort(ORDER_NETHER ).hidden();
 
     // slimesuit
-    material(MaterialIds.clay ).tier(2).sort(ORDER_REPAIR + 5);
+    material(MaterialIds.clay ).tier(2).sort(ORDER_REPAIR + 5).craftable();
     material(MaterialIds.honey).tier(2).sort(ORDER_REPAIR + 5);
     material(MaterialIds.blood).tier(2).sort(ORDER_REPAIR).hidden();
     // slimesuit parts
