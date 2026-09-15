a1dc82abd147d8b01df47f35a2d1412794e27fc0
KnightMiner <knightminer4@gmail.com>
2026-09-15 00:58:59 -0400

    Fix recipes for melting "raw steel" not being marked optional
    Only 1 or 2 mods add raw steel "ore" so that tag is normally empty

--- src/main/java/slimeknights/tconstruct/smeltery/data/SmelteryRecipeProvider.java
+++ src/main/java/slimeknights/tconstruct/smeltery/data/SmelteryRecipeProvider.java
@@ -2251,7 +2251,7 @@ public class SmelteryRecipeProvider extends BaseRecipeProvider implements ISmelt
     metal(consumer, TinkerFluids.moltenIron  ).ore(Byproduct.STEEL        ).metal().dust().plate().gear().coin().sheetmetal().geore().oreberry().minecraftTools().toolCostMelting(11, "tools_costing_11").common(HAMMER, FLAKES).rod();
     metal(consumer, TinkerFluids.moltenCobalt).ore(Byproduct.SMALL_DIAMOND).metal().dust()
       .melting(1/9f, "raw_nugget", 1/2f, false);
-    metal(consumer, TinkerFluids.moltenSteel ).metal().dust().plate().gear().coin().sheetmetal().common(SHOVEL_PLUS, SWORD, AXES, MEKANISM_SHIELD, FLAKES).common(ARMOR_PLUS).wire().rod().rawOre(Byproduct.IRON)
+    metal(consumer, TinkerFluids.moltenSteel ).metal().dust().plate().gear().coin().sheetmetal().common(SHOVEL_PLUS, SWORD, AXES, MEKANISM_SHIELD, FLAKES).common(ARMOR_PLUS).wire().rod().optional().rawOre(Byproduct.IRON)
       .toolItemMelting(11, "railcraft", "spike_maul")
       .melting(1/9f, "raw_nugget", 1/2f, false);
     // gold ore does non-standard byproduct handling, as it wants sparse gold ore to have a different byproduct, hence moving byproducts so we don't have ores for the metal call
